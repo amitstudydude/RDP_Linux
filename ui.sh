d6875f0b52247f63238f14391a0d67b42cd52435
amitstudydude <99377817+amitstudydude@users.noreply.github.com>
2022-03-29 00:33:56 +0530

    Update ui.sh

--- ui.sh
+++ ui.sh
@@ -1,8 +1,8 @@
 #/bin/bash/
 rm ui.sh
 sudo passwd 
-sudo nano /etc/apt/sources.list
-sudo nano /etc/ssh/sshd_config
+#sudo nano /etc/apt/sources.list
+#sudo nano /etc/ssh/sshd_config
 sudo adduser user
 sudo usermod -a -G sudo,adm user
 sudo apt update 
