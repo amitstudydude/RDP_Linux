2a69ba24ce06bc589594655ee0436f69ae045de7
amitstudydude <99377817+amitstudydude@users.noreply.github.com>
2022-03-28 21:11:18 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,11 +1,15 @@
 #/bin/bash
 rm *.sh
+nano /etc/apt/sources.list
+nano /etc/ssh/sshd_config
 sudo passwd 
 sudo adduser user
 usermod -a -G sudo,adm user
-nano /etc/apt/sources.list
 sudo apt update && apt upgrade -y && apt install ubuntu-desktop -y 
-wget https://www.c-nergy.be/downloads/xrdp-installer-1.2.2.zip && unzip *.zip && chmod +x *.sh
-./*.sh && ./*.sh -s
+wget https://www.c-nergy.be/downloads/xrdp-installer-1.2.2.zip 
+unzip *.zip
+chmod +x *.sh
+./*.sh
+./*.sh -s
 pulseaudio -k
 sudo reboot
