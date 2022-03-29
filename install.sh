90868c55eb13b9fe6d286043cdbfcde03e0dc8d5
amitstudydude <99377817+amitstudydude@users.noreply.github.com>
2022-03-29 09:28:21 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,6 +6,9 @@ sudo apt update
 sudo apt upgrade -y
 sudo add-apt-repository ppa:alessandro-strada/ppa
 sudo apt install google-drive-ocamlfuse
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+wget https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb
+sudo apt install ./*.deb -y
 sudo apt install ubuntu-desktop gnome -y
 wget https://www.c-nergy.be/downloads/xrdp-installer-1.2.2.zip 
 sudo unzip *.zip
