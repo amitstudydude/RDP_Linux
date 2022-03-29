3e055c06bdcbb838be84bc7952a55353ada3b554
amitstudydude <99377817+amitstudydude@users.noreply.github.com>
2022-03-29 05:14:15 +0000

    Update after.sh

--- after.sh
+++ after.sh
@@ -2,4 +2,7 @@
 pulseaudio -k
 mkdir /home/user/teamdrive
 google-drive-ocamlfuse -cc
+google-drive-ocamlfuse -label teamdrive 
+rm ~/.gdfuse/teamdrive/config
+cp config ~/.gdfuse/teamdrive
 google-drive-ocamlfuse -label teamdrive /home/user/teamdrive
