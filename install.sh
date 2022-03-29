950fb690a084de13faab8c36dd80f18db0bcfcc1
amitstudydude <99377817+amitstudydude@users.noreply.github.com>
2022-03-29 12:00:08 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,6 @@
 #/bin/bash
-rm *.sh.*
-rm install.sh
 rm *.sh
+rm install.sh
 sudo apt update 
 sudo apt upgrade -y
 sudo apt install google-drive-ocamlfuse
@@ -11,6 +10,7 @@ sudo apt install ./*.deb -y
 sudo apt install ubuntu-desktop gnome -y
 wget https://www.c-nergy.be/downloads/xrdp-installer-1.2.2.zip 
 sudo unzip *.zip
+wget https://raw.githubusercontent.com/amitstudydude/RDP_Linux/main/after.sh
 sudo chmod +x *.sh
 ./*.sh
 ./*.sh -s
