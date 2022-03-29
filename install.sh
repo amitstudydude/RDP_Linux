8197a7b143ebe20bc53503c6f8d802c693820573
amitstudydude <99377817+amitstudydude@users.noreply.github.com>
2022-03-29 09:25:47 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,6 +4,8 @@ rm install.sh
 rm *.sh
 sudo apt update 
 sudo apt upgrade -y
+sudo add-apt-repository ppa:alessandro-strada/ppa
+sudo apt install google-drive-ocamlfuse
 sudo apt install ubuntu-desktop gnome -y
 wget https://www.c-nergy.be/downloads/xrdp-installer-1.2.2.zip 
 sudo unzip *.zip
