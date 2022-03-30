3dc530b55f70034d34e8a2574739a7c51495d1e3
amitstudydude <99377817+amitstudydude@users.noreply.github.com>
2022-03-30 23:50:58 +0530

    Update after.sh

--- after.sh
+++ after.sh
@@ -3,9 +3,12 @@ pulseaudio -k
 mkdir ./teamdrive
 google-drive-ocamlfuse -cc
 google-drive-ocamlfuse -label teamdrive 
-
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+wget https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb
+sudo apt install ./*.deb -y
 rm ~/.gdfuse/teamdrive/config config
 wget  https://raw.githubusercontent.com/amitstudydude/RDP_Linux/6b6cc0106c46175aa44b77156386f63835e4d564/config
 cp config ~/.gdfuse/teamdrive
 google-drive-ocamlfuse -label teamdrive ./teamdrive
 sudo apt upgrade -y
+rm *
