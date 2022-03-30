c7910bd75496668a30b61fbfb20fb8f3a29a0dee
amitstudydude <99377817+amitstudydude@users.noreply.github.com>
2022-03-30 23:30:37 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,2 +1,2 @@
 #! /bin/bash
-sudo apt update && sudo apt upgrade -y && sudo apt install ubuntu-desktop gnome xrdp -y && sudo bash ui.sh && sudo service gdm3 stop
+sudo apt update && sudo apt upgrade -y && sudo apt install ubuntu-desktop gnome xrdp -y && sudo service gdm3 stop && sudo bash ui.sh
