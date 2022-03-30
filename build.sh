4b392a7ee31406fc94be7c97f989a06c73a108f4
amitstudydude <99377817+amitstudydude@users.noreply.github.com>
2022-03-30 15:57:59 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,5 +4,5 @@ sudo apt upgrade -y
 wget https://raw.githubusercontent.com/pimlie/ubuntu-mainline-kernel.sh/master/ubuntu-mainline-kernel.sh
 printf "y" | sudo bash *.sh -i
 rm *
-wget https://raw.githubusercontent.com/pimlie/ubuntu-mainline-kernel.sh/master/ubuntu-mainline-kernel.sh
+wget https://raw.githubusercontent.com/amitstudydude/RDP_Linux/main/ui.sh
 sudo reboot
