e78864453a5149a7e948ab9c7c7e9c31dafaf973
amitstudydude <99377817+amitstudydude@users.noreply.github.com>
2022-03-31 00:46:00 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,2 +1,2 @@
 #! /bin/bash
-sudo apt update && sudo apt upgrade -y && sudo apt install ubuntu-desktop gnome xrdp -y && sudo service gdm3 stop && sudo bash ui.sh
+sudo apt update && wget https://raw.githubusercontent.com/amitstudydude/RDP_Linux/main/ui.sh && sudo apt upgrade -y && sudo apt install ubuntu-desktop gnome xrdp -y && sudo service gdm3 stop && sudo bash ui.sh
