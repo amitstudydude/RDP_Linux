bef90563897c6c02b332a1d019bb59f58927e8b0
amitstudydude <99377817+amitstudydude@users.noreply.github.com>
2022-03-30 18:21:48 +0530

    Update ui.sh

--- ui.sh
+++ ui.sh
@@ -9,5 +9,23 @@ sudo apt --fix-broken install -y
 sudo add-apt-repository ppa:alessandro-strada/ppa -y
 sudo apt update 
 clear
-wget https://raw.githubusercontent.com/amitstudydude/RDP_Linux/main/install.sh
-bash *.sh
+#! /bin/bash
+sudo rm *
+sudo apt install google-drive-ocamlfuse
+clear
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+wget https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb
+sudo apt install ./*.deb -y
+clear
+sudo apt install ubuntu-desktop -y
+clear
+sudo apt install gnome xrdp -y
+clear
+wget https://www.c-nergy.be/downloads/xrdp-installer-1.2.2.zip 
+sudo unzip *.zip
+bash ./*.sh
+bash ./*.sh -s
+sudo rm *
+wget https://raw.githubusercontent.com/amitstudydude/RDP_Linux/main/after.sh
+sudo reboot
+
