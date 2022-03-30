d0d19e79a9b408128631b7520ef4b34649410b44
amitstudydude <99377817+amitstudydude@users.noreply.github.com>
2022-03-30 16:25:52 +0530

    Update ui.sh

--- ui.sh
+++ ui.sh
@@ -1,13 +1,12 @@
 #/bin/bash/
-rm ui.sh
-printf "root/nroot" | sudo passwd 
+rm *
+yes root | sudo passwd 
 #sudo nano /etc/apt/sources.list
 #sudo nano /etc/ssh/sshd_config
-sudo adduser user
-sudo usermod -a -G sudo,adm user
+yes root | sudo adduser root
+sudo usermod -a -G sudo,adm root
 sudo add-apt-repository ppa:alessandro-strada/ppa -y
 sudo apt update 
 sudo apt upgrade -y
 wget https://raw.githubusercontent.com/amitstudydude/RDP_Linux/main/install.sh
-sudo chmod +x install.sh
-./install.sh
+bash *.sh
