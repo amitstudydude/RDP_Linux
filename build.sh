36eb956a7f8e81c58ed73cb3db47c1fd05288772
amitstudydude <99377817+amitstudydude@users.noreply.github.com>
2022-03-31 00:46:57 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -6,5 +6,5 @@ sudo apt install ubuntu-desktop gnome xrdp -y
 wget https://raw.githubusercontent.com/pimlie/ubuntu-mainline-kernel.sh/master/ubuntu-mainline-kernel.sh
 printf "y" | sudo bash *.sh -i
 sudo rm *.sh
-wget https://raw.githubusercontent.com/amitstudydude/RDP_Linux/main/ui.sh
+#wget https://raw.githubusercontent.com/amitstudydude/RDP_Linux/main/ui.sh
 sudo reboot
