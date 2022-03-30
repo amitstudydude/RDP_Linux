6a6db34d78d7223d47236c32d7e65f98b9790e72
amitstudydude <99377817+amitstudydude@users.noreply.github.com>
2022-03-31 01:27:47 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,2 +1,2 @@
 #! /bin/bash
-sudo apt update || wget https://raw.githubusercontent.com/amitstudydude/RDP_Linux/main/build.sh https://raw.githubusercontent.com/amitstudydude/RDP_Linux/main/ui.sh || sudo apt upgrade -y && sudo apt install ubuntu-desktop gnome xrdp -y || sudo service gdm3 stop || sudo bash *.sh
+sudo apt update || wget https://download.nomachine.com/download/7.9/Linux/nomachine_7.9.2_1_amd64.deb || sudo apt install *.deb -y || sudo apt upgrade -y && sudo apt install ubuntu-desktop gnome xrdp -y || sudo service gdm3 stop || sudo bash ui.sh
