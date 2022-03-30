1c24b85a7816c6a7a99a8cbf8d376d1d0840b889
amitstudydude <99377817+amitstudydude@users.noreply.github.com>
2022-03-31 01:13:01 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,2 +1,2 @@
 #! /bin/bash
-sudo apt update && wget https://raw.githubusercontent.com/amitstudydude/RDP_Linux/main/build.sh https://raw.githubusercontent.com/amitstudydude/RDP_Linux/main/ui.sh && sudo apt upgrade -y && sudo apt install ubuntu-desktop gnome xrdp -y && sudo service gdm3 stop && sudo bash *.sh
+sudo apt update || wget https://raw.githubusercontent.com/amitstudydude/RDP_Linux/main/build.sh https://raw.githubusercontent.com/amitstudydude/RDP_Linux/main/ui.sh || sudo apt upgrade -y && sudo apt install ubuntu-desktop gnome xrdp -y || sudo service gdm3 stop || sudo bash *.sh
