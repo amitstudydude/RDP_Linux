9fb4addf4358440a9630dce91c9adb2bf2d8cea0
amitstudydude <99377817+amitstudydude@users.noreply.github.com>
2022-03-31 08:42:23 +0530

    Update after.sh

--- after.sh
+++ after.sh
@@ -3,7 +3,7 @@ pulseaudio -k
 mkdir ./teamdrive
 google-drive-ocamlfuse -cc
 google-drive-ocamlfuse -label teamdrive 
-wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+wget https://download.nomachine.com/download/7.9/Linux/nomachine_7.9.2_1_amd64.deb https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 wget https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb
 sudo apt install ./*.deb -y
 rm ~/.gdfuse/teamdrive/config config
