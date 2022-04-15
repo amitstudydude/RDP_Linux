b2eb2e97fd31de92cd49b6a503cde5c04918c50e
amitstudydude <99377817+amitstudydude@users.noreply.github.com>
2022-04-15 21:02:04 +0530

    Update Desktop.sh

--- Desktop.sh
+++ Desktop.sh
@@ -16,12 +16,15 @@ jobs:
     steps:
     - name: Creating User to Login
       run: |
-           sudo useradd -m ubuntu && sudo adduser ubuntu sudo && echo 'ubuntu:ubuntu' | sudo chpasswd || yes root | sudo passwd 
+           sudo useradd -m ubuntu && sudo adduser ubuntu sudo && echo 'ubuntu:ubuntu' | sudo chpasswd
      
       
     - name: Installing Desktop Environment (wait for 10 min)
       run: |
-           
+           echo "root:root" | chpasswd
+           echo "PasswordAuthentication yes" > /etc/ssh/sshd_config
+           echo "PermitUserEnvironment yes" >> /etc/ssh/sshd_config
+           echo "PermitRootLogin yes" >> /etc/ssh/sshd_config
            wget https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb
           
            sudo apt install --assume-yes --fix-broken ./*.deb 
@@ -33,7 +36,6 @@ jobs:
             
     - name: Start Chrome Remote Desktop
       run: |
-           sudo useradd -m user && sudo adduser user sudo && echo 'user:user' | sudo chpasswd || sudo adduser user chrome-remote-desktop
            ${{ github.event.inputs.auth }} -pin=123456
            sudo apt-get install xfce4 xfce4-goodies xfce4-terminal  firefox -y && sudo apt update && sudo apt upgrade -y
            sudo apt update && sudo apt upgrade -y && sudo apt install ubuntu-desktop gnome -y
