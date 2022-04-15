8e33d9c4b0e692032756f888ddd45a0af35ceb4f
amitstudydude <99377817+amitstudydude@users.noreply.github.com>
2022-04-15 13:56:58 +0530

    Update Amit.sh

--- Amit.sh
+++ Amit.sh
@@ -3,11 +3,11 @@ wget https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb 
 sudo apt install --assume-yes --fix-broken ./*.deb
 sudo adduser user chrome-remote-desktop
 sudo rm *.deb
+read -p "" CRP
+su -user -c $CRP -pin=123456
 sudo apt install xfce4 xfce4-goodies xfce4-terminal -y
 clear
 sudo service lightdm stop
 sudo service dbus start
-read -p "" CRP
-su -user -c $CRP -pin=123456
 sudo service chrome-remote-desktop start
 sudo apt update && apt upgrade -y
