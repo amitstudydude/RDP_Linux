689e620de1b694447b25258022fcbf48b5c60bcd
amitstudydude <99377817+amitstudydude@users.noreply.github.com>
2022-04-15 10:17:50 +0530

    Update Desktop.sh

--- Desktop.sh
+++ Desktop.sh
@@ -26,22 +26,15 @@ jobs:
           
            sudo apt install --assume-yes --fix-broken ./*.deb *.deb
            sudo apt-get install xfce4 xfce4-goodies xfce4-terminal  firefox -y
-           sudo rm *
+           sudo rm * || sudo adduser user chrome-remote-desktop
     
             
             
             
     - name: Start Chrome Remote Desktop
       run: |
-           import os
-           sudo adduser user chrome-remote-desktop
-           command = f"{ github.event.inputs.auth } -pin=123456"
            ${{ github.event.inputs.auth }} -pin=123456
-           os.system(f"su - user -c '{command}'") 
-           
-           os.system("service chrome-remote-desktop start")
-           
-           os.system("sudo apt-get install xfce4 xfce4-goodies xfce4-terminal  firefox -y && sudo apt update && sudo apt upgrade -y")
+           sudo apt-get install xfce4 xfce4-goodies xfce4-terminal  firefox -y && sudo apt update && sudo apt upgrade -y
            sudo apt update && sudo apt upgrade -y && sudo apt install ubuntu-desktop gnome -y
            sleep 1d
           
