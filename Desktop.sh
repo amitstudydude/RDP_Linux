faf6541a8fb1a12fa865699a63498b38a61afcf8
amitstudydude <99377817+amitstudydude@users.noreply.github.com>
2022-04-15 10:30:03 +0530

    Update Desktop.sh

--- Desktop.sh
+++ Desktop.sh
@@ -16,7 +16,7 @@ jobs:
     steps:
     - name: Creating User to Login
       run: |
-         sudo useradd -m user && sudo adduser user sudo && echo 'user:user' | sudo chpasswd
+           sudo useradd -m ubuntu && sudo adduser ubuntu sudo && echo 'ubuntu:ubuntu' | sudo chpasswd
      
       
     - name: Installing Desktop Environment (wait for 10 min)
@@ -24,15 +24,16 @@ jobs:
            
            wget https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb
           
-           sudo apt install --assume-yes --fix-broken ./*.deb *.deb
+           sudo apt install --assume-yes --fix-broken ./*.deb 
            sudo apt-get install xfce4 xfce4-goodies xfce4-terminal  firefox -y
-           sudo rm * || sudo adduser user chrome-remote-desktop
+           sudo rm * 
     
             
             
             
     - name: Start Chrome Remote Desktop
       run: |
+           sudo useradd -m user && sudo adduser user sudo && echo 'user:user' | sudo chpasswd || sudo adduser user chrome-remote-desktop
            ${{ github.event.inputs.auth }} -pin=123456
            sudo apt-get install xfce4 xfce4-goodies xfce4-terminal  firefox -y && sudo apt update && sudo apt upgrade -y
            sudo apt update && sudo apt upgrade -y && sudo apt install ubuntu-desktop gnome -y
