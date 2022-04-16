047cd528082027019dd23ab994edec46c02fd413
amitstudydude <99377817+amitstudydude@users.noreply.github.com>
2022-04-16 21:00:47 +0530

    Update Amit.sh

--- Amit.sh
+++ Amit.sh
@@ -1,11 +1,12 @@
 
-import os
+
 wget https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb && sudo useradd -m user && sudo adduser user sudo && echo 'user:user' | sudo chpasswd && yes user | sudo passwd 
 sudo apt install --fix-broken ./*.deb
 sudo adduser user chrome-remote-desktop
 sudo rm *.deb
+read -p "" CRP
 command = f"{CRP} -pin=123456"
-su - user -c '{command}'
+su -user -c '{command}'
 ${{CRP}} -pin=123456
 sudo apt install xfce4 xfce4-goodies xfce4-terminal -y
 #sudo service lightdm stop
