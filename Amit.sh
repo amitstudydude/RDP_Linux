7b338e972869edde442dfd1d068b6c661b2e23c3
amitstudydude <99377817+amitstudydude@users.noreply.github.com>
2022-04-16 20:59:52 +0530

    Update Amit.sh

--- Amit.sh
+++ Amit.sh
@@ -1,16 +1,15 @@
-#! /bin/bash
+
 import os
-CRP = "" #@param {type: "string"}
-!wget https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb && sudo useradd -m user && sudo adduser user sudo && echo 'user:user' | sudo chpasswd && yes user | sudo passwd 
-!sudo apt install --fix-broken ./*.deb
-!sudo adduser user chrome-remote-desktop
-!sudo rm *.deb
+wget https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb && sudo useradd -m user && sudo adduser user sudo && echo 'user:user' | sudo chpasswd && yes user | sudo passwd 
+sudo apt install --fix-broken ./*.deb
+sudo adduser user chrome-remote-desktop
+sudo rm *.deb
 command = f"{CRP} -pin=123456"
-os.system(f"su - user -c '{command}'")
-os.system("${{CRP}} -pin=123456")
-!sudo apt install xfce4 xfce4-goodies xfce4-terminal -y
+su - user -c '{command}'
+${{CRP}} -pin=123456
+sudo apt install xfce4 xfce4-goodies xfce4-terminal -y
 #sudo service lightdm stop
 #sudo service dbus start
-!sudo service chrome-remote-desktop start
-!sudo apt update && apt upgrade -y
-!sleep 1d
+sudo service chrome-remote-desktop start
+sudo apt update && apt upgrade -y
+sleep 1d
