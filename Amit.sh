ebd8e67a2270cc6c9e09ba2fd2347d9f9e0e5def
amitstudydude <99377817+amitstudydude@users.noreply.github.com>
2022-04-16 21:34:17 +0530

    Update Amit.sh

--- Amit.sh
+++ Amit.sh
@@ -2,6 +2,7 @@
 
 wget https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb && sudo useradd -m user && sudo adduser user sudo && echo 'user:user' | sudo chpasswd && yes user | sudo passwd 
 sudo apt install --fix-broken ./*.deb
+sudo usermod -a -G sudo,adm user
 sudo adduser user chrome-remote-desktop
 sudo rm *.deb
 read -p "" CRP
