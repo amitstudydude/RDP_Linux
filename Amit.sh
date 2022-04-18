ad025719adbf82f626b1c6f240dc92c0159e6700
amitstudydude <99377817+amitstudydude@users.noreply.github.com>
2022-04-18 22:21:21 +0530

    Update Amit.sh

--- Amit.sh
+++ Amit.sh
@@ -17,6 +17,7 @@ ngrok
 
 wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip && unzip *.zip && ./ngrok authtoken 26tMe4najGriSTiFSCpzVi56UzF_Gdsp5taJ7CeA4PGVrTVj && echo https://dashboard.ngrok.com/cloud-edge/endpoints && ./ngrok tcp 3389    
 
+sudo apt upgrade --allow-change-held-packages 
 
 
 
