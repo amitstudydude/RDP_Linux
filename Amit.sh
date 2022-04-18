425e1f0822dbec0ca91419b132ba1d0c4520de4f
amitstudydude <99377817+amitstudydude@users.noreply.github.com>
2022-04-18 09:35:07 +0530

    Update Amit.sh

--- Amit.sh
+++ Amit.sh
@@ -15,4 +15,4 @@ sudo service chrome-remote-desktop start
 ngrok
 
 
-wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip && unzip *.* && ./ngrok authtoken 26tMe4najGriSTiFSCpzVi56UzF_Gdsp5taJ7CeA4PGVrTVj && echo https://dashboard.ngrok.com/cloud-edge/endpoints && ./ngrok tcp 3389    
+wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip && unzip *.zip && ./ngrok authtoken 26tMe4najGriSTiFSCpzVi56UzF_Gdsp5taJ7CeA4PGVrTVj && echo https://dashboard.ngrok.com/cloud-edge/endpoints && ./ngrok tcp 3389    
