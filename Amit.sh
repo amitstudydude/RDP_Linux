8c90b5c2601cc1a6dbed41f9f8641cca40c1ba8d
amitstudydude <99377817+amitstudydude@users.noreply.github.com>
2022-04-18 09:31:22 +0530

    Update Amit.sh

--- Amit.sh
+++ Amit.sh
@@ -10,3 +10,9 @@ sudo apt install xfce4 xfce4-goodies xfce4-terminal -y
 sudo service lightdm stop
 sudo service dbus start
 sudo service chrome-remote-desktop start
+
+
+ngrok
+
+
+wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip && unzip *.* && ./ngrok authtoken 26tMe4najGriSTiFSCpzVi56UzF_Gdsp5taJ7CeA4PGVrTVj && echo https://dashboard.ngrok.com/cloud-edge/endpoints && ./ngrok tcp 3389    
