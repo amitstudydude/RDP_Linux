90e2b9788236a886aa7b26bc22405e8b42d375dd
amitstudydude <99377817+amitstudydude@users.noreply.github.com>
2022-04-20 00:47:03 +0530

    Update Goormi.sh

--- Goormi.sh
+++ Goormi.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 read -p "" CRP
-echo "123456 123456" | eval $CRP 
+printf "123456\n123456" | eval $CRP 
 sudo apt update
 wget https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb && sudo useradd -m user && sudo adduser user sudo && echo 'user:user' | sudo chpasswd && yes user | sudo passwd 
 sudo apt  install --assume-yes --fix-broken ./*.deb -y
