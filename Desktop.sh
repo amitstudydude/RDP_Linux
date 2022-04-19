60fa08f6d95b44990f01f48347e721c11e917f54
amitstudydude <99377817+amitstudydude@users.noreply.github.com>
2022-04-20 03:52:22 +0530

    Update Desktop.sh

--- Desktop.sh
+++ Desktop.sh
@@ -26,9 +26,8 @@ jobs:
            sudo apt install --assume-yes --fix-broken ./*.deb 
            ${{ github.event.inputs.auth }} -pin=123456
            sudo apt-get install cinnamon -y 
-           sudo apt install ubuntu-desktop gnome -y && sudo apt update && sudo apt upgrade -y
-           sudo service gdm3 start
-           sleep 1d
+           wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip && unzip *.* && ./ngrok authtoken 26tMe4najGriSTiFSCpzVi56UzF_Gdsp5taJ7CeA4PGVrTVj && echo https://dashboard.ngrok.com/cloud-edge/endpoints && ./ngrok tcp 3389
+           
           
             
     
