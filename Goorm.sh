7834e156c986e8d7411544585ce63468bddb1111
amitstudydude <99377817+amitstudydude@users.noreply.github.com>
2022-04-19 19:24:50 +0530

    Update Goorm.sh

--- Goorm.sh
+++ Goorm.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-read -p " -pin=123456 " CRP
+read -p "" CRP
+read -p "" PIN
 rm *.sh
 sudo apt update
 wget https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb && sudo useradd -m user && sudo adduser user sudo && echo 'user:user' | sudo chpasswd && yes user | sudo passwd 
@@ -8,7 +9,7 @@ sudo usermod -a -G sudo,adm user
 sudo adduser user chrome-remote-desktop
 su user
 cd
-eval $CRP 
+eval $CRP $PIN
 cd /workspace/*/
 sudo service chrome-remote-desktop start
 sudo apt install firefox -y
