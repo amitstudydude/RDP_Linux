f8d542c0d4ae8defd888388c7ada09814df965f9
amitstudydude <99377817+amitstudydude@users.noreply.github.com>
2022-04-19 21:55:19 +0530

    Update Goorm.sh

--- Goorm.sh
+++ Goorm.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 #read -p "" CRP
+rm *.sh
+wget https://raw.githubusercontent.com/amitstudydude/RDP_Linux/main/Goorm.sh
 eval $CRP --pin=123456
 sudo apt update
 wget https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb && sudo useradd -m user && sudo adduser user sudo && echo 'user:user' | sudo chpasswd && yes user | sudo passwd 
