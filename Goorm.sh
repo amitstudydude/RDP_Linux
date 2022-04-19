030e590ff43089e2d754dd2c2b824c080c612230
amitstudydude <99377817+amitstudydude@users.noreply.github.com>
2022-04-20 00:28:50 +0530

    Update Goorm.sh

--- Goorm.sh
+++ Goorm.sh
@@ -3,7 +3,7 @@
 rm *.sh *.deb
 echo "123456 123456" | eval $CRP 
 sudo apt update
-wget https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb && sudo useradd -m user && sudo adduser user sudo && echo 'user:user' | sudo chpasswd && yes user | sudo passwd 
+wget https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb && sudo useradd -m user && sudo adduser user sudo && echo 'user:user' | sudo chpasswd && yes user | sudo passwd 
 sudo apt  install --assume-yes --fix-broken ./*.deb -y
 sudo usermod -a -G sudo,adm user
 sudo adduser user chrome-remote-desktop
