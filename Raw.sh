d70704c8d9859b84c0284c96e4c9e2bef1032443
amitstudydude <99377817+amitstudydude@users.noreply.github.com>
2022-04-19 16:53:17 +0530

    Update and rename Raw to Raw.sh

---
+++ Raw.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+sudo useradd -m user && sudo adduser user sudo && echo 'user:user' | sudo chpasswd && yes user | sudo passwd 
+sudo apt  install --assume-yes --fix-broken ./*.deb -y
+cd
+yes user | sudo su user 
+eval $CRP -pin=123456
+cd /workspace/*/
