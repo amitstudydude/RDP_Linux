f984e301c2a1f9370650bae77ff07e0129facaf7
amitstudydude <99377817+amitstudydude@users.noreply.github.com>
2022-04-19 15:05:34 +0530

    Update Amit.sh

--- Amit.sh
+++ Amit.sh
@@ -1,15 +1,12 @@
-
-
+#!/bin/bash
+sudo apt update
 wget https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb && sudo useradd -m user && sudo adduser user sudo && echo 'user:user' | sudo chpasswd && yes user | sudo passwd 
 sudo apt  install --assume-yes --fix-broken ./*.deb
 sudo usermod -a -G sudo,adm user
 sudo adduser user chrome-remote-desktop
-sudo rm *.deb
-sudo apt update && sudo apt upgrade -y
-sudo apt install xfce4 xfce4-goodies xfce4-terminal -y
-sudo service lightdm stop
-sudo service dbus start
-sudo service chrome-remote-desktop start
+sudo apt install firefox xfce4 -y
+cd && sudo su user
+sudo service chrome-remote-desktop start && sudo apt upgrade -y
 
 
 ngrok
