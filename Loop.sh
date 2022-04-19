7525f0b7e3e820ae0fae0cca7ddeb82ddfdaf531
amitstudydude <99377817+amitstudydude@users.noreply.github.com>
2022-04-20 00:04:39 +0530

    Update and rename loop.sh to Loop.sh

---
+++ Loop.sh
@@ -0,0 +1,5 @@
+#! /bin/bash
+rm *.sh
+wget https://raw.githubusercontent.com/amitstudydude/RDP_Linux/main/Loop.sh
+bash *.sh
+clear
--- loop.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/bash
-rm *.sh
-wget https://raw.githubusercontent.com/amitstudydude/RDP_Linux/main/loop.sh
-bash *.sh
-clear
