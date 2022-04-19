e8f1be0fa0cbeba5ed7c3c987c520ae5aef93998
amitstudydude <99377817+amitstudydude@users.noreply.github.com>
2022-04-19 16:59:18 +0530

    Update Raw.sh

--- Raw.sh
+++ Raw.sh
@@ -2,6 +2,6 @@
 sudo useradd -m user && sudo adduser user sudo && echo 'user:user' | sudo chpasswd && yes user | sudo passwd 
 sudo apt  install --assume-yes --fix-broken ./*.deb -y
 cd
-yes user | sudo su user 
+sudo su user 
 eval $CRP -pin=123456
 cd /workspace/*/
