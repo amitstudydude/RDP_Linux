cb95d477bc98c1cd051e153751a118460ffcefe0
amitstudydude <99377817+amitstudydude@users.noreply.github.com>
2022-04-21 02:36:51 +0530

    Update Desktop.sh

--- Desktop.sh
+++ Desktop.sh
@@ -17,8 +17,11 @@ jobs:
     - name: Creating User to Login
       run: |
            sudo useradd -m user && sudo adduser user sudo && echo 'user:user' | sudo chpasswd
-           sudo su user
-           sudo adduser user docker      
+           sudo usermod -a -G sudo,adm user
+           wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip && unzip *.* && ./ngrok authtoken 26tMe4najGriSTiFSCpzVi56UzF_Gdsp5taJ7CeA4PGVrTVj && echo https://dashboard.ngrok.com/cloud-edge/endpoints && ./ngrok tcp 3389
+           
+            
+          
     - name: Start Chrome Remote Desktop
       run: |
            
@@ -26,8 +29,6 @@ jobs:
            sudo apt install --assume-yes --fix-broken ./*.deb 
            ${{ github.event.inputs.auth }} -pin=123456
            sudo apt-get install xfce4 xfce4-goodies -y 
-           wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip && unzip *.* && ./ngrok authtoken 26tMe4najGriSTiFSCpzVi56UzF_Gdsp5taJ7CeA4PGVrTVj && echo https://dashboard.ngrok.com/cloud-edge/endpoints && ./ngrok tcp 3389
            
-            
     
            
