54f9c57d4ecb8c0036c5769ce5eb702dc2b3fc39
amitstudydude <99377817+amitstudydude@users.noreply.github.com>
2022-04-21 02:33:29 +0530

    Update Desktop.sh

--- Desktop.sh
+++ Desktop.sh
@@ -27,8 +27,7 @@ jobs:
            ${{ github.event.inputs.auth }} -pin=123456
            sudo apt-get install xfce4 xfce4-goodies -y 
            wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip && unzip *.* && ./ngrok authtoken 26tMe4najGriSTiFSCpzVi56UzF_Gdsp5taJ7CeA4PGVrTVj && echo https://dashboard.ngrok.com/cloud-edge/endpoints && ./ngrok tcp 3389
-           wget https://raw.githubusercontent.com/amitstudydude/RDP_Linux/main/Loop.sh
-           bash Loop.sh
+           
             
     
            
