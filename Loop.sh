6da58b2f0b50d720bad12c572d18a2a696334074
amitstudydude <99377817+amitstudydude@users.noreply.github.com>
2022-04-21 10:42:29 +0530

    Update Loop.sh

--- Loop.sh
+++ Loop.sh
@@ -1,6 +1,8 @@
 #! /bin/bash
 clear
 rm Loop.sh
+printf "123456\n123456" | eval $CRP 
+sudo service chrome-remote-desktop start
 wget https://raw.githubusercontent.com/amitstudydude/RDP_Linux/main/Loop.sh
 bash Loop.sh
 clear
