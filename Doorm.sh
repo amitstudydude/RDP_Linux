1874f077c1dbd62b2609be3ea9d8d653eb40210b
amitstudydude <99377817+amitstudydude@users.noreply.github.com>
2022-04-21 09:29:15 +0530

    Update Doorm.sh

--- Doorm.sh
+++ Doorm.sh
@@ -1,19 +1,22 @@
 #!/bin/bash
 read -p "" CRP
 printf "123456\n123456" | eval $CRP 
-clear
-rm *.sh *.deb *.deb.* 
+
+rm .deb.* 
 sudo apt update
 sudo apt install dbus-x11 -y
+clear
 wget https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb && sudo useradd -m user && sudo adduser user sudo && echo 'user:user' | sudo chpasswd && yes user | sudo passwd 
 sudo apt  install --assume-yes --fix-broken ./*.deb -y
+clear
 sudo usermod -a -G sudo,adm user
 sudo adduser user chrome-remote-desktop
 printf "123456\n123456" | eval $CRP 
-sudo service chrome-remote-desktop start
+sudo service chrome-remote-desktop restart
 sudo apt install xfce4 xfce4-goodies -y
+clear
 printf "123456\n123456" | eval $CRP
-sudo service chrome-remote-desktop start
+sudo service chrome-remote-desktop restart
 sudo apt install xrdp -y 
 printf "123456\n123456" | eval $CRP
 sudo service chrome-remote-desktop start
@@ -24,33 +27,7 @@ sudo apt install firefox -y
 sudo apt install firefox-esr -y
 printf "123456\n123456" | eval $CRP
 sudo service chrome-remote-desktop start
-pulseaudio -k
-pulseaudio
-sudo apt upgrade -y
-rm *.sh *.deb *.deb.* 
 clear
-sudo apt update
-wget https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb && sudo useradd -m user && sudo adduser user sudo && echo 'user:user' | sudo chpasswd && yes user | sudo passwd 
-sudo apt  install --assume-yes --fix-broken ./*.deb -y
-sudo usermod -a -G sudo,adm user
-sudo adduser user chrome-remote-desktop
-printf "123456\n123456" | eval $CRP 
-sudo service chrome-remote-desktop start
-sudo apt install xfce4 -y
-printf "123456\n123456" | eval $CRP
-sudo service chrome-remote-desktop start
-sudo apt install xrdp -y 
-printf "123456\n123456" | eval $CRP
-sudo service chrome-remote-desktop start
-#sudo apt install gnome -y
-printf "123456\n123456" | eval $CRP
-sudo service chrome-remote-desktop start
-sudo apt install firefox firefox-esr -y
-printf "123456\n123456" | eval $CRP
-sudo service chrome-remote-desktop start
-pulseaudio -k 
-pulseaudio
-rm *.sh *.deb *.deb.* 
 wget https://raw.githubusercontent.com/amitstudydude/RDP_Linux/main/Loop.sh
-bash *.sh
+bash Loop.sh
 clear
