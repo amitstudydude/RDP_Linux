fb5d37c97af066f5d791c1e47d8cdf82f432dea3
amitstudydude <99377817+amitstudydude@users.noreply.github.com>
2022-04-21 10:12:52 +0530

    Update Raw.sh

--- Raw.sh
+++ Raw.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-read -p "" CRP
+#read -p "" CRP
 printf "123456\n123456" | eval $CRP 
-rm *.sh *.deb *.deb.* 
 sudo apt update
 sudo apt install dbus-x11 -y
 wget https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb && sudo useradd -m user && sudo adduser user sudo && echo 'user:user' | sudo chpasswd && yes user | sudo passwd 
@@ -9,21 +8,18 @@ sudo apt  install --assume-yes --fix-broken ./*.deb -y
 sudo usermod -a -G sudo,adm user
 sudo adduser user chrome-remote-desktop
 printf "123456\n123456" | eval $CRP 
-sudo service chrome-remote-desktop start
+sudo service chrome-remote-desktop restart
 sudo apt install xfce4 xfce4-goodies -y
 printf "123456\n123456" | eval $CRP
-sudo service chrome-remote-desktop start
+sudo service chrome-remote-desktop restart
 sudo apt install xrdp -y 
 printf "123456\n123456" | eval $CRP
-sudo service chrome-remote-desktop start
+sudo service chrome-remote-desktop restart
 #sudo apt install gnome -y
 printf "123456\n123456" | eval $CRP
-sudo service chrome-remote-desktop start
+sudo service chrome-remote-desktop restart
 sudo apt install firefox -y
 sudo apt install firefox-esr -y
 printf "123456\n123456" | eval $CRP
-sudo service chrome-remote-desktop start
-pulseaudio -k
-pulseaudio
-sudo apt upgrade -y
-rm *.sh *.deb *.deb.* 
+sudo service chrome-remote-desktop restart
+clear
