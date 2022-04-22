bc50c0cadf0f2b762742492cbaf7ba1c67921261
amitstudydude <99377817+amitstudydude@users.noreply.github.com>
2022-04-22 14:13:04 +0530

    Update IPaddress.sh

--- IPaddress.sh
+++ IPaddress.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 gcloud cloud-shell ssh
-ip="ssh -i /home/$(whoami)/.ssh/id_rsa $(whoami)@$(curl ifconfig.me)"
+ip="ssh -i /home/$(whoami)/.ssh/id_rsa -p 6000 $(whoami)@$(curl ifconfig.me)"
 $ip
 
