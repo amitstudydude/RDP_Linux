14709c4e3f5865311b01f098c984942d662096e1
amitstudydude <99377817+amitstudydude@users.noreply.github.com>
2022-04-22 11:57:16 +0530

    Update Doorm.sh

--- Doorm.sh
+++ Doorm.sh
@@ -4,7 +4,7 @@ printf "123456\n123456" | eval $CRP
 
 rm .deb.* *.deb
 sudo apt update
-sudo apt install dbus-x11 -y
+sudo apt install dbus-x11 p7zip-full -y
 clear
 wget https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb && sudo useradd -m user && sudo adduser user sudo && echo 'user:user' | sudo chpasswd && yes user | sudo passwd 
 sudo apt  install --assume-yes --fix-broken ./*.deb -y
