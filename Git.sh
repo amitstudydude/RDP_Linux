a354e99f96ec355a0bfadfbb98e497df5a446635
amitstudydude <99377817+amitstudydude@users.noreply.github.com>
2022-04-23 01:46:09 +0530

    Update Git.sh

--- Git.sh
+++ Git.sh
@@ -2,7 +2,7 @@
 wget https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb
 sudo apt install --assume-yes --fix-broken ./*.deb && rm *.deb
 ${{ github.event.inputs.auth }} -pin=123456
-sudo apt install firefox gnome-core -y
+sudo apt install firefox xfce4 -y
 bash *.sh
 - name: Create SSH Access
       uses: mxschmitt/action-tmate@v3
