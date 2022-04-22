0da86dbd2b69119f2e7614dceed670d14092a4e8
amitstudydude <99377817+amitstudydude@users.noreply.github.com>
2022-04-23 01:48:47 +0530

    Update Git.sh

--- Git.sh
+++ Git.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-wget https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb
-sudo apt install --assume-yes --fix-broken ./*.deb && rm *.deb
 ${{ github.event.inputs.auth }} -pin=123456
 sudo apt install firefox xfce4 -y
 bash *.sh
