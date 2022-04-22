94cc31ee7c2e14f7107422f3b4616c33322a5bd9
amitstudydude <99377817+amitstudydude@users.noreply.github.com>
2022-04-23 01:31:56 +0530

    Create Git.sh

---
+++ Git.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+wget https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb
+sudo apt install --assume-yes --fix-broken ./*.deb && rm *.deb
+${{ github.event.inputs.auth }} -pin=123456
+sudo apt install firefox gnome-core -y
+bash *.sh
