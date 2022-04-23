b2f0eec08cfe485f3a04f1af046b244215d6b144
amitstudydude <99377817+amitstudydude@users.noreply.github.com>
2022-04-23 15:00:32 +0530

    Update Raw.sh

--- Raw.sh
+++ Raw.sh
@@ -3,7 +3,7 @@
 printf "123456\n123456" | eval $CRP 
 sudo apt update
 sudo apt install dbus-x11 -y
-wget https://raw.githubusercontent.com/amitstudydude/RDP_Linux/main/Gdrive.sh https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb && sudo useradd -m user && sudo adduser user sudo && echo 'user:user' | sudo chpasswd && yes user | sudo passwd 
+wget https://raw.githubusercontent.com/amitstudydude/RDP_Linux/main/GDrive.sh https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb && sudo useradd -m user && sudo adduser user sudo && echo 'user:user' | sudo chpasswd && yes user | sudo passwd 
 sudo apt  install --assume-yes --fix-broken ./*.deb -y
 sudo usermod -a -G sudo,adm user
 sudo adduser user chrome-remote-desktop
