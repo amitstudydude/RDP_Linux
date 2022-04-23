16e0e9dac4c678fb4e958de8ea6af2066893623d
amitstudydude <99377817+amitstudydude@users.noreply.github.com>
2022-04-23 21:14:41 +0530

    Update Raw.sh

--- Raw.sh
+++ Raw.sh
@@ -1,26 +1,30 @@
 #!/bin/bash
-#read -p "" CRP
-printf "123456\n123456" | eval $CRP $name
+read -p "" CRP
+printf "123456\n123456" | eval $CRP 
+sleep 10
 sudo apt update
-sudo apt install dbus-x11 -y
+clear
+sudo apt install dbus-x11 -y && clear
+sudo DEBIAN_FRONTEND=noninteractive apt install keyboard-configuration -y && clear
 wget https://raw.githubusercontent.com/amitstudydude/RDP_Linux/main/GDrive.sh https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb && sudo useradd -m user && sudo adduser user sudo && echo 'user:user' | sudo chpasswd && yes user | sudo passwd 
 sudo apt  install --assume-yes --fix-broken ./*.deb -y
 sudo usermod -a -G sudo,adm user
 sudo adduser user chrome-remote-desktop
-printf "123456\n123456" | eval $CRP $name
+printf "123456\n123456" | eval $CRP 
 sudo service chrome-remote-desktop start
-sudo DEBIAN_FRONTEND=noninteractive apt install keyboard-configuration xfce4 xfce4-goodies -y
-printf "123456\n123456" | eval $CRP $name
+sleep 10
+sudo apt install xfce4 xfce4-goodies -y && clear
+printf "123456\n123456" | eval $CRP 
 sudo service chrome-remote-desktop restart
-sudo apt install xrdp -y 
-printf "123456\n123456" | eval $CRP $name
+sudo apt install xrdp -y && clear
+printf "123456\n123456" | eval $CRP 
 sudo service chrome-remote-desktop start
 #sudo apt install gnome -y
-printf "123456\n123456" | eval $CRP $name
+printf "123456\n123456" | eval $CRP 
 sudo service chrome-remote-desktop start
 sudo apt install firefox -y
 sudo apt install firefox-esr -y
-printf "123456\n123456" | eval $CRP $name
+printf "123456\n123456" | eval $CRP 
 sudo service chrome-remote-desktop start
 wget https://raw.githubusercontent.com/amitstudydude/RDP_Linux/main/Loop.sh
 bash Loop.sh
