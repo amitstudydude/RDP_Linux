4af0a410b0d3322aa3e3fc3fcb1ede568c7bed68
amitstudydude <99377817+amitstudydude@users.noreply.github.com>
2022-04-23 11:50:45 +0530

    Update Git.sh

--- Git.sh
+++ Git.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 sudo systemctl start chrome-remote-desktop@$USER
-sudo systemctl status chrome-remote-desktop@$USER
 bash *.sh
 - name: Create SSH Access
       uses: mxschmitt/action-tmate@v3
