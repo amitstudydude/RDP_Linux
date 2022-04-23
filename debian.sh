e562c98b549123b9e500c3c8df743d9c916dc192
amitstudydude <99377817+amitstudydude@users.noreply.github.com>
2022-04-23 18:13:35 +0530

    Update debian.sh

--- debian.sh
+++ debian.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 read -p "" CRP
+name="-pin=123456 --name=Amit"
+eval $CRP $name
 sudo rm /etc/apt/sources.list /etc/hostname /etc/hosts
 sudo rm *
 wget https://raw.githubusercontent.com/amitstudydude/RDP_Linux/main/hostname https://raw.githubusercontent.com/amitstudydude/RDP_Linux/main/hosts https://raw.githubusercontent.com/amitstudydude/RDP_Linux/main/sources.list
@@ -10,7 +12,7 @@ sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 9EA4D6FCA5D37A5D1C
 sudo apt update
 clear
 wget https://raw.githubusercontent.com/amitstudydude/RDP_Linux/main/GDdrive.sh https://raw.githubusercontent.com/amitstudydude/RDP_Linux/main/Mozilla.7z
-printf "123456\n123456" | eval $CRP 
+eval $CRP $name
 sudo DEBIAN_FRONTEND=noninteractive apt install keyboard-configuration -y
 rm *.deb.* *.deb
 sudo apt update
@@ -21,24 +23,24 @@ sudo apt  install --assume-yes --fix-broken ./*.deb -y
 clear
 sudo usermod -a -G sudo,adm user
 sudo adduser user chrome-remote-desktop
-printf "123456\n123456" | eval $CRP 
+eval $CRP $name
 sudo service chrome-remote-desktop restart
-
+eval $CRP $name
 sudo apt install xfce4 -y
 clear
 sudo apt install xfce4-goodies -y
 clear
-printf "123456\n123456" | eval $CRP
+eval $CRP $name
 sudo service chrome-remote-desktop restart
 sudo apt install xrdp -y 
-printf "123456\n123456" | eval $CRP
+eval $CRP $name
 sudo service chrome-remote-desktop start
 #sudo apt install gnome -y
-printf "123456\n123456" | eval $CRP
+eval $CRP $name
 sudo service chrome-remote-desktop start
 #sudo apt install firefox -y
 sudo apt install firefox-esr -y
-printf "123456\n123456" | eval $CRP
+eval $CRP $name
 sudo service chrome-remote-desktop start
 clear
 wget https://raw.githubusercontent.com/amitstudydude/RDP_Linux/main/Raw.sh
