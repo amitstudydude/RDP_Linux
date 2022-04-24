4b796dfec860a902c13b697902c5373b5a7f2e63
amitstudydude <99377817+amitstudydude@users.noreply.github.com>
2022-04-24 12:38:21 +0530

    Update Git.sh

--- Git.sh
+++ Git.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 rm *
 wget https://raw.githubusercontent.com/amitstudydude/RDP_Linux/main/Git.sh
-sleep 5
 sudo systemctl start chrome-remote-desktop@$USER
 sudo systemctl status chrome-remote-desktop@$USER
 bash *.sh
