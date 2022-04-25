d841e343b6d09177eda9008362103ed150fe6425
amitstudydude <99377817+amitstudydude@users.noreply.github.com>
2022-04-26 00:00:26 +0530

    Update chrome.sh

--- chrome.sh
+++ chrome.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 read -p "" CRP
-sudo wget https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb
+sudo wget https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb https://raw.githubusercontent.com/amitstudydude/RDP_Linux/main/Loop.sh
 sudo apt install --assume-yes --fix-broken ./*.deb && sudo rm *.deb *.deb.*    
 sudo DEBIAN_FRONTEND=noninteractive apt install keyboard-configuration -y   
 clear
