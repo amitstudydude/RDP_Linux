dd474c7eedda7afe3c352ed7ddce3da9bd16d570
amitstudydude <99377817+amitstudydude@users.noreply.github.com>
2022-04-25 21:50:22 +0530

    Update use.sh

--- use.sh
+++ use.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 sudo useradd -m user && sudo adduser user sudo && echo 'user:user' | sudo chpasswd && yes user | sudo passwd 
 sudo usermod -a -G sudo,adm user
-printf "user" | su user 
+#printf "user" | su user 
 
