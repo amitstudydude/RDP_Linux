31ad815057787899f95806ff929d3489b84f9ae7
amitstudydude <99377817+amitstudydude@users.noreply.github.com>
2022-04-25 23:48:24 +0530

    Update Set-up.sh

--- Set-up.sh
+++ Set-up.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+apt update && apt install wget sudo -y
 wget https://raw.githubusercontent.com/amitstudydude/RDP_Linux/main/sources.list
 sudo cat sources.list >> /etc/apt/sources.list
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 9EA4D6FCA5D37A5D1CA9C09AAD5F235DF639B041
