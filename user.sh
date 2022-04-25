4e438b34ce5cfd4a9e929cd8c20395ba8ca1d355
amitstudydude <99377817+amitstudydude@users.noreply.github.com>
2022-04-25 21:18:50 +0530

    Update user.sh

--- user.sh
+++ user.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 sudo useradd -m user && sudo adduser user sudo && echo 'user:user' | sudo chpasswd && yes user | sudo passwd 
 sudo usermod -a -G sudo,adm user
-sudo user wget https://raw.githubusercontent.com/amitstudydude/RDP_Linux/main/chrome-remote-desktop.sh && sudo bash chrome-remote-desktop.sh
+su user 
+sudo wget https://raw.githubusercontent.com/amitstudydude/RDP_Linux/main/chrome-remote-desktop.sh && sudo bash chrome-remote-desktop.sh
 
 
