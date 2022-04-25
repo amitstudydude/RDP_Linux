d5bb7ed7e382a1c0c0ddccebd3f1ff5c059e06d5
amitstudydude <99377817+amitstudydude@users.noreply.github.com>
2022-04-26 00:18:14 +0530

    Update Docker.sh

--- Docker.sh
+++ Docker.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 #docker pull ubuntu
 #docker run -it ubuntu
-sudo useradd -m user && sudo adduser user sudo && echo 'user:user' | sudo chpasswd && yes user | sudo passwd 
-sudo usermod -a -G sudo,adm user
-apt update && apt install wget sudo -y
+apt update && apt install wget sudo -y 
+useradd -m user && sudo adduser user sudo && echo 'user:user' | sudo chpasswd && yes user | sudo passwd 
+usermod -a -G sudo,adm user
 wget https://raw.githubusercontent.com/amitstudydude/RDP_Linux/main/sources.list https://raw.githubusercontent.com/amitstudydude/RDP_Linux/main/c.sh
-sudo cat sources.list >> /etc/apt/sources.list
-sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 9EA4D6FCA5D37A5D1CA9C09AAD5F235DF639B041
-sudo apt update
+cat sources.list >> /etc/apt/sources.list
+apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 9EA4D6FCA5D37A5D1CA9C09AAD5F235DF639B041
+apt update
 clear
-sudo DEBIAN_FRONTEND=noninteractive apt install keyboard-configuration -y   
+DEBIAN_FRONTEND=noninteractive apt install keyboard-configuration -y   
 clear
-sudo apt install dbus-x11 -y
+apt install dbus-x11 -y
 clear
-sudo rm *
+rm *
