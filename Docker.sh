9315406fc8e4dd7a5c10e11cd129225239391a44
amitstudydude <99377817+amitstudydude@users.noreply.github.com>
2022-04-25 23:58:13 +0530

    Update Docker.sh

--- Docker.sh
+++ Docker.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-docker pull ubuntu
-docker run -it ubuntu
+#docker pull ubuntu
+#docker run -it ubuntu
 sudo useradd -m user && sudo adduser user sudo && echo 'user:user' | sudo chpasswd && yes user | sudo passwd 
 sudo usermod -a -G sudo,adm user
 apt update && apt install wget sudo -y
-wget https://raw.githubusercontent.com/amitstudydude/RDP_Linux/main/sources.list
+wget https://raw.githubusercontent.com/amitstudydude/RDP_Linux/main/sources.list https://raw.githubusercontent.com/amitstudydude/RDP_Linux/main/chrome.sh
 sudo cat sources.list >> /etc/apt/sources.list
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 9EA4D6FCA5D37A5D1CA9C09AAD5F235DF639B041
 sudo apt update
@@ -19,3 +19,4 @@ sudo apt install ubuntu-desktop -y
 clear
 sudo apt install firefox -y
 clear
+printf "user" | su user
