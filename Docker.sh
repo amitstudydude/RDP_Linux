0a8ccc00431934a90423130267eb6f423839fe95
amitstudydude <99377817+amitstudydude@users.noreply.github.com>
2022-04-26 00:59:35 +0530

    Update Docker.sh

--- Docker.sh
+++ Docker.sh
@@ -4,13 +4,19 @@
 apt update && apt install wget sudo -y 
 useradd -m user && sudo adduser user sudo && echo 'user:user' | sudo chpasswd && yes user | sudo passwd 
 usermod -a -G sudo,adm user
-wget https://raw.githubusercontent.com/amitstudydude/RDP_Linux/main/sources.list https://raw.githubusercontent.com/amitstudydude/RDP_Linux/main/c.sh
-cat sources.list >> /etc/apt/sources.list
-apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 9EA4D6FCA5D37A5D1CA9C09AAD5F235DF639B041
-apt update
-clear
 DEBIAN_FRONTEND=noninteractive apt install keyboard-configuration -y   
 clear
 apt install dbus-x11 -y
 clear
-rm *
+sudo wget https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb https://raw.githubusercontent.com/amitstudydude/RDP_Linux/main/Loop.sh
+sudo apt install --assume-yes --fix-broken ./*.deb  
+sudo DEBIAN_FRONTEND=noninteractive apt install keyboard-configuration -y   
+clear
+sudo apt install dbus-x11 -y    
+clear
+#sudo apt install xfce4 xfce4-goodies -y
+clear
+sudo apt install ubuntu-desktop -y
+clear
+sudo apt install firefox -y
+clear
