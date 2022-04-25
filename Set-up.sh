ef6515e79162fc0bde46e524f0f7e43ce0b7417d
amitstudydude <99377817+amitstudydude@users.noreply.github.com>
2022-04-25 23:51:48 +0530

    Update Set-up.sh

--- Set-up.sh
+++ Set-up.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 apt update && apt install wget sudo -y
-wget https://raw.githubusercontent.com/amitstudydude/RDP_Linux/main/sources.list
+wget https://raw.githubusercontent.com/amitstudydude/RDP_Linux/main/sources.list https://raw.githubusercontent.com/amitstudydude/RDP_Linux/main/user.sh https://raw.githubusercontent.com/amitstudydude/RDP_Linux/main/chrome.sh
 sudo cat sources.list >> /etc/apt/sources.list
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 9EA4D6FCA5D37A5D1CA9C09AAD5F235DF639B041
 sudo apt update
@@ -8,4 +8,5 @@ clear
 #sudo apt install xfce4 xfce4-goodies -y
 sudo apt install ubuntu-desktop -y
 sudo apt install firefox -y
+bash chrome.sh
 clear
