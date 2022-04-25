78720d08f9598ce754da120d4d14d3605d61dd94
amitstudydude <99377817+amitstudydude@users.noreply.github.com>
2022-04-25 23:54:30 +0530

    Update and rename Set-up.sh to Docker.sh

--- Docker.sh
+++ Docker.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
+docker pull ubuntu
+docker run -it ubuntu
+sudo useradd -m user && sudo adduser user sudo && echo 'user:user' | sudo chpasswd && yes user | sudo passwd 
+sudo usermod -a -G sudo,adm user
 apt update && apt install wget sudo -y
-wget https://raw.githubusercontent.com/amitstudydude/RDP_Linux/main/sources.list https://raw.githubusercontent.com/amitstudydude/RDP_Linux/main/user.sh https://raw.githubusercontent.com/amitstudydude/RDP_Linux/main/chrome.sh
+wget https://raw.githubusercontent.com/amitstudydude/RDP_Linux/main/sources.list
 sudo cat sources.list >> /etc/apt/sources.list
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 9EA4D6FCA5D37A5D1CA9C09AAD5F235DF639B041
 sudo apt update
 clear
 #sudo apt install xfce4 xfce4-goodies -y
+clear
 sudo apt install ubuntu-desktop -y
+clear
 sudo apt install firefox -y
-bash chrome.sh
 clear
