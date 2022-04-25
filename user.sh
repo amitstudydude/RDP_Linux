f56c7b01244506d0c25e9a0fa6ca3bbf32c12d01
amitstudydude <99377817+amitstudydude@users.noreply.github.com>
2022-04-25 23:58:34 +0530

    Delete user.sh

--- user.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-docker pull ubuntu
-docker run -it ubuntu
-sudo useradd -m user && sudo adduser user sudo && echo 'user:user' | sudo chpasswd && yes user | sudo passwd 
-sudo usermod -a -G sudo,adm user
-printf "user" | su user 
-
