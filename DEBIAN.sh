5c11d30d007383ccb25f2e48724a51dbb7cdbabb
amitstudydude <99377817+amitstudydude@users.noreply.github.com>
2022-04-26 01:48:07 +0530

    Update DEBIAN.sh

--- DEBIAN.sh
+++ DEBIAN.sh
@@ -5,7 +5,8 @@ sudo apt install --assume-yes --fix-broken ./*.deb && sudo rm *.deb *.deb.*
 eval DISPLAY= /opt/google/chrome-remote-desktop/start-host --code="$CRP" --redirect-url="https://remotedesktop.google.com/_/oauthredirect" --name=Linux --pin=123456
 clear
 wget  https://raw.githubusercontent.com/amitstudydude/RDP_Linux/main/sources.list
-sudo cat sources.list >> /etc/apt/sources.list
+sudo rm /etc/apt/sources.list
+sudo mv sources.list /etc/apt/
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 9EA4D6FCA5D37A5D1CA9C09AAD5F235DF639B041
 sudo apt update
 clear
