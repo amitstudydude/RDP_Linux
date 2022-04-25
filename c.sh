d06e463744ca225d232aed090dd6b58f6c17f169
amitstudydude <99377817+amitstudydude@users.noreply.github.com>
2022-04-26 00:31:09 +0530

    Update c.sh

--- c.sh
+++ c.sh
@@ -3,7 +3,6 @@ read -p "" CRP
 sudo wget https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb https://raw.githubusercontent.com/amitstudydude/RDP_Linux/main/Loop.sh
 sudo apt install --assume-yes --fix-broken ./*.deb && sudo rm *.deb *.deb.*    
 eval DISPLAY= /opt/google/chrome-remote-desktop/start-host --code="$CRP" --redirect-url="https://remotedesktop.google.com/_/oauthredirect" --name=Linux --pin=123456
-sudo service chrome-remote-desktop start
 clear
 sudo DEBIAN_FRONTEND=noninteractive apt install keyboard-configuration -y   
 clear
@@ -12,7 +11,6 @@ clear
 #sudo apt install xfce4 xfce4-goodies -y
 clear
 sudo apt install ubuntu-desktop -y
-sudo service chrome-remote-desktop restart
 clear
 sudo apt install firefox -y
 clear
