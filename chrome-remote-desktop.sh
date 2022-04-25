47ff67cea401a9732a7c3e0d4b0c3dec7d05efc2
amitstudydude <99377817+amitstudydude@users.noreply.github.com>
2022-04-25 20:51:25 +0530

    Update chrome-remote-desktop.sh

--- chrome-remote-desktop.sh
+++ chrome-remote-desktop.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 read -p "" CRP
-wget 
+wget wget https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb
+sudo apt install --assume-yes --fix-broken ./*.deb && rm *.deb *.deb.*          
 DISPLAY= /opt/google/chrome-remote-desktop/start-host --code="$CRP" --redirect-url="https://remotedesktop.google.com/_/oauthredirect" --name=Linux --pin=123456
