15bbd4210b1900be45ce7e591cb7a4e66f23ccec
amitstudydude <99377817+amitstudydude@users.noreply.github.com>
2022-04-25 21:26:26 +0530

    Update chrome.sh

--- chrome.sh
+++ chrome.sh
@@ -2,4 +2,4 @@
 read -p "" CRP
 sudo wget https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb
 sudo apt install --assume-yes --fix-broken ./*.deb && sudo rm *.deb *.deb.*          
-sudo DISPLAY= /opt/google/chrome-remote-desktop/start-host --code="$CRP" --redirect-url="https://remotedesktop.google.com/_/oauthredirect" --name=Linux --pin=123456
+eval DISPLAY= /opt/google/chrome-remote-desktop/start-host --code="$CRP" --redirect-url="https://remotedesktop.google.com/_/oauthredirect" --name=Linux --pin=123456
