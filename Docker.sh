c9e6523f36c65fc9674968f04c9aec252af34b2d
amitstudydude <99377817+amitstudydude@users.noreply.github.com>
2022-04-26 00:08:21 +0530

    Update Docker.sh

--- Docker.sh
+++ Docker.sh
@@ -4,7 +4,7 @@
 sudo useradd -m user && sudo adduser user sudo && echo 'user:user' | sudo chpasswd && yes user | sudo passwd 
 sudo usermod -a -G sudo,adm user
 apt update && apt install wget sudo -y
-wget https://raw.githubusercontent.com/amitstudydude/RDP_Linux/main/sources.list https://raw.githubusercontent.com/amitstudydude/RDP_Linux/main/chrome.sh
+wget https://raw.githubusercontent.com/amitstudydude/RDP_Linux/main/sources.list https://raw.githubusercontent.com/amitstudydude/RDP_Linux/main/c.sh
 sudo cat sources.list >> /etc/apt/sources.list
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 9EA4D6FCA5D37A5D1CA9C09AAD5F235DF639B041
 sudo apt update
