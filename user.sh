4c05e5048e2f791a8e5317d24e40addf6f783004
amitstudydude <99377817+amitstudydude@users.noreply.github.com>
2022-04-25 23:45:52 +0530

    Update user.sh

--- user.sh
+++ user.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+docker pull ubuntu
+docker run -it ubuntu
 sudo useradd -m user && sudo adduser user sudo && echo 'user:user' | sudo chpasswd && yes user | sudo passwd 
 sudo usermod -a -G sudo,adm user
 printf "user" | su user 
