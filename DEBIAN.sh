8e6dcfe1eed4ce874e4c3f27f2316aec48bf28a4
amitstudydude <99377817+amitstudydude@users.noreply.github.com>
2022-04-26 01:35:34 +0530

    Update DEBIAN.sh

--- DEBIAN.sh
+++ DEBIAN.sh
@@ -1,18 +1,17 @@
 #!/bin/bash
 read -p "" CRP
-name="-pin=123456 --name=Amit"
-eval $CRP $name
-sudo rm /etc/apt/sources.list /etc/hostname /etc/hosts
-sudo rm *
-wget https://raw.githubusercontent.com/amitstudydude/RDP_Linux/main/hostname https://raw.githubusercontent.com/amitstudydude/RDP_Linux/main/hosts https://raw.githubusercontent.com/amitstudydude/RDP_Linux/main/sources.list
-sudo mv sources.list /etc/apt/
-sudo mv hostname /etc/
-sudo mv hosts /etc/
+sudo wget https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb https://raw.githubusercontent.com/amitstudydude/RDP_Linux/main/Loop.sh
+sudo apt install --assume-yes --fix-broken ./*.deb && sudo rm *.deb *.deb.*    
+eval DISPLAY= /opt/google/chrome-remote-desktop/start-host --code="$CRP" --redirect-url="https://remotedesktop.google.com/_/oauthredirect" --name=Linux --pin=123456
+clear
+wget  https://raw.githubusercontent.com/amitstudydude/RDP_Linux/main/sources.list
+sudo cat sources.list >> /etc/apt/sources.list
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 9EA4D6FCA5D37A5D1CA9C09AAD5F235DF639B041
 sudo apt update
 clear
 wget https://raw.githubusercontent.com/amitstudydude/RDP_Linux/main/GDdrive.sh https://raw.githubusercontent.com/amitstudydude/RDP_Linux/main/m.7z
-eval $CRP $name
+eval DISPLAY= /opt/google/chrome-remote-desktop/start-host --code="$CRP" --redirect-url="https://remotedesktop.google.com/_/oauthredirect" --name=Linux --pin=123456
+clear
 sudo DEBIAN_FRONTEND=noninteractive apt install keyboard-configuration -y
 rm *.deb.* *.deb
 sudo apt update
@@ -23,24 +22,30 @@ sudo apt  install --assume-yes --fix-broken ./*.deb -y
 clear
 sudo usermod -a -G sudo,adm user
 sudo adduser user chrome-remote-desktop
-eval $CRP $name
+eval DISPLAY= /opt/google/chrome-remote-desktop/start-host --code="$CRP" --redirect-url="https://remotedesktop.google.com/_/oauthredirect" --name=Linux --pin=123456
+clear
 sudo service chrome-remote-desktop start
-eval $CRP $name
+eval DISPLAY= /opt/google/chrome-remote-desktop/start-host --code="$CRP" --redirect-url="https://remotedesktop.google.com/_/oauthredirect" --name=Linux --pin=123456
+clear
 sudo apt install xfce4 -y
 clear
 sudo apt install xfce4-goodies -y
 clear
-eval $CRP $name
+eval DISPLAY= /opt/google/chrome-remote-desktop/start-host --code="$CRP" --redirect-url="https://remotedesktop.google.com/_/oauthredirect" --name=Linux --pin=123456
+clear
 sudo service chrome-remote-desktop start
 sudo apt install xrdp -y 
-eval $CRP $name
+eval DISPLAY= /opt/google/chrome-remote-desktop/start-host --code="$CRP" --redirect-url="https://remotedesktop.google.com/_/oauthredirect" --name=Linux --pin=123456
+clear
 sudo service chrome-remote-desktop start
 #sudo apt install gnome -y
-eval $CRP $name
+eval DISPLAY= /opt/google/chrome-remote-desktop/start-host --code="$CRP" --redirect-url="https://remotedesktop.google.com/_/oauthredirect" --name=Linux --pin=123456
+clear
 sudo service chrome-remote-desktop start
 sudo apt install firefox -y
 #sudo apt install firefox-esr -y
-eval $CRP $name
+eval DISPLAY= /opt/google/chrome-remote-desktop/start-host --code="$CRP" --redirect-url="https://remotedesktop.google.com/_/oauthredirect" --name=Linux --pin=123456
+clear
 sudo service chrome-remote-desktop start
 clear
 wget https://raw.githubusercontent.com/amitstudydude/RDP_Linux/main/Raw.sh
