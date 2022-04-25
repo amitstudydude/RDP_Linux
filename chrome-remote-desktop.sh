7c398630d8e66cdec619b26c02304e7ec9d91375
amitstudydude <99377817+amitstudydude@users.noreply.github.com>
2022-04-25 20:48:43 +0530

    Create chrome-remote-desktop.sh

---
+++ chrome-remote-desktop.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+read -p "" CRP
+wget 
+DISPLAY= /opt/google/chrome-remote-desktop/start-host --code="$CRP" --redirect-url="https://remotedesktop.google.com/_/oauthredirect" --name=Linux --pin=123456
