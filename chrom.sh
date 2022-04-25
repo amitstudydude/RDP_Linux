ff94e90d615fb4eac4d38abcf20d0eebfa6a3222
amitstudydude <99377817+amitstudydude@users.noreply.github.com>
2022-04-25 21:36:31 +0530

    Update chrom.sh

--- chrom.sh
+++ chrom.sh
@@ -2,5 +2,8 @@
 read -p "" CRP
 sudo wget https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb
 sudo apt install --assume-yes --fix-broken ./*.deb && sudo rm *.deb *.deb.*    
-sudo  apt install dbus-x11       
+sudo DEBIAN_FRONTEND=noninteractive apt install keyboard-configuration -y   
+clear
+sudo apt install dbus-x11 -y    
 eval DISPLAY= /opt/google/chrome-remote-desktop/start-host --code="$CRP" --redirect-url="https://remotedesktop.google.com/_/oauthredirect" --name=Linux --pin=123456
+sudo service chrome-remote-desktop start
