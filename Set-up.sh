1c3d780d54ddab56c588e0501af3f3eaa4127434
amitstudydude <99377817+amitstudydude@users.noreply.github.com>
2022-04-25 23:44:40 +0530

    Update Set-up.sh

--- Set-up.sh
+++ Set-up.sh
@@ -4,5 +4,7 @@ sudo cat sources.list >> /etc/apt/sources.list
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 9EA4D6FCA5D37A5D1CA9C09AAD5F235DF639B041
 sudo apt update
 clear
+#sudo apt install xfce4 xfce4-goodies -y
 sudo apt install ubuntu-desktop -y
+sudo apt install firefox -y
 clear
