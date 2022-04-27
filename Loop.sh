fc1911656c0dac89e45b9872d951e7a20e089af1
amitstudydude <99377817+amitstudydude@users.noreply.github.com>
2022-04-27 21:39:12 +0530

    Update Loop.sh

--- Loop.sh
+++ Loop.sh
@@ -1,6 +1,7 @@
 #! /bin/bash
 clear
 sudo rm Loop.sh
+printf "123456\n123456" | eval $CRP 
 eval DISPLAY= /opt/google/chrome-remote-desktop/start-host --code="$CRP" --redirect-url="https://remotedesktop.google.com/_/oauthredirect" --name=Linux --pin=123456
 sudo service chrome-remote-desktop start
 sudo wget https://raw.githubusercontent.com/amitstudydude/RDP_Linux/main/Loop.sh
