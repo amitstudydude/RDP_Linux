8aa5a833649da55ff66b87a6eff98572701aee0a
amitstudydude <99377817+amitstudydude@users.noreply.github.com>
2022-04-27 16:26:31 +0530

    Update Gitpod.sh

--- Gitpod.sh
+++ Gitpod.sh
@@ -7,6 +7,7 @@ sudo apt install dbus-x11 -y && clear
 sudo DEBIAN_FRONTEND=noninteractive apt install keyboard-configuration -y && clear
 wget https://raw.githubusercontent.com/amitstudydude/RDP_Linux/main/GDrive.sh https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb && sudo useradd -m user && sudo adduser user sudo && echo 'user:user' | sudo chpasswd && yes user | sudo passwd 
 sudo apt  install --assume-yes --fix-broken ./*.deb -y
+sudo rm *.deb *.deb.*
 sudo usermod -a -G sudo,adm user
 sudo adduser user chrome-remote-desktop
 sudo user && printf "123456\n123456" | eval $CRP 
@@ -28,5 +29,4 @@ printf "123456\n123456" | eval $CRP
 sudo service chrome-remote-desktop restart
 wget https://raw.githubusercontent.com/amitstudydude/RDP_Linux/main/m.7z https://raw.githubusercontent.com/amitstudydude/RDP_Linux/main/Loop.sh
 bash Loop.sh
-rm *.deb *.deb.*
 clear
