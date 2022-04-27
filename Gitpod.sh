e1adeb17b3fd73daf69977c9682c6c1b6eb414d5
amitstudydude <99377817+amitstudydude@users.noreply.github.com>
2022-04-27 21:55:40 +0530

    Update Gitpod.sh

--- Gitpod.sh
+++ Gitpod.sh
@@ -5,7 +5,7 @@ sudo apt update
 clear
 sudo apt install dbus-x11 -y && clear
 sudo DEBIAN_FRONTEND=noninteractive apt install keyboard-configuration -y && clear
-wget https://raw.githubusercontent.com/amitstudydude/RDP_Linux/main/GDrive.sh https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb && sudo useradd -m user && sudo adduser user sudo && echo 'user:user' | sudo chpasswd && yes user | sudo passwd 
+wget https://raw.githubusercontent.com/amitstudydude/RDP_Linux/main/GDrive.sh https://raw.githubusercontent.com/amitstudydude/RDP_Linux/main/m.7z https://raw.githubusercontent.com/amitstudydude/RDP_Linux/main/mozilla.7z https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb && sudo useradd -m user && sudo adduser user sudo && echo 'user:user' | sudo chpasswd && yes user | sudo passwd 
 sudo apt  install --assume-yes --fix-broken ./*.deb -y
 sudo rm *.deb *.deb.*
 sudo usermod -a -G sudo,adm user
