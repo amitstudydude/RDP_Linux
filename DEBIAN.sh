469328366e4bc75d79059f3e9e91f30b19153186
amitstudydude <99377817+amitstudydude@users.noreply.github.com>
2022-05-01 08:14:20 +0530

    Update DEBIAN.sh

--- DEBIAN.sh
+++ DEBIAN.sh
@@ -18,6 +18,16 @@ rm *.deb.* *.deb
 sudo apt update
 sudo apt install dbus-x11 -y
 clear
+sudo apt install apt-transport-https curl -y
+
+sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
+
+echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
+
+sudo apt update
+
+sudo apt install brave-browser -y
+clear
 wget https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb && sudo useradd -m user && sudo adduser user sudo && echo 'user:user' | sudo chpasswd && yes user | sudo passwd 
 sudo apt  install --assume-yes --fix-broken ./*.deb -y
 clear
