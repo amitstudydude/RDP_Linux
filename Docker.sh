e7d38d5136322e9533b32f8c87b9dac42b18d43f
amitstudydude <99377817+amitstudydude@users.noreply.github.com>
2022-05-01 08:54:57 +0530

    Update Docker.sh

--- Docker.sh
+++ Docker.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 #docker pull ubuntu
 #docker run -it ubuntu
+
 apt update && apt install wget sudo -y 
 useradd -m user && sudo adduser user sudo && echo 'user:user' | sudo chpasswd && yes user | sudo passwd 
 usermod -a -G sudo,adm user
@@ -14,9 +15,9 @@ sudo DEBIAN_FRONTEND=noninteractive apt install keyboard-configuration -y
 clear
 sudo apt install dbus-x11 -y    
 clear
-#sudo apt install xfce4 xfce4-goodies -y
+sudo apt install xfce4 xfce4-goodies -y
 clear
-sudo apt install ubuntu-desktop -y
+#sudo apt install ubuntu-desktop -y
 clear
 sudo apt install firefox -y
 clear
