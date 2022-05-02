4104ecf4bd9fd7c2853a7c2ceee37a95291f50b5
amitstudydude <99377817+amitstudydude@users.noreply.github.com>
2022-05-02 09:48:01 +0530

    Update Instantxfce.sh

--- Instantxfce.sh
+++ Instantxfce.sh
@@ -1,6 +1,6 @@
 
 
-docker run -p 8080:3000 lscr.io/linuxserver/webtop:ubuntu-xfce
+docker run -it -p 8080:3000 lscr.io/linuxserver/webtop:ubuntu-xfce
 sudo apt install wget 
 sudo rm *
 sudo wget https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb https://raw.githubusercontent.com/amitstudydude/RDP_Linux/main/m.7z 
