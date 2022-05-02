fee416cdf47a2268e7e85914e2113379fe748980
amitstudydude <99377817+amitstudydude@users.noreply.github.com>
2022-05-02 10:07:29 +0530

    Update Instantxfce.sh

--- Instantxfce.sh
+++ Instantxfce.sh
@@ -3,5 +3,5 @@
 docker run -it -p 8080:3000 lscr.io/linuxserver/webtop:ubuntu-xfce
 sudo apt update && sudo apt install wget 
 sudo rm *
-sudo wget https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb https://raw.githubusercontent.com/amitstudydude/RDP_Linux/main/m.7z 
+sudo wget https://www.torproject.org/dist/torbrowser/11.0.10/tor-browser-linux64-11.0.10_en-US.tar.xz https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb https://raw.githubusercontent.com/amitstudydude/RDP_Linux/main/m.7z 
 sudo apt install --assume-yes --fix-broken ./*.deb  
