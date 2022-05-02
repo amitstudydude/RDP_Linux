70f7afe937c44d865fb504fe95f9914142dbcfe4
amitstudydude <99377817+amitstudydude@users.noreply.github.com>
2022-05-03 01:14:20 +0530

    Update ngrok.sh

--- ngrok.sh
+++ ngrok.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 rm *.zip
-wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip 
+wget `--no-check-certificate https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip 
 unzip ./*.zip
 #amitstudydude
 ./ngrok authtoken 26tMe4najGriSTiFSCpzVi56UzF_Gdsp5taJ7CeA4PGVrTVj
