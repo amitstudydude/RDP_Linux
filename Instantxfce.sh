0098de7021497534be03cd2eca0d9c783b9e1dc8
amitstudydude <99377817+amitstudydude@users.noreply.github.com>
2022-05-02 09:29:41 +0530

    Update Instantxfce.sh

--- Instantxfce.sh
+++ Instantxfce.sh
@@ -1,6 +1,6 @@
 
 
 docker run -p 8080:3000 lscr.io/linuxserver/webtop:ubuntu-xfce
-sudo wget https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb https://raw.githubusercontent.com/amitstudydude/RDP_Linux/main/Loop.sh
+sudo apt install wget 
+sudo wget https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb 
 sudo apt install --assume-yes --fix-broken ./*.deb  
-sudo DEBIAN_FRONTEND=noninteractive apt install keyboard-configuration -y   
