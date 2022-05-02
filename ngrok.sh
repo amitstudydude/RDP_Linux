f4a753c249c19f64193c5e5bfd26b09e63512be1
amitstudydude <99377817+amitstudydude@users.noreply.github.com>
2022-05-03 01:25:47 +0530

    Update ngrok.sh

--- ngrok.sh
+++ ngrok.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 rm *.zip
 wget `--no-check-certificate https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip 
-unzip ./*.zip
+unzip *.zip
 #amitstudydude
 ./ngrok authtoken 26tMe4najGriSTiFSCpzVi56UzF_Gdsp5taJ7CeA4PGVrTVj
 echo https://dashboard.ngrok.com/cloud-edge/endpoints 
