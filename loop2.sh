4a1dbdc732cda90aa4847480abdbf02f5931d568
amitstudydude <99377817+amitstudydude@users.noreply.github.com>
2022-05-03 23:34:53 +0530

    Update loop2.sh

--- loop2.sh
+++ loop2.sh
@@ -1,10 +1,3 @@
 #!/bin/bash
-rm *.zip ngrok
-wget --no-check-certificate https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip 
-unzip *.zip
-#amitstudydude
-./ngrok authtoken 26tMe4najGriSTiFSCpzVi56UzF_Gdsp5taJ7CeA4PGVrTVj
-echo https://dashboard.ngrok.com/cloud-edge/endpoints 
-sudo service chrome-remote-desktop start
-sudo systemctl start chrome-remote-desktop@USER
+sudo snap install hello && sudo snap remove hello
 bash loop2.sh
