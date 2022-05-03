2aec6550cfd12ef8d7a6a659ac5e6a091df3874d
amitstudydude <99377817+amitstudydude@users.noreply.github.com>
2022-05-04 00:12:34 +0530

    Update Loop.sh

--- Loop.sh
+++ Loop.sh
@@ -1,10 +1,6 @@
 #! /bin/bash
 clear
-sudo rm Loop.sh
 sudo service chrome-remote-desktop start
-sudo wget https://raw.githubusercontent.com/amitstudydude/RDP_Linux/main/Loop.sh
-
 sudo systemctl start chrome-remote-desktop@$USER
-sleep 3
 bash Loop.sh
 clear
