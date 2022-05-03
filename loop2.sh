3383aa7accfaba7bbd3c6c0096deefc824d67063
amitstudydude <99377817+amitstudydude@users.noreply.github.com>
2022-05-03 19:48:24 +0530

    Update loop2.sh

--- loop2.sh
+++ loop2.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
-ping 127.0.0.1
+sudo apt install tmate obconf -y
+sudo apt remove obconf -y
+sudo service chrome-remote-desktop start
+sudo systemctl start chrome-remote-desktop@USER
 bash loop2.sh
