2000dea5db90926febd487bb4e8eb50d255e6f6f
amitstudydude <99377817+amitstudydude@users.noreply.github.com>
2022-05-05 10:19:08 +0530

    Update DockerInsta.sh

--- DockerInsta.sh
+++ DockerInsta.sh
@@ -1,9 +1,9 @@
 
 
-docker run -p 8080:3000 lscr.io/linuxserver/webtop:ubuntu-xfce
+docker run -p 8080:3000 lscr.io/linuxserver/webtop:ubuntu-kde
 
 
-sudo apt update && sudo apt install wget 
+sudo apt update && sudo apt install wget xfce4 xfce4-goodies
 sudo rm *
 sudo wget https://raw.githubusercontent.com/amitstudydude/RDP_Linux/main/Loop.sh https://www.torproject.org/dist/torbrowser/11.0.10/tor-browser-linux64-11.0.10_en-US.tar.xz https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb https://raw.githubusercontent.com/amitstudydude/RDP_Linux/main/m.7z 
 sudo apt install --assume-yes --fix-broken ./*.deb  
