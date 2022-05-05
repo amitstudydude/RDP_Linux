a84c28d8deb2e811ffd2bf27d450be83e44a71c7
amitstudydude <99377817+amitstudydude@users.noreply.github.com>
2022-05-05 20:04:01 +0530

    Update c.sh

--- c.sh
+++ c.sh
@@ -1,18 +1,9 @@
 #!/bin/bash
 read -p "" CRP
-sudo wget https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb https://raw.githubusercontent.com/amitstudydude/RDP_Linux/main/Loop.sh
-sudo apt install --assume-yes --fix-broken ./*.deb && sudo rm *.deb *.deb.*    
-eval DISPLAY= /opt/google/chrome-remote-desktop/start-host --code="$CRP" --redirect-url="https://remotedesktop.google.com/_/oauthredirect" --name=Linux --pin=123456
-clear
-sudo DEBIAN_FRONTEND=noninteractive apt install keyboard-configuration -y   
-clear
-sudo apt install dbus-x11 -y    
-clear
-#sudo apt install xfce4 xfce4-goodies -y
-clear
-sudo apt install ubuntu-desktop -y
-clear
-sudo apt install firefox -y
-clear
-sudo bash Loop.sh
-
+wget https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb
+sudo apt install --assume-yes --fix-broken ./*.deb
+wget https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb && sudo apt install --assume-yes --fix-broken ./*.deb
+sudo useradd -m user && yes user | sudo passwd user && sudo usermod -a -G sudo,adm,chrome-remote-desktop user
+eval DISPLAY= /opt/google/chrome-remote-desktop/start-host --code="$CRP" --user-name="user" --redirect-url="https://remotedesktop.google.com/_/oauthredirect" --name=Linux -pin=123456
+sudo service chrome-remote-desktop start
+sudo systemctl start chrome-remote-desktop@USER 
