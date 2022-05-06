9bbb5301376f94e0b64fcb167d4de94936bbae0b
amitstudydude <99377817+amitstudydude@users.noreply.github.com>
2022-05-06 11:24:36 +0530

    Update ngrok.sh

--- ngrok.sh
+++ ngrok.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
-rm *.zip
-wget --no-check-certificate https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip 
-unzip *.zip
 #amitstudydude
+wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip && unzip ./*.zip
+rm ./*.zip ngrok
 ./ngrok authtoken 26tMe4najGriSTiFSCpzVi56UzF_Gdsp5taJ7CeA4PGVrTVj
-echo https://dashboard.ngrok.com/cloud-edge/endpoints 
 killall ngrok
-./ngrok tcp 3389
+sudo apt install ssh openssh-server -y
+echo "root:root" | chpasswd
+echo "PasswordAuthentication yes" >> /etc/ssh/sshd_config
+echo "PermitUserEnvironment yes" >> /etc/ssh/sshd_config
+echo "PermitRootLogin yes" >> /etc/ssh/sshd_config
+sudo service ssh restart
+./ngrok tcp --region in 22 &>/dev/null. 2>&1
 curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p'
         
