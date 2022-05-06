5ac0385114491406b4dcd96578105ef56e1809ca
amitstudydude <99377817+amitstudydude@users.noreply.github.com>
2022-05-06 11:29:12 +0530

    Update ngrok.sh

--- ngrok.sh
+++ ngrok.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 #amitstudydude
 wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip && unzip ./*.zip
-rm ./*.zip ngrok
 ./ngrok authtoken 26tMe4najGriSTiFSCpzVi56UzF_Gdsp5taJ7CeA4PGVrTVj
-killall ngrok
 sudo apt install ssh openssh-server -y
 echo "root:root" | chpasswd
 echo "PasswordAuthentication yes" >> /etc/ssh/sshd_config
@@ -11,6 +9,6 @@ echo "PermitUserEnvironment yes" >> /etc/ssh/sshd_config
 echo "PermitRootLogin yes" >> /etc/ssh/sshd_config
 sudo service ssh restart
 ./ngrok tcp --region in 22 &>/dev/null. 2>&1
-sudo service ssh restart
+sleep 3
 curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p'
         
