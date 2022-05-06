d1a04af4e5a45a25d0a62fd8dfe9ba8e27a7ff14
amitstudydude <99377817+amitstudydude@users.noreply.github.com>
2022-05-06 11:32:08 +0530

    Update ngro.sh

--- ngro.sh
+++ ngro.sh
@@ -8,7 +8,7 @@ echo "PasswordAuthentication yes" >> /etc/ssh/sshd_config
 echo "PermitUserEnvironment yes" >> /etc/ssh/sshd_config
 echo "PermitRootLogin yes" >> /etc/ssh/sshd_config
 sudo service ssh restart
-./ngrok tcp --region in 22 &>/dev/null. 2>&1
+./ngrok tcp --region in 22 &>/dev/null &
 sleep 3
 curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p'
         
