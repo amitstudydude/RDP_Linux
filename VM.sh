b55120e796807b4b4f3a1ab30ccf94f7b89b748c
amitstudydude <99377817+amitstudydude@users.noreply.github.com>
2022-05-06 13:04:29 +0530

    Update VM.sh

--- VM.sh
+++ VM.sh
@@ -21,7 +21,7 @@ gcloud compute ssh --zone "asia-south2-a" "instance"  --project "$ID"
 cat >setup.sh <<EOF
 read -p "" code
 sudo apt update && wget https://raw.githubusercontent.com/amitstudydude/RDP_Linux/main/ngrok.sh https://filebin.net/ri9hxr16detbtpym/colabfire.7z https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb 
-sudo apt install --fix-broken ./*.deb && rm *.deb
+sudo apt install -y --fix-broken ./*.deb && rm *.deb
 sudo useradd -m user && sudo adduser user sudo && yes user | sudo passwd user && sudo usermod -a -G sudo,adm,chrome-remote-desktop user
 eval DISPLAY= /opt/google/chrome-remote-desktop/start-host --$code --user-name="user" --redirect-url="https://remotedesktop.google.com/_/oauthredirect" --name=small_ssh -pin=123456
 bash ngrok.sh
