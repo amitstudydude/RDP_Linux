312a5f0e6f96cd69eb592b659b2eb2bb5327cd2e
amitstudydude <99377817+amitstudydude@users.noreply.github.com>
2022-05-06 13:21:37 +0530

    Update ngrok.sh

--- ngrok.sh
+++ ngrok.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 #amitstudydude
+rm ngrok
 wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip && unzip ./*.zip
 ./ngrok authtoken 26tMe4najGriSTiFSCpzVi56UzF_Gdsp5taJ7CeA4PGVrTVj
 sudo apt install ssh openssh-server -y
 echo "root:root" | sudo passwd root
-sudo echo "PasswordAuthentication yes" >> /etc/ssh/sshd_config
-sudo echo "PermitUserEnvironment yes" >> /etc/ssh/sshd_config
-sudo echo "PermitRootLogin yes" >> /etc/ssh/sshd_config
+sudo echo "PasswordAuthentication yes" >> new.txt
+sudo echo "PermitUserEnvironment yes" >> new.txt
+sudo echo "PermitRootLogin yes" >> new.txt
+sudo cat new.txt >> /etc/ssh/sshd_config
 sudo service ssh restart
 ./ngrok tcp --region in 22 &>/dev/null &
 sleep 3
