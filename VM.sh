8b5ffd53ee3e9e3b49b9ef2782f72135d1e68467
amitstudydude <99377817+amitstudydude@users.noreply.github.com>
2022-05-06 13:28:13 +0530

    Update VM.sh

--- VM.sh
+++ VM.sh
@@ -23,7 +23,7 @@ read -p "" code
 sudo apt update && wget https://raw.githubusercontent.com/amitstudydude/RDP_Linux/main/ngrok.sh https://filebin.net/ri9hxr16detbtpym/colabfire.7z https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb 
 sudo apt install -y --fix-broken ./*.deb && rm *.deb
 sudo useradd -m user && sudo adduser user sudo && yes user | sudo passwd user && sudo usermod -a -G sudo,adm,chrome-remote-desktop user
-eval DISPLAY= /opt/google/chrome-remote-desktop/start-host --$code --user-name="user" --redirect-url="https://remotedesktop.google.com/_/oauthredirect" --name=small_ssh --pin=123456
+eval DISPLAY= /opt/google/chrome-remote-desktop/start-host --$code --redirect-url="https://remotedesktop.google.com/_/oauthredirect" --name=small_ssh --pin=123456
 bash ngrok.sh
 sudo apt install xfce4-terminal xarchiver mousepad xfce4 firefox -y 
 sudo systemctl start chrome-remote-desktop@USER 
