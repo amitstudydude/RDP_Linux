9e591e6b4d842110989aafac0845fa6843bb26d5
amitstudydude <99377817+amitstudydude@users.noreply.github.com>
2022-05-07 05:33:36 +0530

    Update ngrok.sh

--- ngrok.sh
+++ ngrok.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 #amitstudydude
+killall ngrok
 rm ngrok
 wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip && unzip ./*.zip
 ./ngrok authtoken 26tMe4najGriSTiFSCpzVi56UzF_Gdsp5taJ7CeA4PGVrTVj
@@ -12,5 +13,6 @@ sudo cat new.txt >> /etc/ssh/sshd_config
 sudo service ssh restart
 ./ngrok tcp --region in 22 &>/dev/null &
 sleep 3
+sudo service ssh start
 curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p'
         
