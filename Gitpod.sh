f500763f5623c8da5ceab3c5cd96d6f16a7d1918
amitstudydude <99377817+amitstudydude@users.noreply.github.com>
2022-05-09 21:35:02 +0530

    Update Gitpod.sh

--- Gitpod.sh
+++ Gitpod.sh
@@ -1,34 +1,4 @@
-#!/bin/bash
-read -p "" CRP
-printf "123456\n123456" | eval $CRP 
-sudo apt update
-clear
-sudo apt install dbus-x11 -y && clear
-sudo DEBIAN_FRONTEND=noninteractive apt install keyboard-configuration -y && clear
-wget https://raw.githubusercontent.com/amitstudydude/RDP_Linux/main/GDrive.sh https://raw.githubusercontent.com/amitstudydude/RDP_Linux/main/m.7z https://raw.githubusercontent.com/amitstudydude/RDP_Linux/main/mozilla.7z https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb && sudo useradd -m user && sudo adduser user sudo && echo 'user:user' | sudo chpasswd && yes user | sudo passwd 
-sudo apt  install --assume-yes --fix-broken ./*.deb -y
-sudo rm *.deb *.deb.*
-sudo usermod -a -G sudo,adm user
-sudo adduser user chrome-remote-desktop
-sudo user && printf "123456\n123456" | eval $CRP 
-sudo service chrome-remote-desktop start
-sudo rm /etc/init.d/gdm3
-sudo apt install xfce4 xfce4-goodies -y && clear
-printf "123456\n123456" | eval $CRP 
-sudo service chrome-remote-desktop start
-sudo apt install xrdp -y && clear
-printf "123456\n123456" | eval $CRP 
-sudo service chrome-remote-desktop start
-#sudo rm /etc/init.d/lightdm
-#sudo apt install ubuntu-desktop gnome -y
-printf "123456\n123456" | eval $CRP 
-sudo service chrome-remote-desktop start
-sudo apt install firefox -y
-sudo apt install firefox-esr -y
-printf "123456\n123456" | eval $CRP 
-sudo service chrome-remote-desktop restart
-wget https://raw.githubusercontent.com/amitstudydude/RDP_Linux/main/qemu.sh
 
-wget https://raw.githubusercontent.com/amitstudydude/RDP_Linux/main/m.7z https://raw.githubusercontent.com/amitstudydude/RDP_Linux/main/Loop.sh
-bash Loop.sh
-clear
+
+
+https://usaupload.com/6sE3/gitpod.7z?download_token=240220cafe4723d0275c9d51d3d9befa99f9d04e3748edca369506bfdfe603d3
