96cda7d5e2787b42ba85556379bd467241c33123
amitstudydude <99377817+amitstudydude@users.noreply.github.com>
2022-05-09 07:52:46 +0530

    Update ngrok.sh

--- ngrok.sh
+++ ngrok.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 #amitstudydude
-killall ngrok
-rm ngrok
+killall ngrok > /dev/null 2>&1 
+rm ngrok > /dev/null 2>&1 
 wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip  > /dev/null 2>&1 
-unzip ./*.zip
-./ngrok authtoken 26tMe4najGriSTiFSCpzVi56UzF_Gdsp5taJ7CeA4PGVrTVj
-sudo apt install ssh openssh-server tmate -y > /dev/null 2>&1
-yes root | sudo passwd root
+unzip ./*.zip > /dev/null 2>&1 
+./ngrok authtoken 26tMe4najGriSTiFSCpzVi56UzF_Gdsp5taJ7CeA4PGVrTVj > /dev/null 2>&1 
+sudo apt install xfce4-terminal ssh openssh-server tmate -y > /dev/null 2>&1
+yes root | sudo passwd root > /dev/null 2>&1 
 sudo echo "PasswordAuthentication yes" >> new.txt
 sudo echo "PermitUserEnvironment yes" >> new.txt
 sudo echo "PermitRootLogin yes" >> new.txt
 sudo cat new.txt >> /etc/ssh/sshd_config
-sudo service ssh restart
+sudo service ssh restart > /dev/null 2>&1 
 ./ngrok tcp 22 &>/dev/null &
 sleep 3
-sudo service ssh start
+sudo service ssh start > /dev/null 2>&1 
 curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p'
         
