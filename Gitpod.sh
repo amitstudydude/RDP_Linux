d9b46cd7f80b3879dda64c976ce8afd31f4581b0
amitstudydude <99377817+amitstudydude@users.noreply.github.com>
2022-05-09 21:58:27 +0530

    Update Gitpod.sh

--- Gitpod.sh
+++ Gitpod.sh
@@ -1,4 +1,12 @@
 
-
+wget https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb
+sudo apt install --assume-yes --fix-broken ./*.deb && rm *.deb
+          
 
 wget -O gitpod.7z https://usaupload.com/6sE3/gitpod.7z?download_token=240220cafe4723d0275c9d51d3d9befa99f9d04e3748edca369506bfdfe603d3
+
+
+sudo useradd -m user && yes user | sudo passwd user && sudo usermod -a -G sudo,adm user 
+
+
+.
