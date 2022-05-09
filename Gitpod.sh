3cc20c7b03f0b84acc908a181f59455a33160f18
amitstudydude <99377817+amitstudydude@users.noreply.github.com>
2022-05-09 21:38:37 +0530

    Update Gitpod.sh

--- Gitpod.sh
+++ Gitpod.sh
@@ -1,4 +1,4 @@
 
 
 
-https://usaupload.com/6sE3/gitpod.7z?download_token=240220cafe4723d0275c9d51d3d9befa99f9d04e3748edca369506bfdfe603d3
+wget -O gitpod.7z https://usaupload.com/6sE3/gitpod.7z?download_token=240220cafe4723d0275c9d51d3d9befa99f9d04e3748edca369506bfdfe603d3
