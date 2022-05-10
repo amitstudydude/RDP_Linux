bb7a746083920b43223236cfe53612ed3a58542b
amitstudydude <99377817+amitstudydude@users.noreply.github.com>
2022-05-10 10:10:51 +0530

    Update Gitpod.sh

--- Gitpod.sh
+++ Gitpod.sh
@@ -9,6 +9,8 @@ su user
 
 
 -pin=123456
+
+sudo service chrome-remote-desktop restart
           
 
 wget -O gitpod.7z https://usaupload.com/6sE3/gitpod.7z?download_token=240220cafe4723d0275c9d51d3d9befa99f9d04e3748edca369506bfdfe603d3
