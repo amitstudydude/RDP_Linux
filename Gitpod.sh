c94893cb06d08fd37ab14d2bf8162cdd7fc78fb6
amitstudydude <99377817+amitstudydude@users.noreply.github.com>
2022-05-10 10:04:10 +0530

    Update Gitpod.sh

--- Gitpod.sh
+++ Gitpod.sh
@@ -1,6 +1,14 @@
 
+https://github.com/gitpod-io/gitpod.git
+
+
 wget https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb
 sudo apt install --assume-yes --fix-broken ./*.deb && rm *.deb
+sudo useradd -m user && yes user | sudo passwd user && sudo usermod -a -G sudo,adm user 
+su user
+
+
+-pin=123456
           
 
 wget -O gitpod.7z https://usaupload.com/6sE3/gitpod.7z?download_token=240220cafe4723d0275c9d51d3d9befa99f9d04e3748edca369506bfdfe603d3
@@ -11,8 +19,6 @@ wget -O gitpod.7z https://usaupload.com/6sE3/gitpod.7z?download_token=240220cafe
 wget https://raw.githubusercontent.com/amitstudydude/RDP_Linux/main/qemu.sh && bash qemu.sh
 mkdir new && cd new && wget https://raw.githubusercontent.com/amitstudydude/RDP_Linux/main/GDrive.sh && bash *.sh
 
-
-sudo useradd -m user && yes user | sudo passwd user && sudo usermod -a -G sudo,adm user 
 
 
 .
