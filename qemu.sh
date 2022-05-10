6f76d2fa4ad68d32c347adf9ecc5530139d2e49b
amitstudydude <99377817+amitstudydude@users.noreply.github.com>
2022-05-10 14:57:15 +0530

    Update qemu.sh

--- qemu.sh
+++ qemu.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 sudo rm *.iso *.hda
 #wget https://releases.ubuntu.com/22.04/ubuntu-22.04-desktop-amd64.iso
-wget https://releases.ubuntu.com/22.04/ubuntu-22.04-live-server-amd64.iso
-sudo apt-get install qemu qemu-system qemu-system-x86 -y
-qemu-img create ubuntu.hda 40G
-qemu-system-x86_64 -hda ubuntu.hda -boot d -cdrom ubuntu-22.04-live-server-amd64.iso -m 8192 
 
+wget https://releases.ubuntu.com/22.04/ubuntu-22.04-live-server-amd64.iso
+
+sudo apt-get install qemu qemu-system qemu-system-x86 -y
+
+qemu-img create ubuntu.hda 25G
+qemu-system-x86_64 -hda ubuntu.hda -boot d -cdrom ubuntu-22.04-live-server-amd64.iso -m 8192 
 
 qemu-system-x86_64 -hda ubuntu.hda -boot d -m 8192 
 
