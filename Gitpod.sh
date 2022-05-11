2d779260ef82c6e4303b458751134f1b3bee3f45
amitstudydude <99377817+amitstudydude@users.noreply.github.com>
2022-05-11 12:15:38 +0530

    Update Gitpod.sh

--- Gitpod.sh
+++ Gitpod.sh
@@ -4,12 +4,12 @@ https://github.com/gitpod-io/gitpod.git
 sudo apt update
 wget https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb
 sudo apt install --assume-yes --fix-broken ./*.deb && rm *.deb
-sudo useradd -m user && yes user | sudo passwd user && sudo usermod -a -G sudo,adm user 
-su user
+sudo apt install thunar icewm firefox xarchiver -y
+
+
 
 
        -pin=123456 && sudo service chrome-remote-desktop start
-sudo apt install thunar icewm firefox xarchiver -y
 bash
 sudo echo "sudo service chrome-remote-desktop start && sleep 60 && bash bash.sh" > bash.sh && bash bash.sh
 
@@ -17,6 +17,8 @@ sudo echo "sudo service chrome-remote-desktop start && sleep 60 && bash bash.sh"
 
 sudo wget -O gitpod.7z https://usaupload.com/6sE3/gitpod.7z?download_token=240220cafe4723d0275c9d51d3d9befa99f9d04e3748edca369506bfdfe603d3
 
+sudo useradd -m user && yes user | sudo passwd user && sudo usermod -a -G sudo,adm user 
+su user
 
 
 sudo service chrome-remote-desktop restart
