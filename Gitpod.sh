3aa0263975824c3bef9637cd8d8cc4d0c4b40fae
amitstudydude <99377817+amitstudydude@users.noreply.github.com>
2022-05-11 10:40:22 +0530

    Update Gitpod.sh

--- Gitpod.sh
+++ Gitpod.sh
@@ -35,6 +35,42 @@ wget https://raw.githubusercontent.com/amitstudydude/RDP_Linux/main/qemu.sh && b
 
 mkdir new && cd new && wget https://raw.githubusercontent.com/amitstudydude/RDP_Linux/main/GDrive.sh && bash *.sh
 
+cat >debian.list <<EOF
+deb http://deb.debian.org/debian buster main
+deb http://deb.debian.org/debian buster-updates main
+deb http://deb.debian.org/debian-security buster/updates main
+EOF
+
+
+
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys DCC9EFBF77E11517
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 648ACFD622F3D138
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys AA8E81B4331F7F50
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 112695A0E562B32A
+
+
+cat >chromium.pref <<EOF
+# Note: 2 blank lines are required between entries
+Package: *
+Pin: release a=eoan
+Pin-Priority: 500
+
+Package: *
+Pin: origin "deb.debian.org"
+Pin-Priority: 300
+
+# Pattern includes 'chromium', 'chromium-browser' and similarly
+# named dependencies:
+Package: chromium*
+Pin: origin "deb.debian.org"
+Pin-Priority: 700
+EOF
+sudo rm /etc/apt/sources.list.d/debian.list
+sudo cp debian.list /etc/apt/sources.list.d/
+sudo rm /etc/apt/preferences.d/chromium.pref
+sudo cp chromium.pref /etc/apt/preferences.d/
+sudo apt update && sudo apt install chromium
+
 
 
 .
