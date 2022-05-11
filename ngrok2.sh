fb7f5f7751f64f1a79655abad1746d0b1944d545
amitstudydude <99377817+amitstudydude@users.noreply.github.com>
2022-05-11 22:38:24 +0530

    Create ngrok2.sh

---
+++ ngrok2.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+killall ngrok
+rm ngrok 
+yes root | passwd
+sudo echo "PasswordAuthentication yes" >> new.txt
+sudo echo "PermitUserEnvironment yes" >> new.txt
+sudo echo "PermitRootLogin yes" >> new.txt
+wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip  
+unzip ./ngrok-stable-linux-amd64.zip
+#amitstudydude
+#./ngrok authtoken 291AjiIj2SAmYL3LD7K87OYUkdO_2KQwTggtMF5qaax64G9Te 
+#guh7891@outlook.com
+./ngrok authtoken 291lhRXUGTs48sSBejg1nm4VNDi_2VwY5BCEpNLTdzFj2kV8b
+sudo apt install xfce4-terminal ssh openssh-server tmate -y 
+cat new.txt >> /etc/ssh/sshd_config
+sudo service ssh restart 
+./ngrok --region in tcp 22 
