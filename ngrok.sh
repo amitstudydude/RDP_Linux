1d8c383c277a85a10d31e50a2db4e845fa7f6d8a
amitstudydude <99377817+amitstudydude@users.noreply.github.com>
2022-05-12 21:04:19 +0530

    Update ngrok.sh

--- ngrok.sh
+++ ngrok.sh
@@ -11,11 +11,14 @@ sudo cat /etc/ssh/sshd_config >> sshd_config
 #sudo rm /etc/ssh/sshd_config && sudo mv sshd_config  /etc/ssh/
 wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip  
 unzip ./ngrok-stable-linux-amd64.zip
+#guh7891@outlook.com
+./ngrok authtoken 291lhRXUGTs48sSBejg1nm4VNDi_2VwY5BCEpNLTdzFj2kV8b
+sudo service ssh start 
+./ngrok tcp 22 
 #amitstudydude
 ./ngrok authtoken 291AjiIj2SAmYL3LD7K87OYUkdO_2KQwTggtMF5qaax64G9Te 
-#guh7891@outlook.com
-#./ngrok authtoken 291lhRXUGTs48sSBejg1nm4VNDi_2VwY5BCEpNLTdzFj2kV8b
-sudo service ssh restart 
+sudo service ssh restart
 ./ngrok tcp 22 
+
 
 
