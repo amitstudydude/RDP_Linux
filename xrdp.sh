814f474c92422b5be6433b52c6fead9c955f7318
amitstudydude <99377817+amitstudydude@users.noreply.github.com>
2022-05-13 17:38:18 +0530

    Create xrdp.sh

---
+++ xrdp.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+sudo apt install unzip xrdp -y 
+wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip 
+unzip ./ngrok-stable-linux-amd64.zip
+#guh7891@outlook.com
+./ngrok authtoken 291lhRXUGTs48sSBejg1nm4VNDi_2VwY5BCEpNLTdzFj2kV8b
+sudo service xrdp restart 
+./ngrok tcp 3389
+#amitstudydude
+rm ngrok
+unzip ./ngrok-stable-linux-amd64.zip
+./ngrok authtoken 291AjiIj2SAmYL3LD7K87OYUkdO_2KQwTggtMF5qaax64G9Te" 
+sudo service xrdp restart
+./ngrok tcp 3389
