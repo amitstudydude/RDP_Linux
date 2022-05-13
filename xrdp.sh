431e49232c04b4289e5074b2f709a8416f7da86f
amitstudydude <99377817+amitstudydude@users.noreply.github.com>
2022-05-13 19:37:21 +0530

    Update xrdp.sh

--- xrdp.sh
+++ xrdp.sh
@@ -5,10 +5,10 @@ unzip ./ngrok-stable-linux-amd64.zip
 #guh7891@outlook.com
 ./ngrok authtoken 291lhRXUGTs48sSBejg1nm4VNDi_2VwY5BCEpNLTdzFj2kV8b
 sudo service xrdp restart 
-./ngrok tcp 3389
+./ngrok tcp 3389 &>/dev/null &
 #amitstudydude
 rm ngrok
 unzip ./ngrok-stable-linux-amd64.zip
 ./ngrok authtoken 291AjiIj2SAmYL3LD7K87OYUkdO_2KQwTggtMF5qaax64G9Te
 sudo service xrdp restart
-./ngrok tcp 3389
+./ngrok tcp 3389 &>/dev/null &
