23cf66591dc99af70dac752e21e2a65de3a4bf35
amitstudydude <99377817+amitstudydude@users.noreply.github.com>
2022-06-03 01:57:11 +0530

    Update xstartup.sh

--- xstartup.sh
+++ xstartup.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
-
+unset SESSION_MANAGER
+unset DBUS_SESSION_BUS_ADDRESS
+export GNOME_SHELL_SESSION_MODE=classic
+/etc/X11/xinit/xinitrc
 # Uncomment the following two lines for normal desktop:
 #unset SESSION_MANAGER
  #exec /etc/X11/xinit/xinitrc
@@ -12,10 +15,7 @@ vncconfig -iconic &
 #x-terminal-emulator -geometry 80x24+10+10 -ls -title "$VNCDESKTOP Desktop" &
  #x-window-manager &
 
-unset SESSION_MANAGER
-unset DBUS_SESSION_BUS_ADDRESS
-export GNOME_SHELL_SESSION_MODE=classic
-/etc/X11/xinit/xinitrc
+
 # Assume either Gnome will be started by default when installed
 # We want to kill the session automatically in this case when user logs out. Incase you modify
 # /etc/X11/xinit/Xclients or ~/.Xclients yourself to achieve a different result, then you should
