f13841bd1cb244eea727b46e9c73c16e281100f8
amitstudydude <99377817+amitstudydude@users.noreply.github.com>
2022-06-03 01:56:50 +0530

    Update xstartup.sh

--- xstartup.sh
+++ xstartup.sh
@@ -5,13 +5,12 @@
  #exec /etc/X11/xinit/xinitrc
 #gnome-session --session=gnome-classic &
 
-#[ -x /etc/vnc/xstartup ] && exec /etc/vnc/xstartup
-#[ -r $HOME/.Xresources ] && xrdb $HOME/.Xresources
-#xsetroot -solid grey
-#vncconfig -iconic &
+[ -x /etc/vnc/xstartup ] && exec /etc/vnc/xstartup
+[ -r $HOME/.Xresources ] && xrdb $HOME/.Xresources
+xsetroot -solid grey
+vncconfig -iconic &
 #x-terminal-emulator -geometry 80x24+10+10 -ls -title "$VNCDESKTOP Desktop" &
  #x-window-manager &
-#!/bin/sh
 
 unset SESSION_MANAGER
 unset DBUS_SESSION_BUS_ADDRESS
