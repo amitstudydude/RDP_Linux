af8b2159f1f2d2644ed6c07e9e7e0253d1b865f6
amitstudydude <99377817+amitstudydude@users.noreply.github.com>
2022-06-03 06:12:10 +0530

    Update Lt.sh

--- Lt.sh
+++ Lt.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 lt --port 6080 > nain.txt & 
-echo " cat nain.txt && sleep 2 && bash nain.sh " >> nain.sh 
+echo " cat nain.txt && echo ${{ github.event.inputs.token }} && sleep 2 && bash nain.sh " >> nain.sh 
 bash nain.sh
 bash Lt.sh
