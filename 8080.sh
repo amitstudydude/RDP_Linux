a80912cb7158cb4433a3f5314d5622150207c057
amitstudydude <99377817+amitstudydude@users.noreply.github.com>
2022-06-03 23:55:32 +0530

    Update 8080.sh

--- 8080.sh
+++ 8080.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 rm *
-lt --port 8080 >>1.txt &
-lt --port 6080 >>2.txt &
+lt --port 8080 >>1.txt >&/dev/null 2>&1 &
+lt --port 6080 >>2.txt >&/dev/null 2>&1 &
 cat 1.txt && cat 2.txt 
 wget https://raw.githubusercontent.com/amitstudydude/RDP_Linux/main/8080.sh 
+bash 8080.sh
