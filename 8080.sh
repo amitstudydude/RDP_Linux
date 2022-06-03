42b1cbfc7069787c6dec91729a158adc19572f4b
amitstudydude <99377817+amitstudydude@users.noreply.github.com>
2022-06-03 23:18:48 +0530

    Update 8080.sh

--- 8080.sh
+++ 8080.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
+sudo npm install -g localtunnel
 lt --port 8080 >1.txt &
 lt --port 6080 >2.txt &
-cat 1.txt && cat 2.txt && sleep 2
-bash 8080.sh
+cat 1.txt && cat 2.txt && sleep 2 && bash 8080.sh
+
