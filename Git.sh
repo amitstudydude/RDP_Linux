c9b308c0f76d81b50690f190b047ce20113aa40a
amitstudydude <99377817+amitstudydude@users.noreply.github.com>
2022-06-03 10:51:45 +0530

    Delete Git.sh

--- Git.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-rm *
-wget https://raw.githubusercontent.com/amitstudydude/RDP_Linux/main/Git.sh
-sleep 4
-sudo systemctl start chrome-remote-desktop@$USER
-sudo systemctl status chrome-remote-desktop@$USER
-bash *.sh
-- name: Create SSH Access
-      uses: mxschmitt/action-tmate@v3
