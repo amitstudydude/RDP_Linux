a78b5e2706086db4819a11aac97660d9985b7a63
amitstudydude <99377817+amitstudydude@users.noreply.github.com>
2022-06-03 11:01:05 +0530

    Delete make.sh

--- make.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh 
-[ -x /etc/vnc/xstartup ] && exec /etc/vnc/xstartup 
-[ -r $HOME/.Xresources ] && xrdb $HOME/.Xresources 
-vncconfig -iconic & 
-dbus-launch --exit-with-session gnome-session & 
-           
