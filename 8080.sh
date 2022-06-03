6321dc252bcf2a26395280a44a6fa72318a7b5f0
amitstudydude <99377817+amitstudydude@users.noreply.github.com>
2022-06-04 00:55:27 +0530

    Update 8080.sh

--- 8080.sh
+++ 8080.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-cat 8080.sh
 echo " lt --port 8080 > nail.txt & " >> bash.sh
 echo " lt --port 6080 > pin.txt & " >> bash.sh
-echo " cat nail.txt && cat pin.txt && rm *.sh " >> bash.sh && touch new.sh && mv bash.sh new.sh && cat new.sh && bash new.sh 
+echo " cat nail.txt && cat pin.txt && sleep 2 && rm *.sh " >> bash.sh && touch new.sh && mv bash.sh new.sh && bash new.sh 
 wget https://raw.githubusercontent.com/amitstudydude/RDP_Linux/main/8080.sh
 bash 8080.sh
