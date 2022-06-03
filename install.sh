9f35541166233729a8435200af32610f9da8933d
amitstudydude <99377817+amitstudydude@users.noreply.github.com>
2022-06-03 10:58:46 +0530

    Delete install.sh

--- install.sh
+++
@@ -1,2 +0,0 @@
-#! /bin/bash
-sudo apt update || wget https://download.nomachine.com/download/7.9/Linux/nomachine_7.9.2_1_amd64.deb || sudo apt install *.deb -y || sudo apt upgrade -y && sudo apt install ubuntu-desktop gnome xrdp -y || sudo service gdm3 stop || sudo bash ui.sh
