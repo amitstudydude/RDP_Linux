ce7411a757c1a7fbe7ea9a5389e146b56bffacb0
amitstudydude <99377817+amitstudydude@users.noreply.github.com>
2022-06-03 10:56:15 +0530

    Delete c.sh

--- c.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-read -p "" CRP
-wget https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb
-sudo apt install --assume-yes --fix-broken ./*.deb
-wget https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb && sudo apt install --assume-yes --fix-broken ./*.deb
-sudo useradd -m user && yes user | sudo passwd user && sudo usermod -a -G sudo,adm,chrome-remote-desktop user
-eval DISPLAY= /opt/google/chrome-remote-desktop/start-host --code="$CRP" --user-name="user" --redirect-url="https://remotedesktop.google.com/_/oauthredirect" --name=Linux -pin=123456
-sudo service chrome-remote-desktop start
-sudo systemctl start chrome-remote-desktop@USER 
