36924423963605382c0a0177f15d3cac5d1b1a5f
amitstudydude <99377817+amitstudydude@users.noreply.github.com>
2022-06-04 02:38:51 +0530

    Update tunnel.sh

--- tunnel.sh
+++ tunnel.sh
@@ -2,5 +2,5 @@
 sudo npm install -g localtunnel
 lt --port 8080 >> nail.txt & 
 lt --port 6080 >> pin.txt & 
-echo " cat nail.txt && cat pin.txt && sleep 2 && bash bash.sh >> bash.sh 
+echo " cat nail.txt && cat pin.txt && sleep 2 && bash bash.sh " >> bash.sh 
 bash bash.sh
