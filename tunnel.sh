a94a8a3d4fc92f5c0f5aeb778e1729c3873e88e7
amitstudydude <99377817+amitstudydude@users.noreply.github.com>
2022-06-04 02:36:28 +0530

    Update tunnel.sh

--- tunnel.sh
+++ tunnel.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-echo " lt --port 8080 >> nail.txt & " >> bash.sh
-echo " lt --port 6080 >> pin.txt & " >> bash.sh
-echo " cat nail.txt && cat pin.txt && sleep 10 && touch new.txt && mv nail.txt new.txt && mv pin.txt new.txt && rm *.sh new.txt " >> bash.sh && touch new.sh && mv bash.sh new.sh && bash new.sh  
-wget https://raw.githubusercontent.com/amitstudydude/RDP_Linux/main/tunnel.sh > /dev/null 2>&1
-bash tunnel.sh
+sudo npm install -g localtunnel
+lt --port 8080 >> nail.txt & 
+lt --port 6080 >> pin.txt & 
+echo " cat nail.txt && cat pin.txt && sleep 2 && bash bash.sh >> bash.sh 
+bash bash.sh
