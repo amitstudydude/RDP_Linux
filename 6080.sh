8e7e18fc4bd13a8c3c65437b382def8484437609
amitstudydude <99377817+amitstudydude@users.noreply.github.com>
2022-06-03 22:53:03 +0530

    Update 6080.sh

--- 6080.sh
+++ 6080.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-/opt/novnc/utils/novnc_proxy --web /opt/novnc --vnc localhost:5900 --listen 6080 
+/opt/novnc/utils/novnc_proxy --web /opt/novnc --vnc localhost:5900 --listen 6080 & 
 startxfce4 &
 x11vnc -forever -loop -rfbport 5900 -shared &
 bash 6080.sh
