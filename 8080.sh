82bb431b84c41ea2dc89f838b9665ade495ca61b
amitstudydude <99377817+amitstudydude@users.noreply.github.com>
2022-06-04 00:36:51 +0530

    Update 8080.sh

--- 8080.sh
+++ 8080.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-rm *
 wget https://raw.githubusercontent.com/amitstudydude/RDP_Linux/main/8080.sh
-echo " sudo npm install -g localtunnel && lt --port 8080 >> 1.txt & " >> bash.sh
-echo " cat 1.txt && lt --port 6080 >> 2.txt " >> bash.sh
-echo " cat 2.txt " >> bash.sh && touch new.sh && mv bash.sh new.sh && cat new.sh && bash new.sh && bash 8080.sh
+echo " lt --port 8080 >>1.txt & " >> bash.sh
+echo " cat 1.txt && lt --port 6080 >>2.txt " >> bash.sh
+echo " cat 2.txt && rm *.sh " >> bash.sh && touch new.sh && mv bash.sh new.sh && cat new.sh && bash new.sh && bash 8080.sh
