0a0ef2b62bb79789b81a81f29df5481cc14a88e7
amitstudydude <99377817+amitstudydude@users.noreply.github.com>
2022-06-03 22:01:28 +0530

    Update 6080.sh

--- 6080.sh
+++ 6080.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-x11vnc -forever -loop -reflect localhost:5900 -shared &
+x11vnc -forever -loop -rfbport 5900 -shared &
 lt --port 6080 > nain.txt & 
-echo " cat nain.txt && sleep 2 && bash nain.sh " >> nain.sh 
-bash nain.sh
+cat nain.txt
 bash 6080.sh
