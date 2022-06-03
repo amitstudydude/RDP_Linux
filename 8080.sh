b4ac5e3e2f5eda835a94cb69a290a90a9bacd116
amitstudydude <99377817+amitstudydude@users.noreply.github.com>
2022-06-04 00:46:48 +0530

    Update 8080.sh

--- 8080.sh
+++ 8080.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-echo " lt --port 8080 >>nail.txt & " >> bash.sh
-echo " cat nail.txt && lt --port 6080 >>pin.txt & " >> bash.sh
-echo " cat pin.txt && rm *.sh " >> bash.sh && touch new.sh && mv bash.sh new.sh && cat new.sh && bash new.sh && bash 8080.sh
+echo " lt --port 8080 >> nail.txt & " >> bash.sh
+echo " lt --port 6080 >> pin.txt & " >> bash.sh
+echo " cat nail.txt && cat pin.txt && rm *.sh " >> bash.sh && touch new.sh && mv bash.sh new.sh && cat new.sh && bash new.sh && bash 8080.sh
