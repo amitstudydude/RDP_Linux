a36e1bd6e31a0885e58fde79e795ef4cfbd4d958
amitstudydude <99377817+amitstudydude@users.noreply.github.com>
2022-06-04 01:16:45 +0530

    Update and rename 8080.sh to tunnel.sh

--- 8080.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-echo " lt --port 8080 >> nail.txt & " >> bash.sh
-echo " lt --port 6080 >> pin.txt & " >> bash.sh
-echo " cat nail.txt && cat pin.txt && sleep 2 && rm *.sh " >> bash.sh && touch new.sh && mv bash.sh new.sh && bash new.sh 
-wget https://raw.githubusercontent.com/amitstudydude/RDP_Linux/main/8080.sh > /dev/null &
-bash 8080.sh
---
+++ tunnel.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+cat tunnel.sh
+echo " lt --port 8080 >> nail.txt & " >> bash.sh
+echo " lt --port 6080 >> pin.txt & " >> bash.sh
+echo " cat nail.txt && cat pin.txt && sleep 2 && rm *.sh " >> bash.sh && touch new.sh && mv bash.sh new.sh && bash new.sh  
+wget https://raw.githubusercontent.com/amitstudydude/RDP_Linux/main/tunnel.sh 
+bash tunnel.sh
