ad77924698902b7f90470e9080a84a30ef3c747d
amitstudydude <99377817+amitstudydude@users.noreply.github.com>
2022-06-04 10:35:31 +0530

    Update tunnel.sh

--- tunnel.sh
+++ tunnel.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 sudo npm install -g localtunnel
 /opt/novnc/utils/novnc_proxy --web /opt/novnc --vnc localhost:5900 --listen 6080 &
+startxfce4 &
+x11vnc -rfbport 5900 -forever -loop -shared &
 lt --port 8080 >> nail.txt & 
 lt --port 6080 >> pin.txt & 
 echo " cat nail.txt && cat pin.txt && sleep 2 && bash bash.sh " >> bash.sh 
