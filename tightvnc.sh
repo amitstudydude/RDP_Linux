947d759e34e2748f2c5864d05e2eb8ecdb4b2c80
amitstudydude <99377817+amitstudydude@users.noreply.github.com>
2022-06-04 20:39:10 +0530

    Update and rename n.sh to tightvnc.sh

---
+++ tightvnc.sh
@@ -0,0 +1,46 @@
+#!/bin/sh
+cd && cd ../..
+printf "root\nroot" | sudo passwd runneradmin && printf "root\nroot" | sudo passwd runner && printf "root\nroot" | sudo passwd root 
+printf "root" | su root -c "sudo apt-get install build-essential cmake git libjson-c-dev libwebsockets-dev && git clone https://github.com/tsl0922/ttyd.git && git clone https://github.com/novnc/noVNC.git /opt/novnc && git clone https://github.com/novnc/websockify /opt/novnc/utils/websockify && \ 
+cd ttyd && mkdir build && cd build && cmake .. && make && make install && ttyd -p 8080 bash -x & " 
+echo "<html><head><meta http-equiv=\"Refresh\" content=\"0; url=vnc.html?autoconnect=true&reconnect=true&reconnect_delay=1000&resize=scale&quality=9\"></head></html>" > /opt/novnc/index.html 
+
+sudo DEBIAN_FRONTEND=noninteractive apt install -y tzdata npm keyboard-configuration lightdm apt-utils xserver-xorg-video-dummy xserver-xorg-legacy tightvncserver
+sudo apt install xfce4 
+
+dpkg-reconfigure lightdm
+startxfce4 &
+
+sudo apt install tightvncserver 
+
+
+
+prog=/usr/bin/vncpasswd
+mypass="123456"
+
+/usr/bin/expect <<EOF
+spawn "$prog"
+expect "Password:"
+send "$mypass\r"
+expect "Verify:"
+send "$mypass\r"
+expect eof
+exit
+EOF
+
+sudo npm install -g localtunnel
+npm update
+
+/opt/novnc/utils/novnc_proxy --web /opt/novnc --vnc localhost:5900 --listen 6080 &
+
+vncserver :0 : vncserver -clean -kill :0
+cd && cd .vnc
+echo " startxfce4 & " >> xstartup
+
+vncserver :0 -alwaysshared
+
+lt --port 8080 >> nail.txt & 
+lt --port 6080 >> pin.txt & 
+echo " cat nail.txt && cat pin.txt && sleep 2 && bash bash.sh " >> bash.sh 
+bash bash.sh 
+
