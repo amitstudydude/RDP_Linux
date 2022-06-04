da6b43c0b002efbf4392682ca793db6c58223f71
amitstudydude <99377817+amitstudydude@users.noreply.github.com>
2022-06-05 03:33:35 +0530

    Update Guac.sh

--- Guac.sh
+++ Guac.sh
@@ -1,8 +1,8 @@
 #! /bin/bash
 cd
 sudo apt install --allow-change-held-packages --fix-broken -y libpulse-dev  libssl-dev libvorbis-dev libwebsockets-dev libvncserver-dev libtelnet-dev libssh2-1-dev libpango1.0-dev freerdp2-dev libavcodec-dev libavformat-dev libavutil-dev   libossp-uuid-dev libtool-bin uuid-dev   libcairo2-dev  && \
-printf 'n\nn\ny\nroot\nroot\nroot\nroot\' | wget https://raw.githubusercontent.com/MysticRyuujin/guac-install/main/guac-install.sh && \
-bash guac-install.sh 
+ wget https://raw.githubusercontent.com/MysticRyuujin/guac-install/main/guac-install.sh && \
+printf "n\nn\ny\nroot\nroot\nroot\nroot" | bash guac-install.sh 
 
 
 
