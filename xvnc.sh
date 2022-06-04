915e91e45e3c986468a86d23635d3634a045474d
amitstudydude <99377817+amitstudydude@users.noreply.github.com>
2022-06-05 03:16:01 +0530

    Update xvnc.sh

--- xvnc.sh
+++ xvnc.sh
@@ -2,7 +2,7 @@
 cd && cd ../..
 printf "root\nroot" | sudo passwd runneradmin && printf "root\nroot" | sudo passwd runner && printf "root\nroot" | sudo passwd root 
 printf "root" | su root -c "sudo apt-get install build-essential cmake git libjson-c-dev libwebsockets-dev && git clone https://github.com/tsl0922/ttyd.git && git clone https://github.com/novnc/noVNC.git /opt/novnc && git clone https://github.com/novnc/websockify /opt/novnc/utils/websockify && \ 
-cd ttyd && mkdir build && cd build && cmake .. && make && make install && ttyd -p 8080 bash -x & " 
+cd ttyd && mkdir build && cd build && cmake .. && make && make install && ttyd -p 1234 bash -x & " 
 echo "<html><head><meta http-equiv=\"Refresh\" content=\"0; url=vnc.html?autoconnect=true&reconnect=true&reconnect_delay=1000&resize=scale&quality=9\"></head></html>" > /opt/novnc/index.html 
 
 sudo DEBIAN_FRONTEND=noninteractive apt install -y tzdata xfonts-base xfonts-100dpi npm xfonts-75dpi keyboard-configuration lightdm apt-utils xserver-xorg-video-dummy xserver-xorg-legacy tightvncserver 
@@ -46,10 +46,10 @@ vncserver -alwaysshared
 
 
 
-   
+lt --port 1234 >> nail.txt &
 lt --port 8080 >> nail.txt & 
-lt --port 6080 >> pin.txt & 
-echo " cat nail.txt && cat pin.txt && sleep 2 && bash bash.sh " >> bash.sh 
+lt --port 6080 >> nail.txt & 
+echo " cat nail.txt && sleep 2 && bash bash.sh " >> bash.sh 
 
 
 bash bash.sh &
