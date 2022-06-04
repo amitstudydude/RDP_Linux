e8798c69a4ccd398dfee38a9b66763a022d2a7bc
amitstudydude <99377817+amitstudydude@users.noreply.github.com>
2022-06-04 19:51:38 +0530

    Update and rename v.sh to b.sh

--- b.sh
+++ b.sh
@@ -15,5 +15,6 @@ exit
 EOF
 
 sudo npm install -g localtunnel
+/opt/novnc/utils/novnc_proxy --web /opt/novnc --vnc localhost:5933 --listen 6040 &
 lt --port 6400 & 
-vncserver -name github -alwaysshared -basehttpport 6400 
+vncserver -display :33 -name github -alwaysshared -basehttpport 6400 
