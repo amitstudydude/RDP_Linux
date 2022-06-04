8b794610c1ed229d85df1c34bf51228f94fded76
amitstudydude <99377817+amitstudydude@users.noreply.github.com>
2022-06-04 19:53:49 +0530

    Update and rename b.sh to bn.sh

--- bn.sh
+++ bn.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-sudo apt install tightvncserver tightvnc-java
+sudo apt install tightvncserver 
 
 prog=/usr/bin/vncpasswd
 mypass="123456"
@@ -17,4 +17,4 @@ EOF
 sudo npm install -g localtunnel
 /opt/novnc/utils/novnc_proxy --web /opt/novnc --vnc localhost:5933 --listen 6040 &
 lt --port 6400 & 
-vncserver -display :33 -name github -alwaysshared -basehttpport 6400 
+vncserver :33 -name github -alwaysshared 
