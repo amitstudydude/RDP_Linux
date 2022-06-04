c1719a535e786cdeb7503048d193388266669908
amitstudydude <99377817+amitstudydude@users.noreply.github.com>
2022-06-05 02:56:09 +0530

    Update Guac.sh

--- Guac.sh
+++ Guac.sh
@@ -1,5 +1,21 @@
 #! /bin/bash
 cd
+apt install -y libpulse-dev libssl-dev libvorbis-dev libwebsockets-dev libvncserver-dev libtelnet-dev libssh2-1-dev libpango1.0-dev freerdp2-dev libavcodec-dev libavformat-dev libavutil-dev libswsccale-dev  libossp-uuid-dev libtool-bin uuid-dev libpng12-dev libjpeg62-dev libcairo2-dev libjpeg62-turbo-dev && \
+wget https://raw.githubusercontent.com/MysticRyuujin/guac-install/main/guac-install.sh && \
+bash guac-install.sh 
 
-wget -o i.sh https://raw.githubusercontent.com/MysticRyuujin/guac-install/main/guac-install.sh 
-bash i.sh
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
