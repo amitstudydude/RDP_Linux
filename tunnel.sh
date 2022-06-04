750fb432ec3cbc6c77b1800f22338ac4edadf95c
amitstudydude <99377817+amitstudydude@users.noreply.github.com>
2022-06-04 13:00:10 +0530

    Update tunnel.sh

--- tunnel.sh
+++ tunnel.sh
@@ -3,8 +3,9 @@ sudo npm install -g localtunnel
 sudo DEBIAN_FRONTEND=noninteractive apt install -y tzdata npm keyboard-configuration lightdm apt-utils xserver-xorg-video-dummy xserver-xorg-legacy x11vnc
 sudo apt install xfce4 
 /opt/novnc/utils/novnc_proxy --web /opt/novnc --vnc localhost:5900 --listen 6080 &
+dpkg-reconfigure lightdm
 startxfce4 &
-x11vnc -rfbport 5900 -forever -loop -shared &
+x11vnc -rfbport 5900 -forever -auth /var/run/lightdm/root/:0 -shared &
 lt --port 8080 >> nail.txt & 
 lt --port 6080 >> pin.txt & 
 echo " cat nail.txt && cat pin.txt && sleep 10 && bash bash.sh " >> bash.sh 
