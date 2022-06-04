d70344257047ebe77519a1ac08a94b246e917b86
amitstudydude <99377817+amitstudydude@users.noreply.github.com>
2022-06-04 17:27:47 +0530

    Update tunnel.sh

--- tunnel.sh
+++ tunnel.sh
@@ -10,8 +10,8 @@ startxfce4 &
 x11vnc -rfbport 5900 -forever -bg -auth /var/run/lightdm/root/:0 -shared -nopw &
 lt --port 8080 >> nail.txt & 
 lt --port 6080 >> pin.txt & 
-echo " cat nail.txt && cat pin.txt && sleep 10 && bash bash.sh " >> bash.sh 
+echo " cat nail.txt && cat pin.txt && bash bash.sh " >> bash.sh 
 bash bash.sh &
 wget https://raw.githubusercontent.com/amitstudydude/RDP_Linux/main/tunnel.sh
-bash bash.sh
+bash bash.sh 
 
