3e560fc649a4d65771610c4bb840755bdc6bd864
amitstudydude <99377817+amitstudydude@users.noreply.github.com>
2022-06-04 23:46:32 +0530

    Update x.sh

--- x.sh
+++ x.sh
@@ -44,10 +44,11 @@ echo " startxfce4 & " >> xstartup
 chmod +x xstartup
 vncserver -alwaysshared
 
-
+/opt/novnc/utils/novnc_proxy --web /opt/novnc --vnc localhost:5901 --listen 6080 & 
+      
 lt --port 8080 >> nail.txt & 
 lt --port 6080 >> pin.txt & 
 echo " cat nail.txt && cat pin.txt && sleep 2 && bash bash.sh " >> bash.sh 
 
 
-bash bash.sh &
+bash bash.sh 
