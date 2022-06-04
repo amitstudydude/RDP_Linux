49b1c20f13c066250bbef042859095a6a41b0061
amitstudydude <99377817+amitstudydude@users.noreply.github.com>
2022-06-04 12:36:52 +0530

    Create vncloop.sh

---
+++ vncloop.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+sleep 300
+sudo npm install -g localtunnel
+sudo DEBIAN_FRONTEND=noninteractive apt install -y tzdata npm keyboard-configuration lightdm apt-utils xserver-xorg-video-dummy xserver-xorg-legacy x11vnc
+sudo apt install xfce4 
+/opt/novnc/utils/novnc_proxy --web /opt/novnc --vnc localhost:5900 --listen 6080 &
+startxfce4 &
+x11vnc -rfbport 5900 -forever -loop -shared &
+lt --port 8080 >> nail.txt & 
+lt --port 6080 >> pin.txt & 
+sleep 300
+bash vncloop.sh
