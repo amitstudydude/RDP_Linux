d2acc7b4a38e344fcce47bc43dbcd8ac6afc7b1b
amitstudydude <99377817+amitstudydude@users.noreply.github.com>
2022-06-05 01:40:54 +0530

    Update x.sh

--- x.sh
+++ x.sh
@@ -47,7 +47,9 @@ vncserver -alwaysshared
 
 
 /opt/novnc/utils/novnc_proxy --web /opt/novnc --vnc localhost:5901 --listen 6080 & 
-      
+
+cat x.sh
+   
 lt --port 8080 >> nail.txt & 
 lt --port 6080 >> pin.txt & 
 echo " cat nail.txt && cat pin.txt && sleep 2 && bash bash.sh " >> bash.sh 
