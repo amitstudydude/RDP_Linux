2df8b549a0592c2a1bf890800bb711de75a828a7
amitstudydude <99377817+amitstudydude@users.noreply.github.com>
2022-06-05 18:26:41 +0530

    Update xstartup.sh

--- xstartup.sh
+++ xstartup.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-sudo apt install ubuntu-desktop gnome-session-flashback nautilus mate-terminal gnome-panel gnome-settings-daemon metacity
+sudo apt-get install indicator-applet-complete
+sudo apt install ubuntu-desktop gnome-session-flashback nautilus mate-terminal gnome-panel ubuntu* gnome* gnome-settings-daemon metacity
 unset SESSION_MANAGER
 unset DBUS_SESSION_BUS_ADDRESS
 chmod +x xstartup
@@ -8,15 +9,14 @@ chmod +x xstartup
 xsetroot -solid grey
 export XKL_XMODMAP_DISABLE=1 
 export XDG_CURRENT_DESKTOP="GNOME-Flashback:Unity" 
-#export XDG_CURRENT_DESKTOP=ubuntu:GNOME
 export XDG_MENU_PREFIX="gnome-flashback-" 
-unset DBUS_SESSION_BUS_ADDRESS 
 gnome-session -session=gnome-flashback-metacity -disable-acceleration-check --debug &
 gnome-panel &
-gnome-settings-daemon &
+gsettings set org.gnome.desktop.background show-desktop-icons true &
 metacity &
 nautilus &
 mate-terminal &
+ubuntu-sesstings &
 sudo cat xstartup.sh > xstartup 
 vncserver -alwaysshared
 
