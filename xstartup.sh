47159e51bfd1e5073b68ba1a44da3a6888931269
amitstudydude <99377817+amitstudydude@users.noreply.github.com>
2022-06-05 20:09:14 +0530

    Update xstartup.sh

--- xstartup.sh
+++ xstartup.sh
@@ -5,9 +5,6 @@ sudo apt install ubuntu-desktop gnome-session-flashback nautilus mate-terminal g
 unset SESSION_MANAGER
 unset DBUS_SESSION_BUS_ADDRESS
 chmod +x xstartup
-[ -x /etc/vnc/xstartup ] && exec /etc/vnc/xstartup
-[ -r $HOME/.Xresources ] && xrdb $HOME/.Xresources
-xsetroot -solid grey
 export XKL_XMODMAP_DISABLE=1 
 export XDG_CURRENT_DESKTOP="GNOME-Flashback:GNOME" 
 export XDG_MENU_PREFIX="gnome-flashback-" 
