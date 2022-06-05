96907bc49aa234726c5587eb5940539766291941
amitstudydude <99377817+amitstudydude@users.noreply.github.com>
2022-06-05 16:48:26 +0530

    Update xstartup.sh

--- xstartup.sh
+++ xstartup.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
+export XKL_XMODMAP_DISABLE=1
+unset SESSION_MANAGER
+unset DBUS_SESSION_BUS_ADDRESS
 gnome-panel &
 gnome-settings-daemon &
 metacity &
 nautilus &
-    
+gnome-terminal &
