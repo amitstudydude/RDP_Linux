aa7804fe0ff1ea23894939164ac813f1c4e8a37b
amitstudydude <99377817+amitstudydude@users.noreply.github.com>
2022-06-05 21:19:47 +0530

    Update xstarup.sh

--- xstarup.sh
+++ xstarup.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 apt-get update && apt-get -y upgrade && apt-get -y dist-upgrade 
-#sudo apt install tightvncserver expect xfce4-session lightdm
-sudo apt-get install indicator-applet-complete
+sudo apt install tightvncserver expect xfce4-session lightdm
+#sudo apt-get install indicator-applet-complete
 sudo apt install ubuntu-desktop gnome-session-flashback nautilus mate-terminal gnome-panel  gnome-settings-daemon metacity
-unset SESSION_MANAGER
-unset DBUS_SESSION_BUS_ADDRESS
+#unset SESSION_MANAGER
+#unset DBUS_SESSION_BUS_ADDRESS
 chmod +x xstartup
-export XKL_XMODMAP_DISABLE=1 
-export XDG_CURRENT_DESKTOP="GNOME-Flashback:GNOME" 
-export XDG_MENU_PREFIX="gnome-flashback-" 
-gnome-session -session=gnome-flashback-metacity -disable-acceleration-check --debug &
-gnome-panel &
-vncconfig -iconic &
-gsettings set org.gnome.desktop.background show-desktop-icons true &
-metacity &
-nautilus &
-#startxfce4 &
-mate-terminal &
+#export XKL_XMODMAP_DISABLE=1 
+#export XDG_CURRENT_DESKTOP="GNOME-Flashback:GNOME" 
+#export XDG_MENU_PREFIX="gnome-flashback-" 
+#gnome-session -session=gnome-flashback-metacity -disable-acceleration-check --debug &
+#gnome-panel &
+#vncconfig -iconic &
+#gsettings set org.gnome.desktop.background show-desktop-icons true &
+#metacity &
+#nautilus &
+startxfce4 &
+#mate-terminal &
 #ubuntu-sesstings &
 sudo cat xstartup.sh > xstartup 
 vncserver -alwaysshared
