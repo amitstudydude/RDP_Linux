7bd1bcf1d1f8a30a53fbe4330e67481f83cf23b4
amitstudydude <99377817+amitstudydude@users.noreply.github.com>
2022-06-05 08:51:49 +0530

    Update Guac.sh

--- Guac.sh
+++ Guac.sh
@@ -1,16 +1,55 @@
 #! /bin/bash
-cd
-sudo apt install --allow-change-held-packages --fix-broken -y libpulse-dev  libssl-dev libvorbis-dev libwebsockets-dev libvncserver-dev libtelnet-dev libssh2-1-dev libpango1.0-dev freerdp2-dev libavcodec-dev libavformat-dev libavutil-dev   libossp-uuid-dev libtool-bin uuid-dev   libcairo2-dev  && \
- wget https://raw.githubusercontent.com/MysticRyuujin/guac-install/main/guac-install.sh && \
-printf "n\nn\ny\nroot\nroot\nroot\nroot" | bash guac-install.sh 
-sudo systemctl enable guacd
-sudo systemctl start guacd
-sudo systemctl daemon reload
+ 
 
-curl -O https://pagekite.net/pk/pagekite.py
-chmod +x pagekite.py
-sudo mv -f pagekite.py /usr/local/bin
-printf "y\ny\ny0nxj8ze4@mozmail.com\na3z2cfdb\na3z2cfdb\ny" | pagekite.py 6080 vnc-jha.pagekite.me
+cd
+ufw enable && ufw allow 8080 && ufw allow 22
+sudo apt install tomcat9 tomcat9-admin tomcat9-common tomcat9-user
+sudo apt install gcc g++ maven openjdk-8-jdk build-essential libcairo2-dev libjpeg-turbo8-dev \
+    libpng-dev libtool-bin libossp-uuid-dev libvncserver-dev \
+    freerdp2-dev libssh2-1-dev libtelnet-dev libwebsockets-dev \
+    libpulse-dev libvorbis-dev libwebp-dev libssl-dev \
+    libpango1.0-dev libswscale-dev libavcodec-dev libavutil-dev \
+    libavformat-dev
+wget https://downloads.apache.org/guacamole/1.4.0/source/guacamole-server-1.4.0.tar.gz https://downloads.apache.org/guacamole/1.4.0/source/guacamole-client-1.4.0.tar.gz https://downloads.apache.org/guacamole/1.4.0/binary/guacamole-1.4.0.war
+tar -xvf guacamole-server-1.4.0.tar.gz
+tar -xvf guacamole-client-1.4.0.tar.gz
+cd guacamole-server-1.4.0
+sudo ./configure --with-init-dir=/etc/init.d --enable-allow-freerdp-snapshots
+sudo make
+sudo make install
+sudo ldconfig
+sudo systemctl daemon-reload
+sudo systemctl start guacd
+sudo systemctl enable guacd
+sudo mkdir -p /etc/guacamole/{extensions,lib}
+cd
+sudo mv guacamole-1.4.0.war /var/lib/tomcat9/webapps/guacamole.war
+sudo systemctl restart tomcat9 guacd
+cd
+cd guacamole-client-1.4.0.tar.gz
+mvn package
+cat /etc/guacamole/guacamole.properties 
+mkdir /usr/share/tomcat8/.guacomol
+ln -s  /etc/guacamole/guacamole.properties /usr/share/tomcat8/.guacomol
+sudo systemctl daemon-reload
+sudo systemctl restart tomcat9 guacd
+
+
+
+
+
+#sudo apt install --allow-change-held-packages --fix-broken -y libpulse-dev  libssl-dev libvorbis-dev libwebsockets-dev libvncserver-dev libtelnet-dev libssh2-1-dev libpango1.0-dev freerdp2-dev libavcodec-dev libavformat-dev libavutil-dev   libossp-uuid-dev libtool-bin uuid-dev   libcairo2-dev  && \
+#wget https://raw.githubusercontent.com/MysticRyuujin/guac-install/main/guac-install.sh && \
+
+#printf "n\nn\ny\nroot\nroot\nroot\nroot" | bash guac-install.sh 
+#sudo systemctl enable guacd
+#dudo systemctl start guacd
+#sudo systemctl daemon reload
+
+#curl -O https://pagekite.net/pk/pagekite.py
+#chmod +x pagekite.py
+#sudo mv -f pagekite.py /usr/local/bin
+#printf "y\ny\ny0nxj8ze4@mozmail.com\na3z2cfdb\na3z2cfdb\ny" | pagekite.py 6080 vnc-jha.pagekite.me
 
 
 
