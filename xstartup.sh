eec5e37160e0955f540d8d8bfd06875b35759159
amitstudydude <99377817+amitstudydude@users.noreply.github.com>
2022-06-05 17:22:05 +0530

    Update xstartup.sh

--- xstartup.sh
+++ xstartup.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
-export XKL_XMODMAP_DISABLE=1
 unset SESSION_MANAGER
 unset DBUS_SESSION_BUS_ADDRESS
+export XKL_XMODMAP_DISABLE=1 
+export XDG_CURRENT_DESKTOP="GNOME-Flashback:Unity" 
+export XDG_MENU_PREFIX="gnome-flashback-" 
+unset DBUS_SESSION_BUS_ADDRESS 
+gnome-session -session=gnome-flashback-metacity -disable-acceleration-check --debug &
 gnome-panel &
 gnome-settings-daemon &
 metacity &
