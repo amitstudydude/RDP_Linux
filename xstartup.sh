ac7cf7d5a2e5c5e5fe3d1344ba0ed0219aeeebac
amitstudydude <99377817+amitstudydude@users.noreply.github.com>
2022-06-05 18:01:21 +0530

    Update xstartup.sh

--- xstartup.sh
+++ xstartup.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 cd && cd .vnc
 sudo apt install ubuntu-desktop gnome-session-flashback nautilus mate-terminal gnome-panel gnome-settings-daemon metacity
-sudo chmod +x xstartup
 unset SESSION_MANAGER
 unset DBUS_SESSION_BUS_ADDRESS
-
+chmod +x xstartup
 [ -x /etc/vnc/xstartup ] && exec /etc/vnc/xstartup
 [ -r $HOME/.Xresources ] && xrdb $HOME/.Xresources
 xsetroot -solid grey
-
 export XKL_XMODMAP_DISABLE=1 
 export XDG_CURRENT_DESKTOP="GNOME-Flashback:Unity" 
 #export XDG_CURRENT_DESKTOP=ubuntu:GNOME
@@ -20,6 +18,8 @@ gnome-settings-daemon &
 metacity &
 nautilus &
 mate-terminal &
+sudo mv xstartup.sh xstartup 
+
 
 
 
