86efb465c6fefa7b6a68805c10acd376d33fa39d
amitstudydude <99377817+amitstudydude@users.noreply.github.com>
2022-06-06 00:38:58 +0530

    Update getit.sh

--- getit.sh
+++ getit.sh
@@ -83,7 +83,7 @@ lt --port 8080 >> nail.txt &
 #lt --port 6080 >> nail.txt & 
 echo " cat nail.txt && systemctl start gdm3 && systemctl start xrdp &&  sleep 2 && bash bash.sh " >> bash.sh 
 
-bash bash.sh 
+bash bash.sh &
 
 sudo apt-get install build-essential cmake git libjson-c-dev libwebsockets-dev && git clone https://github.com/tsl0922/ttyd.git && git clone https://github.com/novnc/noVNC.git /opt/novnc && git clone https://github.com/novnc/websockify /opt/novnc/utils/websockify && \ 
 cd ttyd && mkdir build && cd build && cmake .. && make && make install && ttyd -p 1234 bash -x & 
