5d2012c18bd71e5e20923ca4d6a23142ecbab339
amitstudydude <99377817+amitstudydude@users.noreply.github.com>
2022-06-05 13:45:40 +0530

    Update Guac.sh

--- Guac.sh
+++ Guac.sh
@@ -1,8 +1,8 @@
 #! /bin/bash
- 
-
-cd
-ufw enable && ufw allow 8080 && ufw allow 22
+cd 
+sudo wget https://raw.githubusercontent.com/MysticRyuujin/guac-install/main/guac-install.sh && \
+printf "n\nn\ny\nroot\nroot\nroot\nroot" | sudo bash guac-install.sh 
+sudo ufw enable && sudo  ufw allow 8080 && sudo ufw allow 22
 sudo apt install tomcat9 tomcat9-admin tomcat9-common tomcat9-user
 sudo apt install gcc g++ maven openjdk-8-jdk build-essential libcairo2-dev libjpeg-turbo8-dev \
     libpng-dev libtool-bin libossp-uuid-dev libvncserver-dev \
@@ -10,12 +10,13 @@ sudo apt install gcc g++ maven openjdk-8-jdk build-essential libcairo2-dev libjp
     libpulse-dev libvorbis-dev libwebp-dev libssl-dev \
     libpango1.0-dev libswscale-dev libavcodec-dev libavutil-dev \
     libavformat-dev
+cd
 wget https://downloads.apache.org/guacamole/1.4.0/source/guacamole-server-1.4.0.tar.gz https://downloads.apache.org/guacamole/1.4.0/source/guacamole-client-1.4.0.tar.gz https://downloads.apache.org/guacamole/1.4.0/binary/guacamole-1.4.0.war
 tar -xvf guacamole-server-1.4.0.tar.gz
 tar -xvf guacamole-client-1.4.0.tar.gz
 cd guacamole-server-1.4.0
-sudo ./configure --with-init-dir=/etc/init.d --enable-allow-freerdp-snapshots
-sudo make && make install 
+sudo ./configure --with-init-dir=/etc/init.d 
+sudo make 
 sudo make install
 sudo ldconfig
 sudo systemctl daemon-reload
@@ -28,21 +29,22 @@ sudo systemctl restart tomcat9 guacd
 cd
 cd guacamole-client-1.4.0
 mvn package
-cat /etc/guacamole/guacamole.properties 
-sudo mkdir /usr/share/tomcat8/.guacomol
+sudo makdir /usr/share/tomcat8/
+sudo touch /usr/share/tomcat8/.guacomol
 ln -s  /etc/guacamole/guacamole.properties /usr/share/tomcat8/.guacomol
 sudo systemctl daemon-reload
 sudo systemctl restart tomcat9 guacd
+sudo apt update && sudo apt install xfce4 xfce4-goodies expect && sudo wget https://raw.githubusercontent.com/amitstudydude/RDP_Linux/main/xvnc.sh && sudo bash xvnc.sh
 
-echo " guacd-hostname: localhost " 
-echo " guacd-port: 4822 "
+
+
+
+
 
 
 
 #sudo apt install --allow-change-held-packages --fix-broken -y libpulse-dev  libssl-dev libvorbis-dev libwebsockets-dev libvncserver-dev libtelnet-dev libssh2-1-dev libpango1.0-dev freerdp2-dev libavcodec-dev libavformat-dev libavutil-dev   libossp-uuid-dev libtool-bin uuid-dev   libcairo2-dev  && \
-#wget https://raw.githubusercontent.com/MysticRyuujin/guac-install/main/guac-install.sh && \
-
-#printf "n\nn\ny\nroot\nroot\nroot\nroot" | sudo bash guac-install.sh 
+#
 #sudo systemctl enable guacd
 #dudo systemctl start guacd
 #sudo systemctl daemon reload
@@ -58,4 +60,11 @@ echo " guacd-port: 4822 "
 
 #chmod +x guac-install.sh
 #./guac-install.sh --mysqlpwd root --guacpwd root --nomfa --installmysql
+
+
+
+
+
+
+
 
