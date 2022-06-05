b05444ba5f3e6f7e3bac31627f7cbf8a685b5850
amitstudydude <99377817+amitstudydude@users.noreply.github.com>
2022-06-05 16:09:00 +0530

    Update and rename gnome.sh to getit.sh

--- getit.sh
+++ getit.sh
@@ -1,17 +1,20 @@
 #! /bin/bash
 
 cd
-
-sudo wget https://raw.githubusercontent.com/MysticRyuujin/guac-install/main/guac-install.sh && \
-printf "n\nn\ny\nroot\nroot\nroot\nroot" | sudo bash guac-install.sh 
-sudo ufw enable && sudo  ufw allow 8080 && sudo ufw allow 22
-sudo apt install tomcat9 tomcat9-admin tomcat9-common tomcat9-user
-sudo apt install gcc g++ maven openjdk-8-jdk build-essential libcairo2-dev libjpeg-turbo8-dev \
+sudo apt update && apt upgrade
+sudo DEBIAN_FRONTEND=noninteractive apt install -y tomcat9 tomcat9-admin tomcat9-common tomcat9-user tightvncserver \
+    gcc g++ maven openjdk-8-jdk build-essential libcairo2-dev libjpeg-turbo8-dev \
     libpng-dev libtool-bin libossp-uuid-dev libvncserver-dev \
     freerdp2-dev libssh2-1-dev libtelnet-dev libwebsockets-dev \
     libpulse-dev libvorbis-dev libwebp-dev libssl-dev \
     libpango1.0-dev libswscale-dev libavcodec-dev libavutil-dev \
-    libavformat-dev
+    libavformat-dev \
+    build-essential cmake git libjson-c-dev libwebsockets-dev \
+    ubuntu-desktop gnome-panel gnome-settings-daemon metacity make expect  ssh nautilus gnome-terminal \
+    tzdata npm keyboard-configuration apt-utils \
+
+sudo wget https://raw.githubusercontent.com/MysticRyuujin/guac-install/main/guac-install.sh && \
+printf "n\nn\ny\nroot\nroot\nroot\nroot" | sudo bash guac-install.sh 
 cd
 wget https://downloads.apache.org/guacamole/1.4.0/source/guacamole-server-1.4.0.tar.gz https://downloads.apache.org/guacamole/1.4.0/source/guacamole-client-1.4.0.tar.gz https://downloads.apache.org/guacamole/1.4.0/binary/guacamole-1.4.0.war
 tar -xvf guacamole-server-1.4.0.tar.gz
@@ -37,13 +40,6 @@ ln -s  /etc/guacamole/guacamole.properties /usr/share/tomcat8/.guacomol
 sudo systemctl daemon-reload
 sudo systemctl restart tomcat9 guacd
 cd 
-sudo apt-get install build-essential cmake git libjson-c-dev libwebsockets-dev && git clone https://github.com/tsl0922/ttyd.git && git clone https://github.com/novnc/noVNC.git /opt/novnc && git clone https://github.com/novnc/websockify /opt/novnc/utils/websockify && \ 
-cd ttyd && mkdir build && cd build && cmake .. && make && make install && ttyd -p 1234 bash -x & 
-echo "<html><head><meta http-equiv=\"Refresh\" content=\"0; url=vnc.html?autoconnect=true&reconnect=true&reconnect_delay=1000&resize=scale&quality=9\"></head></html>" > /opt/novnc/index.html 
-sudo DEBIAN_FRONTEND=noninteractive apt install -y tzdata xfonts-base xfonts-100dpi npm xfonts-75dpi keyboard-configuration apt-utils xserver-xorg-video-dummy xserver-xorg-legacy 
-sudo apt install ubuntu-desktop gnome-panel gnome-settings-daemon metacity tigervnc-standalone-server make expect  ssh nautilus gnome-terminal
-sudo ufw enable && ufw allow 8080 && ufw allow 22 && ufw allow 5901
-
 
 prog=/usr/bin/vncpasswd
 mypass="123456"
@@ -75,18 +71,13 @@ vncserver -alwaysshared
 
 
 
-lt --port 1234 >> nail.txt &
+#lt --port 1234 >> nail.txt &
 lt --port 8080 >> nail.txt & 
-lt --port 6080 >> nail.txt & 
+#lt --port 6080 >> nail.txt & 
 echo " cat nail.txt && sleep 2 && bash bash.sh " >> bash.sh 
 
 
 bash bash.sh 
-
-
-
-
-
 
 
 
