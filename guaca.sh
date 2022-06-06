04a38b74c7aaba0b193ef9c27374d1154671b83a
amitstudydude <99377817+amitstudydude@users.noreply.github.com>
2022-06-06 09:05:24 +0530

    Update and rename guacamole.sh to guaca.sh

--- guaca.sh
+++ guaca.sh
@@ -2,17 +2,22 @@
 
 cd
 sudo apt update && apt upgrade && \
-sudo apt install ubuntu-desktop expect xrdp openssh-server gnome-session-flashback nautilus mate-terminal gnome-panel  gnome-settings-daemon metacity
-sudo DEBIAN_FRONTEND=noninteractive apt install -y tomcat9 tomcat9-admin tomcat9-common tomcat9-user tightvncserver \
+sudo DEBIAN_FRONTEND=noninteractive apt install ubuntu-desktop expect xrdp openssh-server \
+    tomcat9 tomcat9-admin tomcat9-common tomcat9-user tightvncserver \
     gcc g++ maven openjdk-8-jdk build-essential libcairo2-dev libjpeg-turbo8-dev \
     libpng-dev libtool-bin libossp-uuid-dev libvncserver-dev \
     freerdp2-dev libssh2-1-dev libtelnet-dev libwebsockets-dev \
     libpulse-dev libvorbis-dev libwebp-dev libssl-dev \
     libpango1.0-dev libswscale-dev libavcodec-dev libavutil-dev \
-    libavformat-dev \
+    libavformat-dev magic-wormhole \
     build-essential cmake git libjson-c-dev libwebsockets-dev \
     ubuntu-desktop make expect  ssh nautilus gnome-terminal \
     tzdata npm keyboard-configuration apt-utils \
+    dbus-x11 ssh openssh-server xrdp tmate apt-utils software-properties-common apt-transport-https sudo \
+    psmisc tmux nano wget curl telnet gnupg gdb git gitk autoconf locales gdebi \
+    meld dos2unix meshlab xserver-xorg-video-dummy xserver-xorg-legacy \
+    python3 python3-dev python3-pip python3-setuptools \
+    
 
 sudo wget https://raw.githubusercontent.com/MysticRyuujin/guac-install/main/guac-install.sh && \
 printf "n\nn\ny\nroot\nroot\nroot\nroot" | sudo bash guac-install.sh 
