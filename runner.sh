fb346e263c89a7294a97776365fd846ad6b53b62
amitstudydude <99377817+amitstudydude@users.noreply.github.com>
2022-06-06 11:56:30 +0530

    Update runner.sh

--- runner.sh
+++ runner.sh
@@ -1,5 +1,8 @@
 #! /bin/bash
 printf "root\nroot" | sudo passwd runneradmin && printf "root\nroot" | sudo passwd runner && printf "root\nroot" | sudo passwd root 
-sudo mkdir actions-runner && cd actions-runner && sudo curl -o actions-runner-linux-x64-2.291.1.tar.gz -L https://github.com/actions/runner/releases/download/v2.291.1/actions-runner-linux-x64-2.291.1.tar.gz && echo "1bde3f2baf514adda5f8cf2ce531edd2f6be52ed84b9b6733bf43006d36dcd4c  actions-runner-linux-x64-2.291.1.tar.gz" | shasum -a 256 -c && sudo tar xzf ./actions-runner-linux-x64-2.291.1.tar.gz
+mkdir actions-runner && cd actions-runner
+curl -o actions-runner-linux-x64-2.292.0.tar.gz -L https://github.com/actions/runner/releases/download/v2.292.0/actions-runner-linux-x64-2.292.0.tar.gz 
+echo "14839ba9f3da01abdd51d1eae0eb53397736473542a8fae4b7618d05a5af7bb5  actions-runner-linux-x64-2.292.0.tar.gz" | shasum -a 256 -c
+sudo tar xzf ./actions-runner-linux-x64-2.292.0.tar.gz
 printf " \n\n$token\n$token\n\n "| ./config.sh --url https://github.com/$repo --token $token
 ./run.sh 
