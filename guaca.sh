664ba8b58c1681a0a8ab7e547e37c18542e21120
amitstudydude <99377817+amitstudydude@users.noreply.github.com>
2022-06-06 15:02:52 +0530

    Update guaca.sh

--- guaca.sh
+++ guaca.sh
@@ -43,12 +43,13 @@ sudo npm install -g localtunnel
 
 rm page*
 wget https://raw.githubusercontent.com/amitstudydude/RDP_Linux/main/page.sh https://raw.githubusercontent.com/amitstudydude/RDP_Linux/main/page.service
-sudo cp page.service /etc/systemd/system/
-sudo cp page.sh /usr/local/sbin/page.sh
+sudo cat page.service > /etc/systemd/system/page.service
+sudo cat page.sh > /usr/local/sbin/page.sh
 sudo chmod +x /usr/local/sbin/page.sh
 sudo systemctl daemon-reload
 sudo systemctl enable page.service 
 sudo systemctl restart page.service 
+sudo systemctl status page.service 
 
 
 lt --port 8080 >> nail.txt &
