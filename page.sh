5299ed45215d0c27ce01fc691b598ca31797e3bc
amitstudydude <99377817+amitstudydude@users.noreply.github.com>
2022-06-06 08:22:47 +0530

    Create page.sh

---
+++ page.sh
@@ -0,0 +1,14 @@
+
+
+>>> Creating kite: jha.pagekite.me                            [CTRL+C = Cancel]
+
+    Welcome to PageKite!
+    
+    Please answer a few quick questions to
+    create your first kite.
+    
+    By continuing, you agree to play nice
+    and abide by the Terms of Service at:
+    - https://pagekite.net/humans.txt
+
+ => Use the PageKite.net service? [Y/n]
