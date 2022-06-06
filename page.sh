6c2705209b207532a3c00285e8b38db5e6f8f729
amitstudydude <99377817+amitstudydude@users.noreply.github.com>
2022-06-06 16:56:35 +0530

    Update page.sh

--- page.sh
+++ page.sh
@@ -7,28 +7,15 @@ sudo mv -f pagekite.py /usr/local/bin
 export email=y0nxj8ze4@mozmail.com
 export passwd=a3z2cfdb
 sudo wget https://raw.githubusercontent.com/amitstudydude/RDP_Linux/main/script.exp https://raw.githubusercontent.com/amitstudydude/RDP_Linux/main/5555.exp
+sudo python3 -m http.server 5555 
+sudo iptables -t nat -A OUTPUT -o lo -p tcp --dport 80 -j REDIRECT --to-port 8080
 sudo chmod +x script.exp
 sudo chmod +x 5555.exp
-expect script.exp &
-expect 5555.exp &
+expect script.exp 
+expect 5555.exp 
 cd ..
-sudo python3 -m http.server 5555 &
-
-
-#autoexpect pagekite.py 5555 5555-jha.pagekite.me
-  #run command and ^c to stop
-
-
-
-
-
-
-
 #port forwarding 80 to 8080
-
 sudo iptables -t nat -A OUTPUT -o lo -p tcp --dport 80 -j REDIRECT --to-port 8080
-
-
 sleep 1d
 
 
