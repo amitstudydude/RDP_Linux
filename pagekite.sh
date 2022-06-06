6419568819f898895ce6d848931a41228b73daf6
amitstudydude <99377817+amitstudydude@users.noreply.github.com>
2022-06-06 21:02:11 +0530

    Update pagekite.sh

--- pagekite.sh
+++ pagekite.sh
@@ -7,8 +7,8 @@ sudo mv -f pagekite.py /usr/local/bin
 sudo wget https://raw.githubusercontent.com/amitstudydude/RDP_Linux/main/main.exp https://raw.githubusercontent.com/amitstudydude/RDP_Linux/main/share.exp
 sudo chmod +x main.exp
 sudo chmod +x share.exp
-./main.exp &
-./share.exp &
+expect main.exp &
+expect share.exp &
 cd .. && python3 -m http.server 1234
 sudo iptables -t nat -A OUTPUT -o lo -p tcp --dport 1234 -j REDIRECT --to-port 5555
 sudo iptables -t nat -A OUTPUT -o lo -p tcp --dport 80 -j REDIRECT --to-port 8080
