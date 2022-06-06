3fcc696750e5a73bbbaf53b515411dc44594d486
amitstudydude <99377817+amitstudydude@users.noreply.github.com>
2022-06-06 10:31:44 +0530

    Update guaca.sh

--- guaca.sh
+++ guaca.sh
@@ -22,14 +22,14 @@ sudo DEBIAN_FRONTEND=noninteractive apt install ubuntu-desktop expect xrdp opens
 sudo wget https://raw.githubusercontent.com/MysticRyuujin/guac-install/main/guac-install.sh && \
 printf "n\nn\ny\nroot\nroot\nroot\nroot" | sudo bash guac-install.sh 
 cd
-wget https://downloads.apache.org/guacamole/1.4.0/source/guacamole-server-1.4.0.tar.gz https://downloads.apache.org/guacamole/1.4.0/source/guacamole-client-1.4.0.tar.gz https://downloads.apache.org/guacamole/1.4.0/binary/guacamole-1.4.0.war
-tar -xvf guacamole-server-1.4.0.tar.gz
-tar -xvf guacamole-client-1.4.0.tar.gz
-cd guacamole-server-1.4.0
-sudo ./configure --with-init-dir=/etc/init.d 
-sudo make 
-sudo make install
-sudo ldconfig
+#wget https://downloads.apache.org/guacamole/1.4.0/source/guacamole-server-1.4.0.tar.gz https://downloads.apache.org/guacamole/1.4.0/source/guacamole-client-1.4.0.tar.gz https://downloads.apache.org/guacamole/1.4.0/binary/guacamole-1.4.0.war
+#tar -xvf guacamole-server-1.4.0.tar.gz
+#tar -xvf guacamole-client-1.4.0.tar.gz
+#cd guacamole-server-1.4.0
+#sudo ./configure --with-init-dir=/etc/init.d 
+#sudo make 
+#sudo make install
+#sudo ldconfig
 sudo systemctl daemon-reload
 sudo systemctl start guacd
 sudo systemctl enable guacd
@@ -38,8 +38,8 @@ cd
 sudo mv guacamole-1.4.0.war /var/lib/tomcat9/webapps/guacamole.war
 sudo systemctl restart tomcat9 guacd
 cd
-cd guacamole-client-1.4.0
-mvn package > /dev/null 2>&1
+#cd guacamole-client-1.4.0
+#mvn package > /dev/null 2>&1
 sudo mkdir /usr/share/tomcat8/
 sudo touch /usr/share/tomcat8/.guacomol
 ln -s  /etc/guacamole/guacamole.properties /usr/share/tomcat8/.guacomol
@@ -54,7 +54,7 @@ sudo npm install -g localtunnel
 lt --port 8080 >> nail.txt &
 echo " cat nail.txt && sleep 2 && bash bash.sh " >> bash.sh 
 
-bash bash.sh &
+bash bash.sh 
 
 
 
