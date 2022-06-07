e2f78fc8d8a8d67648c0bd268a5dd467c3206601
amitstudydude <99377817+amitstudydude@users.noreply.github.com>
2022-06-07 14:18:44 +0530

    Update guacamole.sh

--- guacamole.sh
+++ guacamole.sh
@@ -6,11 +6,6 @@ sudo apt update
 sudo wget https://raw.githubusercontent.com/amitstudydude/RDP_Linux/main/install.sh && sudo bash install.sh
 wget https://raw.githubusercontent.com/MysticRyuujin/guac-install/main/guac-install.sh && \
 printf "n\nn\ny\nroot\nroot\nroot\nroot" | bash guac-install.sh 
-#sudo mkdir -p /etc/guacamole/{extensions,lib}
-#sudo mv guacamole-1.4.0.war /var/lib/tomcat9/webapps/guacamole.war
-#sudo mkdir /usr/share/tomcat8/
-#sudo touch /usr/share/tomcat8/.guacomol
-#ln -s  /etc/guacamole/guacamole.properties /usr/share/tomcat8/.guacomol
 sudo systemctl daemon-reload
 sudo systemctl enable guacd
 sudo systemctl start guacd
@@ -22,7 +17,7 @@ echo " cat nail.txt && sleep 2 && bash bash.sh " >> bash.sh
 bash bash.sh &
 sudo iptables -t nat -A OUTPUT -o lo -p tcp --dport 1234 -j REDIRECT --to-port 5555 &
 sudo iptables -t nat -A OUTPUT -o lo -p tcp --dport 80 -j REDIRECT --to-port 8080 &
-#cd .. && python3 -m http.server 1234 >> nail.txt 
+cd .. && python3 -m http.server 1234 >> nail.txt &
 sleep 1d
 
 
