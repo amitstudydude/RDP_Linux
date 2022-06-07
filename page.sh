c9aaced73a06a642e8828bd8bc6e8e7529ad49cb
amitstudydude <99377817+amitstudydude@users.noreply.github.com>
2022-06-07 12:08:27 +0530

    Update page.sh

--- page.sh
+++ page.sh
@@ -2,9 +2,8 @@
 cd /root
 sudo apt update 
 sudo apt install curl iptables expect
-cd .. && python3 -m http.server 1234 &
-sudo iptables -t nat -A OUTPUT -o lo -p tcp --dport 1234 -j REDIRECT --to-port 5555
-sudo iptables -t nat -A OUTPUT -o lo -p tcp --dport 80 -j REDIRECT --to-port 8080
+sudo iptables -t nat -A OUTPUT -o lo -p tcp --dport 1234 -j REDIRECT --to-port 5555 &
+sudo iptables -t nat -A OUTPUT -o lo -p tcp --dport 80 -j REDIRECT --to-port 8080 &
 #sudo rm $(which pagekite.py) $(which pagekite-gtk.py)
 cd /root
 sudo curl -O https://pagekite.net/pk/pagekite.py
@@ -14,6 +13,7 @@ sudo wget https://raw.githubusercontent.com/amitstudydude/RDP_Linux/main/main.ex
 sudo chmod +x main.exp
 sudo chmod +x share.exp
 expect main.exp &
+cd .. && python3 -m http.server 1234 &
 sleep 30
 ./share.exp &
 
