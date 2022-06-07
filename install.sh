05028a7afd57a142fdf932e0d99a38b0c01e86c6
amitstudydude <99377817+amitstudydude@users.noreply.github.com>
2022-06-07 13:55:32 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -61,4 +61,4 @@ sudo DEBIAN_FRONTEND=noninteractive apt -y install python3-setuptools
 sudo DEBIAN_FRONTEND=noninteractive apt -y install gcc 
 sudo DEBIAN_FRONTEND=noninteractive apt -y install g++ 
 sudo DEBIAN_FRONTEND=noninteractive apt -y install openjdk-8-jdk
-sudo DEBIAN_FRONTEND=noninteractive apt -y install ubuntu-desktop 
+#sudo DEBIAN_FRONTEND=noninteractive apt -y install ubuntu-desktop 
