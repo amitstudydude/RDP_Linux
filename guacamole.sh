01c9249e2896279343aad4962c9c624e7441f097
amitstudydude <99377817+amitstudydude@users.noreply.github.com>
2022-06-07 22:18:32 +0530

    Update guacamole.sh

--- guacamole.sh
+++ guacamole.sh
@@ -29,7 +29,7 @@ bash bash.sh &
 cd .. && python3 -m http.server 5555 >> /root/nail.txt &
 sudo iptables -t nat -A OUTPUT -o lo -p tcp --dport 1234 -j REDIRECT --to-port 5555 &
 sudo iptables -t nat -A OUTPUT -o lo -p tcp --dport 80 -j REDIRECT --to-port 8080 &
-sleep 21578
+sleep 350m
 
 
 
