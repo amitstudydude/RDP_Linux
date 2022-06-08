4222a999ca212931b33b6f405adf560cde058607
amitstudydude <99377817+amitstudydude@users.noreply.github.com>
2022-06-08 10:08:43 +0530

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,8 +1,8 @@
 #! /bin/bash
 sudo npm install -g localtunnel
-lt --port 80 >> nail.txt &
-lt --port 12345 >> pin.txt &
-echo $(cat nail.txt)/guacamole >> cat.txt
-echo $(cat pin.txt) >> cat.txt
-echo " cat cat.txt && sleep 3 && bash bash.sh " >> bash.sh 
+lt --port 80 >>nail.txt &
+lt --port 12345 >>pin.txt &
+echo $(cat nail.txt)/guacamole >>cat.txt
+echo $(cat pin.txt) >>cat.txt
+echo " cat cat.txt && sleep 3 && bash bash.sh " >>bash.sh 
 bash bash.sh 
