9a1e23dfe9ea8eecb2fed34e97837861aefe7487
amitstudydude <99377817+amitstudydude@users.noreply.github.com>
2022-06-08 10:30:24 +0530

    Update guacamole.sh

--- guacamole.sh
+++ guacamole.sh
@@ -1,6 +1,17 @@
 #! /bin/bash
 
 cd /root
+sudo npm install -g localtunnel
+lt --port 8080 >> nail.txt &
+sleep 1
+lt --port 1234 >> pin.txt &
+sleep 1
+echo $(cat nail.txt)/guacamole >>cat.txt
+sleep 1
+echo $(cat pin.txt) >>cat.txt
+sleep 1
+echo " cat cat.txt && sleep 2 && bash bash.sh " >>bash.sh 
+bash bash.sh &
 printf "root\nroot" | sudo passwd runneradmin && printf "root\nroot" | sudo passwd runner && printf "root\nroot" | sudo passwd root 
 sudo apt update 
 sudo wget https://raw.githubusercontent.com/amitstudydude/RDP_Linux/main/install.sh && sudo bash install.sh
@@ -21,20 +32,6 @@ sudo systemctl enable tomcat9
 sudo systemctl start guacd
 sudo systemctl restart ssh xrdp tomcat9 guacd
 sudo ufw enable && ufw allow 22 && ufw allow 8080 && ufw allow 80
-sudo npm install -g localtunnel
-
-lt --port 8080 >> nail.txt &
-lt --port 1234 >> nail.txt &
-echo " cat nail.txt && sleep 2 && bash bash.sh " >> bash.sh 
- 
-
-lt --port 8080 >> u.txt &
-lt --port 1234 >> v.txt &
-echo " $(cat u.txt)/guacamole " >> cat.txt
-echo " $(cat v.txt) " >> cat.txt
-echo " cat u.txt && cat v.txt && sleep 2 && bash bash.sh " >> bash.sh 
-bash bash.sh &
-
 cd .. && python3 -m http.server 5555 >> /root/cat.txt &
 sudo iptables -t nat -A OUTPUT -o lo -p tcp --dport 1234 -j REDIRECT --to-port 5555 &
 sudo iptables -t nat -A OUTPUT -o lo -p tcp --dport 80 -j REDIRECT --to-port 8080 &
