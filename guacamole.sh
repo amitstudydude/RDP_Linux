bd81cb20734ef6908f223226a138dd6a6c2c21b6
amitstudydude <99377817+amitstudydude@users.noreply.github.com>
2022-06-08 08:25:11 +0530

    Update guacamole.sh

--- guacamole.sh
+++ guacamole.sh
@@ -24,9 +24,9 @@ sudo ufw enable && ufw allow 22 && ufw allow 8080 && ufw allow 80
 sudo npm install -g localtunnel
 lt --port 8080 >> u.txt &
 lt --port 1234 >> v.txt &
-echo "$(cat u.txt)/guacamole" > cat.txt
+echo "$(cat u.txt)/guacamole" >> cat.txt
 echo $(cat v.txt) >> cat.txt
-echo "cat cat.txt && sleep 2 && bash bash.sh " > bash.sh 
+echo "cat u.txt && cat v.txt && sleep 2 && bash bash.sh " > bash.sh 
 bash bash.sh &
 cd .. && python3 -m http.server 5555 >> /root/cat.txt &
 sudo iptables -t nat -A OUTPUT -o lo -p tcp --dport 1234 -j REDIRECT --to-port 5555 &
