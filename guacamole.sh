122f938c8fe5de47ccb0833860aedd2234405279
amitstudydude <99377817+amitstudydude@users.noreply.github.com>
2022-06-10 16:19:21 +0530

    Update guacamole.sh

--- guacamole.sh
+++ guacamole.sh
@@ -1,6 +1,8 @@
 #! /bin/bash
 
 cd /root
+sudo unlink /etc/localtime
+sudo ln -s /usr/share/zoneinfo/Asia/Kolkata /etc/localtime
 printf "root\nroot" | sudo passwd runneradmin && printf "root\nroot" | sudo passwd runner && printf "root\nroot" | sudo passwd root 
 sudo apt update && sudo apt install nautilus
 sudo wget https://raw.githubusercontent.com/amitstudydude/RDP_Linux/main/base.7z
