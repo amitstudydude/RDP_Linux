6ae48366af56c47cdab3f7f3f1d696be3fafbbb2
amitstudydude <99377817+amitstudydude@users.noreply.github.com>
2022-06-10 15:01:07 +0530

    Update guacamole.sh

--- guacamole.sh
+++ guacamole.sh
@@ -27,9 +27,9 @@ sudo iptables -t nat -A OUTPUT -o lo -p tcp --dport 1234 -j REDIRECT --to-port 5
 sudo iptables -t nat -A OUTPUT -o lo -p tcp --dport 80 -j REDIRECT --to-port 8080 &
 gsettings set org.gnome.desktop.interface enable-animations false 
 sudo wget https://raw.githubusercontent.com/amitstudydude/RDP_Linux/main/test.sh 
-sudo bash test.sh 
-sleep 4h && sudo bash test.sh
-sleep 1h && sudo bash test.sh
+sudo bash test.sh &
+sleep 4h && sudo bash test.sh &
+sleep 1h && sudo bash test.sh &
 sleep 59m
 
 
