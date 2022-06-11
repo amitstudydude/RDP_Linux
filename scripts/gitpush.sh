a7c304cef3a78a0b5a1a715ad2021c44c09d0e1e
amitstudydude <99377817+amitstudydude@users.noreply.github.com>
2022-06-11 19:06:56 +0530

    Update gitpush.sh

--- scripts/gitpush.sh
+++ scripts/gitpush.sh
@@ -1,12 +1,24 @@
-rm -rf gitpush.sh
+rm -rf *.sh
+sleep 5 
 cd .github/workflows
+sleep 5
 sudo unlink /etc/localtime
+sleep 5
 sudo ln -s /usr/share/zoneinfo/Asia/Kolkata /etc/localtime
-date >> .md
+sleep 5
+date > .md
+sleep 5
 git config --global user.name 'amitstudydude'
+sleep 5
 git config --global user.email 'amitstudydude@users.noreply.github.com'
+sleep 5
 git remote set-url origin https://x-access-token:$token@github.com/$repo
+sleep 5
 git checkout "$ref"
+sleep 5
 git add -A
+sleep 5
 git commit -am "Automated report"
+sleep 5
 git push origin main
+sleep 4h
