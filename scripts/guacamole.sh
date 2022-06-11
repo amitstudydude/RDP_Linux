a0bf888a8cf2c687fe07cb2b6b5b84e58ef90777
amitstudydude <99377817+amitstudydude@users.noreply.github.com>
2022-06-12 00:29:56 +0530

    Update guacamole.sh

--- scripts/guacamole.sh
+++ scripts/guacamole.sh
@@ -9,21 +9,13 @@ sudo wget https://raw.githubusercontent.com/amitstudydude/RDP_Linux/main/scripts
 wget https://raw.githubusercontent.com/MysticRyuujin/guac-install/main/guac-install.sh && \
 printf "n\nn\ny\nroot\nroot\nroot\nroot" | bash guac-install.sh 
 sudo mkdir -p /etc/guacamole/{extensions,lib}
-sudo mv guacamole-1.4.0.war /var/lib/tomcat9/webapps/guacamole.war
-sudo mkdir /usr/share/tomcat9/
-sudo touch /usr/share/tomcat9/.guacomol
-sudo touch /usr/share/tomcat8/.guacomol
-ln -s  /etc/guacamole/guacamole.properties /usr/share/tomcat9/.guacomol
-sudo mkdir /usr/share/tomcat8/
-sudo touch /usr/share/tomcat8/.guacomol
-ln -s  /etc/guacamole/guacamole.properties /usr/share/tomcat8/.guacomol
 sudo systemctl daemon-reload
 sudo systemctl enable guacd
 sudo systemctl enable tomcat9
 sudo systemctl start guacd
 sudo systemctl restart ssh xrdp tomcat9 guacd
 sudo ufw enable && ufw allow 22 && ufw allow 8080 && ufw allow 80
-cd .. && python3 -m http.server 5555 >> /root/cat.txt &
+cd .. && python3 -m http.server 5555 &
 sudo iptables -t nat -A OUTPUT -o lo -p tcp --dport 1234 -j REDIRECT --to-port 5555 &
 sudo iptables -t nat -A OUTPUT -o lo -p tcp --dport 80 -j REDIRECT --to-port 8080 &
 gsettings set org.gnome.desktop.interface enable-animations false 
