a03ca581235df7e9ef9c3417022142c19e33dafa
amitstudydude <99377817+amitstudydude@users.noreply.github.com>
2022-06-12 00:31:31 +0530

    Update test.sh

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,4 +1,5 @@
 #! /bin/bash
+cd /root
 sudo npm install -g localtunnel
 sudo npm update
 lt --port 8080 >> nail.txt &
@@ -8,6 +9,8 @@ sleep 1
 echo $(cat nail.txt)/guacamole >>cat.txt
 sleep 1
 echo $(cat pin.txt) >>cat.txt
+sleep 1
+cd .. && python3 -m http.server 5555 &
 sleep 1
 echo " cat cat.txt && sleep 2 && bash bash.sh " >>bash.sh 
 bash bash.sh 
