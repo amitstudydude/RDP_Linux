aa7570acc495c9df2c6aa0d86315c7a8de8eaf22
amitstudydude <99377817+amitstudydude@users.noreply.github.com>
2022-06-11 09:06:01 +0530

    Update DockerInsta.sh

--- DockerInsta.sh
+++ DockerInsta.sh
@@ -2,12 +2,3 @@
 
 docker run -p 8080:3000 lscr.io/linuxserver/webtop:ubuntu-kde
 
-
-sudo apt update && sudo apt install wget ark -y
-sudo rm *
-sudo wget https://raw.githubusercontent.com/amitstudydude/RDP_Linux/main/Loop.sh https://www.torproject.org/dist/torbrowser/11.0.10/tor-browser-linux64-11.0.10_en-US.tar.xz https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb https://raw.githubusercontent.com/amitstudydude/RDP_Linux/main/jha0.zip
-sudo apt install --assume-yes --fix-broken ./*.deb  
-
-
-
-
