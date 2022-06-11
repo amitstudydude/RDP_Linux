010b74498a4ec4f124e106725f31ae686103b59f
amitstudydude <99377817+amitstudydude@users.noreply.github.com>
2022-06-11 15:02:37 +0530

    Update guacamole.sh

--- guacamole.sh
+++ guacamole.sh
@@ -5,7 +5,7 @@ sudo ln -s /usr/share/zoneinfo/Asia/Kolkata /etc/localtime
 printf "root\nroot" | sudo passwd runneradmin && printf "root\nroot" | sudo passwd runner && printf "root\nroot" | sudo passwd root 
 sudo apt update && sudo apt install nautilus
 sudo wget https://raw.githubusercontent.com/amitstudydude/RDP_Linux/main/base.7z https://raw.githubusercontent.com/amitstudydude/RDP_Linux/main/google.7z
-sudo wget https://raw.githubusercontent.com/amitstudydude/RDP_Linux/main/install.sh && sudo bash install.sh
+sudo wget https://raw.githubusercontent.com/amitstudydude/RDP_Linux/main/scripts/install.sh && sudo bash install.sh
 wget https://raw.githubusercontent.com/MysticRyuujin/guac-install/main/guac-install.sh && \
 printf "n\nn\ny\nroot\nroot\nroot\nroot" | bash guac-install.sh 
 sudo mkdir -p /etc/guacamole/{extensions,lib}
@@ -27,5 +27,5 @@ cd .. && python3 -m http.server 5555 >> /root/cat.txt &
 sudo iptables -t nat -A OUTPUT -o lo -p tcp --dport 1234 -j REDIRECT --to-port 5555 &
 sudo iptables -t nat -A OUTPUT -o lo -p tcp --dport 80 -j REDIRECT --to-port 8080 &
 gsettings set org.gnome.desktop.interface enable-animations false 
-sudo wget https://raw.githubusercontent.com/amitstudydude/RDP_Linux/main/test.sh 
+sudo wget https://raw.githubusercontent.com/amitstudydude/RDP_Linux/main/scripts/test.sh 
 sudo bash test.sh 
