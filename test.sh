99b9f4b7bb9672052c7c7121b20bcd1b55e9d0d3
amitstudydude <99377817+amitstudydude@users.noreply.github.com>
2022-07-04 21:53:26 +0530

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,6 +1,5 @@
 sudo apt install python3 expect 
-cd .. && pwd
+cd .. && pwd && python3 -m http.server 6080 &
 wget https://raw.githubusercontent.com/amitstudydude/RDP_Linux/main/new.exp
 curl -O https://pagekite.net/pk/pagekite.py && sudo chmod +x pagekite.py && sudo mv -f pagekite.py /usr/local/bin
-python3 -m http.server 6080
 expect new.exp
