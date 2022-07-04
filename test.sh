9961bf38080e4e6841bcec0c0cbeccc7e3ba4f11
amitstudydude <99377817+amitstudydude@users.noreply.github.com>
2022-07-04 21:59:49 +0530

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,4 +1,6 @@
-sudo apt install python3 expect 
+sudo apt install python3 expect ufw
+sudo ufw enable 
+sudo ufw allow 6080
 cd .. && pwd && python3 -m http.server 6080 &
 wget https://raw.githubusercontent.com/amitstudydude/RDP_Linux/main/new.exp
 curl -O https://pagekite.net/pk/pagekite.py && sudo chmod +x pagekite.py && sudo mv -f pagekite.py /usr/local/bin
