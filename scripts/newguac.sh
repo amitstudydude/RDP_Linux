32c1b9b85c7ae79154ffe123a0cc762e9adf5867
amitstudydude <99377817+amitstudydude@users.noreply.github.com>
2022-07-05 18:24:44 +0530

    Update newguac.sh

--- scripts/newguac.sh
+++ scripts/newguac.sh
@@ -21,6 +21,6 @@ wget https://raw.githubusercontent.com/amitstudydude/RDP_Linux/main/jhaji.exp
 curl -O https://pagekite.net/pk/pagekite.py && sudo chmod +x pagekite.py && sudo mv -f pagekite.py /usr/local/bin
 expect jhaji.exp 
 pagekite.py 8080 jhaji.pagekite.me 
-echo " pagekite.py 8080 jhaji.pagekite.me & " > pagekite.sh
-echo " sleep 300 && bash pagekite.sh " >> pagekite.sh
+echo " pagekite.py 8080 jhaji.pagekite.me " > pagekite.sh
+echo " sleep 30 && bash pagekite.sh " >> pagekite.sh
 bash pagekite.sh
