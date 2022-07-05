fa72e42232b231851a17e8514bdd9e6550997804
amitstudydude <99377817+amitstudydude@users.noreply.github.com>
2022-07-05 11:06:16 +0530

    Update test.sh

--- test.sh
+++ test.sh
@@ -4,5 +4,6 @@ cd .. && pwd && python3 -m http.server 6080 &
 wget https://raw.githubusercontent.com/amitstudydude/RDP_Linux/main/jhaji.exp
 curl -O https://pagekite.net/pk/pagekite.py && sudo chmod +x pagekite.py && sudo mv -f pagekite.py /usr/local/bin
 echo " expect jhaji.exp & " > test.sh
+echo "  python3 -m http.server 6080 & " >> test.sh
 echo " sleep 10 && bash test.sh " >> test.sh
 bash test.sh
