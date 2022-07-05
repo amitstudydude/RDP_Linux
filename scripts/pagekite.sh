5fd3447876a07076ae9dfc928370affd0b63da8d
amitstudydude <99377817+amitstudydude@users.noreply.github.com>
2022-07-05 12:34:32 +0530

    Update pagekite.sh

--- scripts/pagekite.sh
+++ scripts/pagekite.sh
@@ -4,5 +4,5 @@ cd .. && pwd && python3 -m http.server 6080 &
 wget https://raw.githubusercontent.com/amitstudydude/RDP_Linux/main/jhaji.exp
 curl -O https://pagekite.net/pk/pagekite.py && sudo chmod +x pagekite.py && sudo mv -f pagekite.py /usr/local/bin
 echo " expect jhaji.exp & " > pagekite.sh
-echo " sleep 10 && bash test.sh " >> pagekite.sh
+echo " sleep 10 && bash pagekite.sh " >> pagekite.sh
 bash pagekite.sh
