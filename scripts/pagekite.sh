314b441151a0f74f44547ea9a9ee5605def76673
amitstudydude <99377817+amitstudydude@users.noreply.github.com>
2022-07-06 13:32:11 +0530

    Delete pagekite.sh

--- scripts/pagekite.sh
+++
@@ -1,9 +0,0 @@
-sudo apt install python3 expect ufw
-sudo ufw enable && sudo ufw allow 8080
-cd .. && pwd && python3 -m http.server 6080 &
-wget https://raw.githubusercontent.com/amitstudydude/RDP_Linux/main/jhaji.exp
-curl -O https://pagekite.net/pk/pagekite.py && sudo chmod +x pagekite.py && sudo mv -f pagekite.py /usr/local/bin
-expect jhaji.exp 
-echo " pagekite.py 8080 jhaji.pagekite.me & " > pagekite.sh
-echo " sleep 10 && bash pagekite.sh " >> pagekite.sh
-bash pagekite.sh
