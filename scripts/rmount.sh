54d9b85917d93897f10ff327fee5030a2130dea7
amitstudydude <99377817+amitstudydude@users.noreply.github.com>
2022-07-06 18:35:27 +0530

    Update rmount.sh

--- scripts/rmount.sh
+++ scripts/rmount.sh
@@ -1,7 +1,7 @@
 su - root -c "wget https://raw.githubusercontent.com/amitstudydude/RDP_Linux/main/rclone.conf && mkdir -p /root/.config/rclone/ && cp rclone.conf /root/.config/rclone "
 curl https://rclone.org/install.sh | sudo bash > /dev/null 2>&1
 rclone lsd Teamdrive:
-mkdir Clone && cd Clone
+mkdir Cloud && cd Cloud
 mkdir Mega Teamdrive Drive
 rclone mount Mega: /content/Cloud/Mega --user-agent 'Mozilla' --vfs-cache-mode full --cache-dir '/content/Cloud/cache' --allow-other --daemon 
 rclone mount Teamdrive: /content/Cloud/Teamdrive --user-agent 'Mozilla' --vfs-cache-mode full --cache-dir '/content/Cloud/cache' --allow-other --daemon 
