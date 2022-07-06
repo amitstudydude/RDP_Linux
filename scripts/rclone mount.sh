bc16dbe6fbd833849d856aca37254c63bb2c38d6
amitstudydude <99377817+amitstudydude@users.noreply.github.com>
2022-07-06 15:02:53 +0530

    Create rclone mount.sh

---
+++ scripts/rclone mount.sh
@@ -0,0 +1,8 @@
+su - root -c "wget https://raw.githubusercontent.com/amitstudydude/RDP_Linux/main/rclone.conf && mkdir -p /root/.config/rclone/ && cp rclone.conf /root/.config/rclone "
+curl https://rclone.org/install.sh | sudo bash
+rclone lsd Teamdrive:
+mkdir Cloud && cd Cloud
+mkdir -p Mega Teamdrive Drive
+rclone mount Teamdrive: Teamdrive > /dev/null 2>&1 &
+rclone mount Mega: Mega > /dev/null 2>&1 &
+rclone mount Drive: Drive > /dev/null 2>&1
