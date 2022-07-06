dc27d6f640b21a2de2c44253c4644ad83b348923
amitstudydude <99377817+amitstudydude@users.noreply.github.com>
2022-07-06 18:44:51 +0530

    Update rclonemount.sh

--- scripts/rclonemount.sh
+++ scripts/rclonemount.sh
@@ -8,7 +8,7 @@ rclone mount Teamdrive: /content/Cloud/Teamdrive --user-agent 'Mozilla' --vfs-ca
 rclone mount Drive: /content/Cloud/Drive --user-agent 'Mozilla' --vfs-cache-mode full --cache-dir '/content/Cloud/cache' --allow-other --daemon --transfers 100 
 
 
-rclone --user-agent "Mozilla" copy "/content/Cloud/Mega/" "/content/Cloud/Teamdrive/new" --transfers 200 --checkers 20 --stats-one-line --stats=5s -v 
+rclone --user-agent "Mozilla" copy "/content/Cloud/Mega/" "/content/Cloud/Teamdrive/new" --transfers 200 --checkers 200 --stats-one-line --stats=5s -v 
 
 cd && rm Mega Teamdrive Drive cache
 #--buffer-size 256M --transfers 100 --vfs-read-chunk-size 500M --vfs-cache-max-size 50G --vfs-cache-max-age 0h0m1s --vfs-cache-poll-interval 0m1s 
