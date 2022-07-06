dbd732ed39bc3bc4bad19fb736a0130a2449d0e8
amitstudydude <99377817+amitstudydude@users.noreply.github.com>
2022-07-06 18:36:50 +0530

    Update and rename rmount.sh to rclonemount.sh

--- scripts/rclonemount.sh
+++ scripts/rclonemount.sh
@@ -2,15 +2,15 @@ su - root -c "wget https://raw.githubusercontent.com/amitstudydude/RDP_Linux/mai
 curl https://rclone.org/install.sh | sudo bash > /dev/null 2>&1
 rclone lsd Teamdrive:
 mkdir Cloud && cd Cloud
-mkdir Mega Teamdrive Drive
+mkdir Mega Teamdrive Drive cache
 rclone mount Mega: /content/Cloud/Mega --user-agent 'Mozilla' --vfs-cache-mode full --cache-dir '/content/Cloud/cache' --allow-other --daemon 
 rclone mount Teamdrive: /content/Cloud/Teamdrive --user-agent 'Mozilla' --vfs-cache-mode full --cache-dir '/content/Cloud/cache' --allow-other --daemon 
 rclone mount Drive: /content/Cloud/Drive --user-agent 'Mozilla' --vfs-cache-mode full --cache-dir '/content/Cloud/cache' --allow-other --daemon 
-cd && rm Mega Teamdrive Drive cache
+
 
 rclone --user-agent "Mozilla" copy "/content/Cloud/Mega/" "/content/Cloud/Teamdrive/new" --transfers 20 --checkers 20 --stats-one-line --stats=5s -v 
 
-
+cd && rm Mega Teamdrive Drive cache
 #--buffer-size 256M --transfers 10 --vfs-read-chunk-size 500M --vfs-cache-max-size 50G --vfs-cache-max-age 0h0m1s --vfs-cache-poll-interval 0m1s 
 
 
