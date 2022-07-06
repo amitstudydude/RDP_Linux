81237c458b0e959524dba0fffb639aac5fc80dd8
amitstudydude <99377817+amitstudydude@users.noreply.github.com>
2022-07-06 18:25:11 +0530

    Update rmount.sh

--- scripts/rmount.sh
+++ scripts/rmount.sh
@@ -1,8 +1,6 @@
 su - root -c "wget https://raw.githubusercontent.com/amitstudydude/RDP_Linux/main/rclone.conf && mkdir -p /root/.config/rclone/ && cp rclone.conf /root/.config/rclone "
 curl https://rclone.org/install.sh | sudo bash > /dev/null 2>&1
 rclone lsd Teamdrive:
-mkdir Cloud && cd Cloud
-mkdir -p Mega Teamdrive Drive cache
 rclone mount Mega: /content/Cloud/Mega --user-agent 'Mozilla' --buffer-size 256M --transfers 10 --vfs-cache-mode minimal --vfs-read-chunk-size 500M --vfs-cache-max-size 50G --vfs-cache-max-age 0h0m1s --vfs-cache-poll-interval 0m1s --cache-dir '/content/Cloud/cache' --allow-other --daemon 
 rclone mount Teamdrive: /content/Cloud/Teamdrive --user-agent 'Mozilla' --buffer-size 256M --transfers 10 --vfs-cache-mode minimal --vfs-read-chunk-size 500M --vfs-cache-max-size 50G --vfs-cache-max-age 0h0m1s --vfs-cache-poll-interval 0m1s --cache-dir '/content/Cloud/cache' --allow-other --daemon 
 rclone mount Drive: /content/Cloud/Drive --user-agent 'Mozilla' --buffer-size 256M --transfers 10 --vfs-cache-mode minimal --vfs-read-chunk-size 500M --vfs-cache-max-size 50G --vfs-cache-max-age 0h0m1s --vfs-cache-poll-interval 0m1s --cache-dir '/content/Cloud/cache' --allow-other --daemon 
