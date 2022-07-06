438f207cc56bab3b074c8655403b78ec9a2d6d4c
amitstudydude <99377817+amitstudydude@users.noreply.github.com>
2022-07-06 15:54:02 +0530

    Update rclonemount.sh

--- scripts/rclonemount.sh
+++ scripts/rclonemount.sh
@@ -1,9 +1,9 @@
 su - root -c "wget https://raw.githubusercontent.com/amitstudydude/RDP_Linux/main/rclone.conf && mkdir -p /root/.config/rclone/ && cp rclone.conf /root/.config/rclone "
-curl https://rclone.org/install.sh | sudo bash
+curl https://rclone.org/install.sh | sudo bash > /dev/null 2>&1
 rclone lsd Teamdrive:
 mkdir Cloud && cd Cloud
 mkdir -p Mega Teamdrive Drive
 rclone mount Teamdrive: Teamdrive > /dev/null 2>&1 &
-rclone mount Mega: Mega > /dev/null 2>&1 &
-rclone mount Drive: Drive > /dev/null 2>&1 &
-
+sleep 3 && rclone mount Mega: Mega > /dev/null 2>&1 &
+sleep 3 && rclone mount Drive: Drive > /dev/null 2>&1 &
+sleep 3 
