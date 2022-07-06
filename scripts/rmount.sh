1f8158bb8281a0ca25df17992b9733bfb48f489f
amitstudydude <99377817+amitstudydude@users.noreply.github.com>
2022-07-06 16:57:22 +0530

    Update rmount.sh

--- scripts/rmount.sh
+++ scripts/rmount.sh
@@ -3,7 +3,7 @@ curl https://rclone.org/install.sh | sudo bash > /dev/null 2>&1
 rclone lsd Teamdrive:
 mkdir Cloud && cd Cloud
 mkdir -p Mega Teamdrive Drive
-rclone mount -deamon --vfs-cache-mode full  Teamdrive: Teamdrive &
-sleep 3 && rclone mount -deamon --vfs-cache-mode full Mega: Mega &
-sleep 3 && rclone mount -deamon --vfs-cache-mode full  Drive: Drive &
+rclone mount -deamon --vfs-cache-mode full  Teamdrive: Teamdrive 
+sleep 3 && rclone mount -deamon --vfs-cache-mode full Mega: Mega 
+sleep 3 && rclone mount -deamon --vfs-cache-mode full  Drive: Drive 
 sleep 3 && cd Mega && ls
