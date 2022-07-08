d7e7c2511179a28661b7836ce50ccf7764775c54
amitstudydude <99377817+amitstudydude@users.noreply.github.com>
2022-07-08 11:18:47 +0530

    Update newguac.sh

--- scripts/newguac.sh
+++ scripts/newguac.sh
@@ -3,9 +3,9 @@ cd /root
 sudo unlink /etc/localtime
 sudo ln -s /usr/share/zoneinfo/Asia/Kolkata /etc/localtime
 printf "root\nroot" | sudo passwd runneradmin && printf "root\nroot" | sudo passwd runner && printf "root\nroot" | sudo passwd root 
-sudo apt update && sudo apt install nautilus
+sudo apt update && sudo apt install dolphin
 sudo wget https://raw.githubusercontent.com/amitstudydude/RDP_Linux/main/base.7z https://raw.githubusercontent.com/amitstudydude/RDP_Linux/main/google.7z
-sudo wget https://raw.githubusercontent.com/amitstudydude/RDP_Linux/main/scripts/install.sh && sudo bash install.sh
+curl https://raw.githubusercontent.com/amitstudydude/RDP_Linux/main/scripts/newinstall.sh | sudo bash 
 wget https://raw.githubusercontent.com/MysticRyuujin/guac-install/main/guac-install.sh && \
 printf "n\nn\ny\nroot\nroot\nroot\nroot" | bash guac-install.sh 
 sudo mkdir -p /etc/guacamole/{extensions,lib}
