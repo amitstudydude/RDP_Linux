b60d38376c384282097ee3de5deec2e4fe276f73
amitstudydude <99377817+amitstudydude@users.noreply.github.com>
2022-07-08 11:31:41 +0530

    Update newinstall.sh

--- scripts/newinstall.sh
+++ scripts/newinstall.sh
@@ -1,2 +1,4 @@
-sudo DEBIAN_FRONTEND=noninteractive apt -y install openssh-server ssh apt-utils gcc g++ openjdk-8-jdk gnome-session  libjson-c-dev cmake libwebsockets-dev build-essential make expect libavcodec-dev libavutil-dev libavformat-dev libwebsockets-dev git libtelnet-dev libpango1.0-dev libswscale-dev libpulse-dev libvorbis-dev libwebp-dev libssl-dev libssh2-1-dev freerdp2-dev libvncserver-dev libossp-uuid-dev libtool-bin libpng-dev libjpeg-turbo8-dev libcairo2-dev xrdp tomcat9 maven build-essential
+sudo DEBIAN_FRONTEND=noninteractive apt -y install tomcat9-admin tomcat9-user tomcat9-common openssh-server ssh apt-utils gcc g++ openjdk-8-jdk gnome-session  libjson-c-dev cmake libwebsockets-dev build-essential make expect libavcodec-dev libavutil-dev libavformat-dev libwebsockets-dev git libtelnet-dev libpango1.0-dev libswscale-dev libpulse-dev libvorbis-dev libwebp-dev libssl-dev libssh2-1-dev freerdp2-dev libvncserver-dev libossp-uuid-dev libtool-bin libpng-dev libjpeg-turbo8-dev libcairo2-dev xrdp tomcat9 maven build-essential
   
+ 
+ 
