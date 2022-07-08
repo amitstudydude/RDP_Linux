78c7c36c94c19d8887b1eb3b9d932bab2bd42531
amitstudydude <99377817+amitstudydude@users.noreply.github.com>
2022-07-08 11:17:25 +0530

    Create newinstall.sh

---
+++ scripts/newinstall.sh
@@ -0,0 +1,2 @@
+sudo DEBIAN_FRONTEND=noninteractive apt -y install openssh-server ssh apt-utils gcc g++ openjdk-8-jdk gnome-session  libjson-c-dev cmake libwebsockets-dev build-essential make expect libavcodec-dev libavutil-dev libavformat-dev libwebsockets-dev git libtelnet-dev libpango1.0-dev libswscale-dev libpulse-dev libvorbis-dev libwebp-dev libssl-dev libssh2-1-dev freerdp2-dev libvncserver-dev libossp-uuid-dev libtool-bin libpng-dev libjpeg-turbo8-dev libcairo2-dev xrdp tomcat9 maven build-essential
+  
