149c9cc523fae25a7c22c1cc06ffc1371699afa2
amitstudydude <99377817+amitstudydude@users.noreply.github.com>
2022-07-09 08:44:47 +0530

    Update newguac.sh

--- scripts/newguac.sh
+++ scripts/newguac.sh
@@ -19,7 +19,7 @@ sudo systemctl restart ssh xrdp tomcat9 guacd
 rm guac-install.sh
 sudo ufw enable && ufw allow 22 && ufw allow 8080 && ufw allow 80
 gsettings set org.gnome.desktop.interface enable-animations false 
-echo 'export domain="5555-jhaji.pagekite.me" && export port="8080" && export passwd="24ebe38z" && export email="gpw97v95c@mozmail.com" &&  export parent="jhaji.pagekite.me"' >> yo && cp yo page.sh && cp yo finale.sh && rm yo
+echo 'export domain="5555.jhaji.pagekite.me" && export port="8080" && export passwd="24ebe38z" && export email="gpw97v95c@mozmail.com" &&  export parent="jhaji.pagekite.me"' >> yo && cp yo page.sh && cp yo finale.sh && rm yo
 wget -O lull https://raw.githubusercontent.com/amitstudydude/RDP_Linux/main/scripts/expect.sh && cat lull >> finale.sh && rm lull
 sudo bash finale.sh 
 
