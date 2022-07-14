05ebcf176217042384010478b15b60a6d6bfed9a
amitstudydude <99377817+amitstudydude@users.noreply.github.com>
2022-07-14 13:39:29 +0530

    Delete ttyd1.sh

--- scripts/ttyd1.sh
+++
@@ -1,4 +0,0 @@
-sudo apt-get install build-essential cmake git libjson-c-dev libwebsockets-dev && git clone https://github.com/tsl0922/ttyd.git && git clone https://github.com/novnc/noVNC.git /opt/novnc && git clone https://github.com/novnc/websockify /opt/novnc/utils/websockify && \ 
-cd ttyd && mkdir build && cd build && cmake .. && make && make install && ttyd -p 1234 bash -x & 
-npm install -g localtunnel
-lt --port 1234 &
