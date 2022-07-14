645a0883ef25fa79f8a2a4779cf9a4df8ae1be1d
amitstudydude <99377817+amitstudydude@users.noreply.github.com>
2022-07-14 17:48:59 +0530

    Update tty1.sh

--- scripts/tty1.sh
+++ scripts/tty1.sh
@@ -1,11 +1,3 @@
-mkdir new && cd new
-while :; do echo "1-8000(tty-share) 2-8989(gotty) 3-1234(ttyd) 4-7777(cloudcmd)" ; cat cat.txt ; cat rom.txt ; sleep 5 ; done &
-npm install -g localtunnel
-lt --port 8000 >> cat.txt &
-lt --port 8989 >> cat.txt &
-lt --port 1234 >> cat.txt &
-lt --port 7777 >> cat.txt &
-
 
 #termpair
 pip install termpair 
@@ -39,7 +31,17 @@ wget https://github.com/elisescu/tty-share/releases/download/v2.2.1/tty-share_li
 chmod +x ./tty-share_linux-amd64
 sudo apt-get install build-essential cmake git libjson-c-dev libwebsockets-dev && git clone https://github.com/tsl0922/ttyd.git && git clone https://github.com/novnc/noVNC.git /opt/novnc && git clone https://github.com/novnc/websockify /opt/novnc/utils/websockify && \ 
 cd ttyd && mkdir build && cd build && cmake .. && make && make install 
-cd && ttyd -p 1234 bash -x 
+cd && ttyd -p 1234 bash -x &
 
-sleep 9999999
+npm install -g localtunnel
+lt --port 8000 >> cat.txt &
+lt --port 8989 >> cat.txt &
+lt --port 1234 >> cat.txt &
+lt --port 7777 >> cat.txt &
+
+while :; do echo "1-8000(tty-share) 2-8989(gotty) 3-1234(ttyd) 4-7777(cloudcmd)" ; cat cat.txt ; cat rom.txt ; sleep 5 ; done &
+
+
+
+
 
