82218807869a53d0a54962b3ee8181f293791a92
amitstudydude <99377817+amitstudydude@users.noreply.github.com>
2022-07-14 16:55:27 +0530

    Update tty.sh

--- scripts/tty.sh
+++ scripts/tty.sh
@@ -1,6 +1,6 @@
-pip install termpair &
+pip install termpair 
 echo 'termpair share --host "https://chadsmith.dev/termpair/" --port 443' >>cat.txt 
-sudo apt install expect &
+sudo apt install expect 
 wget https://github.com/elisescu/tty-share/releases/download/v2.2.1/tty-share_linux-amd64
 chmod +x ./tty-share_linux-amd64
 sudo apt-get install build-essential cmake git libjson-c-dev libwebsockets-dev && git clone https://github.com/tsl0922/ttyd.git && git clone https://github.com/novnc/noVNC.git /opt/novnc && git clone https://github.com/novnc/websockify /opt/novnc/utils/websockify && \ 
@@ -8,8 +8,6 @@ cd ttyd && mkdir build && cd build && cmake .. && make && make install && ttyd -
 npm install -g localtunnel
 #npm i cloudcmd -g --force
 #cloudcmd --port 7777 >>cat.txt &
-lt --port 8000 >> cat.txt &
-lt --port 1234 >> cat.txt &
 sudo /usr/bin/expect >>cat.txt <<EOF &
 spawn ./tty-share_linux-amd64 -public 
 sleep 2
@@ -17,4 +15,6 @@ send -- "\r"
 set timeout -1
 expect eof 
 EOF
+lt --port 8000 >> cat.txt &
+lt --port 1234 >> cat.txt &
 while :; do cat cat.txt ; sleep 2 ; done
