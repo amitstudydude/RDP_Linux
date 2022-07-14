2943a1f5c719c9ceeef60b324a51121a45a434a7
amitstudydude <99377817+amitstudydude@users.noreply.github.com>
2022-07-14 12:46:53 +0530

    Update tty.sh

--- scripts/tty.sh
+++ scripts/tty.sh
@@ -1,7 +1,7 @@
 sudo apt install expect
 wget https://github.com/elisescu/tty-share/releases/download/v2.2.1/tty-share_linux-amd64
 chmod +x ./tty-share_linux-amd64
-while :; do cat cat.txt ; sleep 2 ; done &
+while :; do cat cat.txt ; sleep 10 ; done &
 sudo /usr/bin/expect >cat.txt <<EOF
 spawn ./tty-share_linux-amd64 -public 
 sleep 2
