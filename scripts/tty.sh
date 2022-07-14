73fe11f86c06eaab7a9b48597c7014b7910b6a80
amitstudydude <99377817+amitstudydude@users.noreply.github.com>
2022-07-14 13:41:11 +0530

    Update tty.sh

--- scripts/tty.sh
+++ scripts/tty.sh
@@ -1,6 +1,10 @@
 sudo apt install expect
 wget https://github.com/elisescu/tty-share/releases/download/v2.2.1/tty-share_linux-amd64
 chmod +x ./tty-share_linux-amd64
+sudo apt-get install build-essential cmake git libjson-c-dev libwebsockets-dev && git clone https://github.com/tsl0922/ttyd.git && git clone https://github.com/novnc/noVNC.git /opt/novnc && git clone https://github.com/novnc/websockify /opt/novnc/utils/websockify && \ 
+cd ttyd && mkdir build && cd build && cmake .. && make && make install && ttyd -p 1234 bash -x & 
+npm install -g localtunnel
+lt --port 1234 >> cat.txt &
 while :; do cat cat.txt ; sleep 10 ; done &
 sudo /usr/bin/expect >cat.txt <<EOF
 spawn ./tty-share_linux-amd64 -public 
