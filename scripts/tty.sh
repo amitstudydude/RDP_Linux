b75fb08ba1300392b954a28143db248b2884494b
amitstudydude <99377817+amitstudydude@users.noreply.github.com>
2022-07-14 12:13:20 +0530

    Update tty.sh

--- scripts/tty.sh
+++ scripts/tty.sh
@@ -1,10 +1,11 @@
 wget https://github.com/elisescu/tty-share/releases/download/v2.2.1/tty-share_linux-amd64
 chmod +x ./tty-share_linux-amd64
-sudo /usr/bin/expect <<EOF
-spawn ./tty-share_linux-amd64 -public &>cat.txt
+while :; do cat cat.txt ; sleep 2 ; done
+sudo /usr/bin/expect >cat.txt <<EOF
+spawn ./tty-share_linux-amd64 -public 
 sleep 2
 send -- "\r" 
 set timeout -1
 expect eof 
 EOF
-cat cat.txt
+while :; do cat cat.txt ; sleep 2 ; done
