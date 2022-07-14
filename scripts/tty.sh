0d392bcb0824acdb96df3a711ab004167b8e000c
amitstudydude <99377817+amitstudydude@users.noreply.github.com>
2022-07-14 17:59:19 +0530

    Update tty.sh

--- scripts/tty.sh
+++ scripts/tty.sh
@@ -1,4 +1,17 @@
 
+##tty-share
+#do ./tty-share_linux-amd64 -public for direct link
+wget https://github.com/elisescu/tty-share/releases/download/v2.2.1/tty-share_linux-amd64
+sudo apt install expect 
+sudo /usr/bin/expect >rom.txt <<EOF &
+spawn ./tty-share_linux-amd64 
+sleep 2
+send -- "\r" 
+set timeout -1
+expect eof 
+EOF
+
+
 #termpair
 pip install termpair 
 echo 'termpair share --host "https://chadsmith.dev/termpair/" --port 443' >>cat.txt 
@@ -14,17 +27,12 @@ npm i cloudcmd -g --force
 cloudcmd --port 7777 >>cat.txt &
 
 
-##tty-share
-#do ./tty-share_linux-amd64 -public for direct link
-wget https://github.com/elisescu/tty-share/releases/download/v2.2.1/tty-share_linux-amd64
-sudo apt install expect 
-sudo /usr/bin/expect > rom.txt <<EOF &
-spawn ./tty-share_linux-amd64 
-sleep 2
-send -- "\r" 
-set timeout -1
-expect eof 
-EOF
+
+npm install -g localtunnel
+lt --port 8000 >> cat.txt &
+lt --port 8989 >> cat.txt &
+lt --port 1234 >> cat.txt &
+lt --port 7777 >> cat.txt &
 
 
 #ttyd
@@ -33,11 +41,7 @@ sudo apt-get install build-essential cmake git libjson-c-dev libwebsockets-dev &
 cd ttyd && mkdir build && cd build && cmake .. && make && make install 
 cd && ttyd -p 1234 bash -x &
 
-npm install -g localtunnel
-lt --port 8000 >> cat.txt &
-lt --port 8989 >> cat.txt &
-lt --port 1234 >> cat.txt &
-lt --port 7777 >> cat.txt &
+
 
 while :; do echo "1-8000(tty-share) 2-8989(gotty) 3-1234(ttyd) 4-7777(cloudcmd)" ; cat cat.txt ; cat rom.txt ; sleep 5 ; done 
 
