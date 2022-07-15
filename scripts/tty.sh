dfffcb04677a39cf511da453f64b63b9d95d4a4a
amitstudydude <99377817+amitstudydude@users.noreply.github.com>
2022-07-15 07:54:56 +0530

    Update tty.sh

--- scripts/tty.sh
+++ scripts/tty.sh
@@ -4,7 +4,7 @@ lt --port 8000 >> cat.txt &
 lt --port 8989 >> cat.txt &
 lt --port 1234 >> cat.txt &
 lt --port 7777 >> cat.txt &
-while :; do echo "1-8000(tty-share) 2-8989(gotty) 3-1234(ttyd) 4-7777(cloudcmd)" ; cat ./cat.txt ; cat ./rom.txt ; sleep 30 ; done &
+while :; do echo "1-8000(tty-share) 2-8989(gotty) 3-1234(ttyd) 4-7777(cloudcmd)" ; cat ./cat.txt ; cat ./rom.txt ; sleep 10 ; done &
 
 
 ##tty-share
