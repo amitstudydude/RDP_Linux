ab279de0ca170d73c1b9335c0e538c8b567432a3
amitstudydude <99377817+amitstudydude@users.noreply.github.com>
2022-07-20 16:03:02 +0530

    Update autoclick.sh

--- scripts/autoclick.sh
+++ scripts/autoclick.sh
@@ -3,3 +3,6 @@ xdotool getmouselocation --shell
 while :; do xdotool mousemove $X $Y click 1 ; done
 
 
+##curl https://raw.githubusercontent.com/amitstudydude/RDP_Linux/main/scripts/autoclick.sh | bash
+
+##
