80a568566187c16430415aeaae11869efb545be5
amitstudydude <99377817+amitstudydude@users.noreply.github.com>
2022-07-20 15:57:29 +0530

    Update autoclick.sh

--- scripts/autoclick.sh
+++ scripts/autoclick.sh
@@ -1,9 +1,4 @@
-#!/bin/bash
 sudo apt install xdotool
-xdotool click --repeat 10000000000000 --delay 10000 1
+while :; do xdotool click --repeat 10000000000000 --delay 10000 1 ; done
 
 
-while [ 1 ]; do
-  xdotool mousemove 126 535 click 1 &
-  sleep 5
-done
