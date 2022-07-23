080dcf0a96271dafa6df0704783f22f758167668
amitstudydude <99377817+amitstudydude@users.noreply.github.com>
2022-07-23 13:38:15 +0530

    Update autoclick.sh

--- scripts/autoclick.sh
+++ scripts/autoclick.sh
@@ -1,6 +1,6 @@
 sudo apt install xdotool
 xdotool getmouselocation --shell
-while :; do xdotool mousemove $X $Y click 1 ; done
+while :; do xdotool mousemove $X $Y click 1 ; sleep 10 ;  done
 
 
 ##curl https://raw.githubusercontent.com/amitstudydude/RDP_Linux/main/scripts/autoclick.sh | bash
