5b63d059f938286efd8b5c2ff9d91d6d7f1dd60f
amitstudydude <99377817+amitstudydude@users.noreply.github.com>
2022-07-26 15:59:20 +0530

    Update ssh.sh

--- scripts/ssh.sh
+++ scripts/ssh.sh
@@ -1,27 +1,35 @@
-wget https://raw.githubusercontent.com/amitstudydude/RDP_Linux/main/google.7z && 7z x *7z  && rm *7z 
-wget  https://raw.githubusercontent.com/amitstudydude/RDP_Linux/main/scripts/brave && sudo bash brave && rm brave
-sudo apt update && sudo apt install xrdp gnome-session ssh openssh-server -y 
+wget https://raw.githubusercontent.com/amitstudydude/RDP_Linux/main/google.7z && 7z x *7z  && rm *7z &
+wget  https://raw.githubusercontent.com/amitstudydude/RDP_Linux/main/scripts/brave && sudo bash brave && rm brave &
+rm -rf amit
+git init
+git clone https://jhaji-kv@bitbucket.org/7sk8g739g/amit.git
+cd amit
+rm *.txt
+sudo apt install expect
+sudo /usr/bin/expect <<EOF
+spawn git push origin
+sleep 3
+send -- "utT2AtgBKq9PKvckcLLN\r"
+sleep 1
+expect eof
+EOF
+mkdir rdp 
+cd rdp
+rm *.txt
+while :; do cat tcp ; sleep 2 ; done &
+sudo apt update && sudo apt install pip && pip install jprq
+jprq tcp 22 &>>doge &
+jprq tcp 3389 &>>doge &
+sudo apt update && sudo apt install pip xrdp gnome-session ssh openssh-server -y 
 printf "root\nroot" | sudo passwd runneradmin && printf "root\nroot" | sudo passwd runner && printf "root\nroot" | sudo passwd root        
-sudo service xrdp restart
 echo "PasswordAuthentication yes" >>sshd_config
 echo "PermitUserEnvironment yes" >>sshd_config
 echo "PermitRootLogin yes" >>sshd_config
 sudo cat sshd_config >> /etc/ssh/sshd_config
 sudo service ssh restart
 rm sshd_config
-rm -rf amit
-git init
-git clone https://jhaji-kv@bitbucket.org/7sk8g739g/amit.git
-cd amit
-rm '*.txt'
-mkdir rdp 
-cd rdp
-rm '*.txt'
-while :; do cat tcp ; sleep 2 ; done &
-apt update && apt install pip && pip install jprq 
-jprq tcp 22 &>>doge &
-jprq tcp 3389 &>>doge &
-sed '22,24!d' doge >> tcp
+sudo service xrdp restart
+sed '22,24!d' doge >>tcp
 git config --global user.email "you@example.com"
 git config --global user.name "Your Name"
 git add tcp
