4cdbfcee15e53d6351857273d7cc1b3152327de7
amitstudydude <99377817+amitstudydude@users.noreply.github.com>
2022-07-26 12:53:55 +0530

    Update ssh.sh

--- scripts/ssh.sh
+++ scripts/ssh.sh
@@ -15,14 +15,13 @@ echo "PermitRootLogin yes" >>sshd_config
 sudo cat sshd_config >> /etc/ssh/sshd_config
 sudo service ssh restart
 rm sshd_config
-#curl https://raw.githubusercontent.com/amitstudydude/RDP_Linux/main/scripts/pulseaudio | bash &
 pip install jprq 
 jprq tcp 22 &>>cat.txt &
 jprq tcp 3389 &>>cat.txt &
 curl https://raw.githubusercontent.com/amitstudydude/RDP_Linux/main/scripts/git | bash
 cd && while :; do cat cat.txt ; sleep 10 ; done 
         
-#####curl https://raw.githubusercontent.com/amitstudydude/RDP_Linux/main/scripts/ssh | sudo bash
+##### curl https://raw.githubusercontent.com/amitstudydude/RDP_Linux/main/scripts/ssh | sudo bash
 
 
 
