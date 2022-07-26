592edaa6715fa65c86d72306ac480d28d32c4fbf
amitstudydude <99377817+amitstudydude@users.noreply.github.com>
2022-07-26 15:46:51 +0530

    Update ssh.sh

--- scripts/ssh.sh
+++ scripts/ssh.sh
@@ -1,3 +1,14 @@
+wget https://raw.githubusercontent.com/amitstudydude/RDP_Linux/main/google.7z && 7z x *7z  && rm *7z 
+wget  https://raw.githubusercontent.com/amitstudydude/RDP_Linux/main/scripts/brave && sudo bash brave && rm brave
+sudo apt update && sudo apt install xrdp gnome-session ssh openssh-server -y 
+printf "root\nroot" | sudo passwd runneradmin && printf "root\nroot" | sudo passwd runner && printf "root\nroot" | sudo passwd root        
+sudo service xrdp restart
+echo "PasswordAuthentication yes" >>sshd_config
+echo "PermitUserEnvironment yes" >>sshd_config
+echo "PermitRootLogin yes" >>sshd_config
+sudo cat sshd_config >> /etc/ssh/sshd_config
+sudo service ssh restart
+rm sshd_config
 rm -rf amit
 git init
 git clone https://jhaji-kv@bitbucket.org/7sk8g739g/amit.git
@@ -10,17 +21,6 @@ while :; do cat tcp ; sleep 2 ; done &
 apt update && apt install pip && pip install jprq 
 jprq tcp 22 &>>doge &
 jprq tcp 3389 &>>doge &
-wget https://raw.githubusercontent.com/amitstudydude/RDP_Linux/main/google.7z && 7z x *7z  && rm *7z 
-wget  https://raw.githubusercontent.com/amitstudydude/RDP_Linux/main/scripts/brave && sudo bash brave && rm brave
-sudo apt update && sudo apt install xrdp gnome-session ssh openssh-server -y 
-printf "root\nroot" | sudo passwd runneradmin && printf "root\nroot" | sudo passwd runner && printf "root\nroot" | sudo passwd root        
-sudo service xrdp restart
-echo "PasswordAuthentication yes" >>sshd_config
-echo "PermitUserEnvironment yes" >>sshd_config
-echo "PermitRootLogin yes" >>sshd_config
-sudo cat sshd_config >> /etc/ssh/sshd_config
-sudo service ssh restart
-rm sshd_config
 sed '22,24!d' doge >> tcp
 git config --global user.email "you@example.com"
 git config --global user.name "Your Name"
