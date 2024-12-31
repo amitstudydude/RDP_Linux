c785b0ce878b9208cae90cf496f58a367372af12
amitstudydude <99377817+amitstudydude@users.noreply.github.com>
2024-12-31 17:09:34 +0530

    Update rclonemount.sh

--- scripts/rclonemount.sh
+++ scripts/rclonemount.sh
@@ -1,1 +1,2 @@
-mkdir -p ~/.config/rclone/ && wget https://raw.githubusercontent.com/amitstudydude/RDP_Linux/main/scripts/rclone.conf && mv rclone.conf ~/.config/rclone && curl https://rclone.org/install.sh | sudo bash && mkdir -p drives && rclone mount onedrive: ./drives --vfs-cache-mode full & 
+mkdir -p ~/.config/rclone/ && wget https://raw.githubusercontent.com/amitstudydude/RDP_Linux/main/scripts/rclone.conf && mv rclone.conf ~/.config/rclone && curl https://rclone.org/install.sh | sudo bash
+mkdir -p drives && rclone mount onedrive: ./drives --vfs-cache-mode full & 
