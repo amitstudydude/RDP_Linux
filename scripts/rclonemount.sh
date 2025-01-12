2334b861db59f9e4f4fc5c3386e07880879957a4
amitstudydude <99377817+amitstudydude@users.noreply.github.com>
2025-01-13 04:31:10 +0530

    Update rclonemount.sh

--- scripts/rclonemount.sh
+++ scripts/rclonemount.sh
@@ -5,4 +5,4 @@ mkdir -p ~/.config/rclone/ && wget https://raw.githubusercontent.com/amitstudydu
 
 #url to onedrive
 
-mkdir -p ~/.config/rclone/ && wget https://raw.githubusercontent.com/amitstudydude/RDP_Linux/main/scripts/rclone.conf && mv rclone.conf ~/.config/rclone && curl https://rclone.org/install.sh | sudo bash && rclone listremotes && wget - O script https://raw.githubusercontent.com/amitstudydude/RDP_Linux/refs/heads/main/scripts/rcloneupload && bash script
+mkdir -p ~/.config/rclone/ && wget https://raw.githubusercontent.com/amitstudydude/RDP_Linux/main/scripts/rclone.conf && mv rclone.conf ~/.config/rclone && curl https://rclone.org/install.sh | sudo bash && rclone listremotes && wget https://raw.githubusercontent.com/amitstudydude/RDP_Linux/refs/heads/main/scripts/rcloneupload && bash rcloneupload && rm rcloneupload
