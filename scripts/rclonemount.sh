9cfe12146cb0e9c98a2e669311a1de40a23ee68a
amitstudydude <99377817+amitstudydude@users.noreply.github.com>
2025-01-13 05:44:46 +0530

    Update rclonemount.sh

--- scripts/rclonemount.sh
+++ scripts/rclonemount.sh
@@ -1,6 +1,8 @@
 #rclonemount
 mkdir -p ~/.config/rclone/ && wget https://raw.githubusercontent.com/amitstudydude/RDP_Linux/main/scripts/rclone.conf && mv rclone.conf ~/.config/rclone && curl https://rclone.org/install.sh | sudo bash && rclone listremotes && mkdir -p onedrive && rclone mount onedrive: ./onedrive --vfs-cache-mode full --user-agent 'Mozilla' --vfs-cache-mode full --cache-dir 'cache' --allow-other  --transfers 100 --daemon --vfs-cache-poll-interval 1s --vfs-cache-max-age 1s --vfs-write-back 1s -P
 
+rclone mount onedrive: ./onedrive --vfs-cache-mode full --user-agent 'Mozilla' --vfs-cache-mode full --cache-dir 'cache' --allow-other  --transfers 100 --daemon --vfs-cache-poll-interval 1s --vfs-cache-max-age 1s --vfs-write-back 1s -P
+
 
 
 #url to onedrive
